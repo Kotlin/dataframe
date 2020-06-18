f7f998b4f52371f83078823e24bc9387400d257b
Anatoly Nikitin <Anatoly.Nikitin@jetbrains.com>
2020-06-18 15:20:23 +0300

    Add 'read' method

---
+++ src/main/kotlin/krangl/typed/Read.kt
@@ -0,0 +1,12 @@
+package krangl.typed
+
+import krangl.ColType
+import krangl.DataFrame
+import krangl.readCSV
+import krangl.readDelim
+import org.apache.commons.csv.CSVFormat
+import java.net.URI
+
+fun read(fileOrUrl: String,
+                  format: CSVFormat = CSVFormat.DEFAULT.withHeader(),
+                  colTypes: Map<String, ColType> = mapOf()) = DataFrame.readCSV(fileOrUrl, format, colTypes).typed<Unit>()
