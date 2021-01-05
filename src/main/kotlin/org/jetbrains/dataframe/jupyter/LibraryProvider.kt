087546c9d15935bf7290d83beda1d530acf858d6
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-05 03:56:06 +0300

    Fix GroupedColumn rendering

--- src/main/kotlin/org/jetbrains/dataframe/jupyter/LibraryProvider.kt
+++ src/main/kotlin/org/jetbrains/dataframe/jupyter/LibraryProvider.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.dataframe.jupyter
 
 import org.jetbrains.dataframe.*
+import org.jetbrains.dataframe.api.columns.GroupedColumn
 import org.jetbrains.kotlinx.jupyter.api.*
 import org.jetbrains.kotlinx.jupyter.api.TypeHandlerExecution
 import org.jetbrains.kotlinx.jupyter.api.libraries.*
@@ -8,10 +9,11 @@ import org.jetbrains.kotlinx.jupyter.api.libraries.*
 
 internal class LibraryProvider: JupyterIntegration({
 
-        render<DataCol> { dataFrameOf(listOf(it)) }
         render<DataFrame<*>> { HTML(it.toHTML()) }
-        render<GroupedDataFrame<*,*>> { it.plain() }
         render<DataRow<*>> { it.toDataFrame() }
+        render<GroupedColumn<*>> { it.df }
+        render<DataCol> { dataFrameOf(listOf(it)) }
+        render<GroupedDataFrame<*,*>> { it.plain() }
 
         import("org.jetbrains.dataframe.*")
         import("org.jetbrains.dataframe.io.*")
