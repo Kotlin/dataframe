4cb1ec32db9aca34333aea80dc75b0f28ca3d450
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-15 01:33:37 +0300

    Read test for 'census'

--- src/test/kotlin/org/jetbrains/dataframe/io/CensusTest.kt
+++ src/test/kotlin/org/jetbrains/dataframe/io/CensusTest.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.dataframe.io
 
+import io.kotlintest.shouldBe
 import org.jetbrains.dataframe.*
 import org.junit.Ignore
 import org.junit.Test
@@ -7,14 +8,16 @@ import org.junit.Test
 class CensusTest {
 
     val path = "data/census.csv"
-    val df = DataFrame.readCSV(path)
+    val df = DataFrame.read(path)
 
     @Test
-    @Ignore
-    fun grouping(){
+    fun grouping() {
 
         df.columnNames().forEach { println(it) }
-        val grouped = df.move { cols { it.name.contains(":") } }.into { it.name.split(":").reversed() }
-        grouped.print()
+        val singleNames = df.columnNames().filter { !it.contains(":") }.toSet()
+        val grouped = df.move {
+            cols { it.name.split(":").let { it.size > 1 && !singleNames.contains(it.last()) } }
+        }.into { it.name.split(":").filter { it.isNotBlank() }.reversed() }
+        grouped.select { cols { it.isGrouped() } }.ncol shouldBe 93
     }
 }
