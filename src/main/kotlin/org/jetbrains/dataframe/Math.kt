67b1c46e2d676dc451b3f3493f2068189db7b80c
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-16 02:15:01 +0300

    Add 'groupBy' operation for column

--- src/main/kotlin/org/jetbrains/dataframe/Math.kt
+++ src/main/kotlin/org/jetbrains/dataframe/Math.kt
@@ -32,42 +32,6 @@ fun <T> Iterable<T>.computeSize(counter: Counter) = map {
 
 internal fun Int.zeroToOne() = if(this == 0) 1 else this
 
-inline fun <reified T : Number> Iterable<T>.mean(): Double = mean(T::class)
-
-fun <T : Number> Iterable<T>.mean(clazz: KClass<T>) = when (clazz) {
-    Double::class -> (this as Iterable<Double>).mean()
-    Int::class -> (this as Iterable<Int>).mean()
-    Long::class -> (this as Iterable<Long>).mean()
-    BigDecimal::class -> (this as Iterable<BigDecimal>).mean()
-    else -> throw IllegalArgumentException()
-}
-
-fun <T: Number> ColumnData<T?>.mean(): Double = (if(hasNulls) values.filterNotNull() else (values as Iterable<T>)).mean(type.jvmErasure as KClass<T>)
-
-@JvmName("doubleMean")
-fun Iterable<Double>.mean(): Double {
-    val counter = Counter()
-    return computeSize(counter).sum() / counter.value.zeroToOne()
-}
-
-@JvmName("intMean")
-fun Iterable<Int>.mean(): Double {
-    val counter = Counter()
-    return computeSize(counter).map { it.toDouble() }.sum() / counter.value.zeroToOne()
-}
-
-@JvmName("longMean")
-fun Iterable<Long>.mean(): Double {
-    val counter = Counter()
-    return computeSize(counter).sum().toDouble() / counter.value.zeroToOne()
-}
-
-@JvmName("bigDecimalMean")
-fun Iterable<BigDecimal>.mean(): Double {
-    val counter = Counter()
-    return computeSize(counter).sum().toDouble() / counter.value.zeroToOne()
-}
-
 fun <T: Number> Iterable<T>.sum(clazz: KClass<T>) = when (clazz) {
     Double::class -> (this as Iterable<Double>).sum() as T
     Int::class -> (this as Iterable<Int>).sum() as T
@@ -91,12 +55,3 @@ fun <T: Number> ColumnData<T>.sum() = values.sum(type.jvmErasure as KClass<T>)
 inline fun <T, reified D : Comparable<D>> DataFrame<T>.median(col: ColumnDef<D?>): Double = get(col).median()
 inline fun <T, reified D : Comparable<D>> DataFrame<T>.median(crossinline selector: RowSelector<T, D?>): Double = rows.asSequence().map { selector(it, it) }.filterNotNull().asIterable().median()
 inline fun <T, reified D : Comparable<D>> DataFrame<T>.median(col: KProperty<D?>): Double = get(col).median()
-inline fun <T, reified D : Number> DataFrame<T>.mean(crossinline selector: RowSelector<T, D?>): Double = rows.asSequence().map { selector(it, it) }.filterNotNull().asIterable().mean()
-inline fun <T, reified D : Number> DataFrame<T>.mean(col: ColumnDef<D>): Double = get(col).mean()
-inline fun <T, reified D : Number> DataFrame<T>.mean(col: KProperty<D>): Double = get(col).mean()
-
-fun <T> DataFrame<T>.mean(): DataRow<T> {
-    return columns.map {
-        column(it.name(), listOf((it as ColumnData<Number>).mean()))
-    }.asDataFrame<T>()[0]
-}
---
+++ src/main/kotlin/org/jetbrains/dataframe/api/mean.kt
@@ -0,0 +1,66 @@
+package org.jetbrains.dataframe
+
+import org.jetbrains.dataframe.api.columns.ColumnData
+import java.math.BigDecimal
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.jvm.jvmErasure
+
+
+inline fun <reified T : Number> Iterable<T>.mean(): Double = mean(T::class)
+
+inline fun <T, reified D : Number> DataFrame<T>.mean(crossinline selector: RowSelector<T, D?>): Double = rows.asSequence().map { selector(it, it) }.filterNotNull().asIterable().mean()
+inline fun <T, reified D : Number> DataFrame<T>.mean(col: ColumnDef<D>): Double = get(col).mean()
+inline fun <T, reified D : Number> DataFrame<T>.mean(col: KProperty<D>): Double = get(col).mean()
+
+fun <T> DataFrame<T>.mean(): DataRow<T> {
+    return columns.map {
+        column(it.name(), listOf((it as ColumnData<Number>).mean()))
+    }.asDataFrame<T>()[0]
+}
+
+fun <T, G> GroupedDataFrame<T, G>.mean(): DataFrame<T> {
+
+    val keyColumnNames = keys.columnNames().toSet()
+    return aggregate {
+        columns.filter { (it.type.classifier!! as KClass<*>).isSubclassOf(Number::class) && !keyColumnNames.contains(it.name()) }
+            .forEach { col ->
+                (col as ColumnData<Number?>).mean() into col.name()
+            }
+    }
+}
+
+fun <T : Number> Iterable<T>.mean(clazz: KClass<T>) = when (clazz) {
+    Double::class -> (this as Iterable<Double>).mean()
+    Int::class -> (this as Iterable<Int>).mean()
+    Long::class -> (this as Iterable<Long>).mean()
+    BigDecimal::class -> (this as Iterable<BigDecimal>).mean()
+    else -> throw IllegalArgumentException()
+}
+
+fun <T: Number> ColumnData<T?>.mean(): Double = (if(hasNulls) values.filterNotNull() else (values as Iterable<T>)).mean(type.jvmErasure as KClass<T>)
+
+@JvmName("doubleMean")
+fun Iterable<Double>.mean(): Double {
+    val counter = Counter()
+    return computeSize(counter).sum() / counter.value.zeroToOne()
+}
+
+@JvmName("intMean")
+fun Iterable<Int>.mean(): Double {
+    val counter = Counter()
+    return computeSize(counter).map { it.toDouble() }.sum() / counter.value.zeroToOne()
+}
+
+@JvmName("longMean")
+fun Iterable<Long>.mean(): Double {
+    val counter = Counter()
+    return computeSize(counter).sum().toDouble() / counter.value.zeroToOne()
+}
+
+@JvmName("bigDecimalMean")
+fun Iterable<BigDecimal>.mean(): Double {
+    val counter = Counter()
+    return computeSize(counter).sum().toDouble() / counter.value.zeroToOne()
+}
