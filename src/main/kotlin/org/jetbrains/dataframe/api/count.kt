25c2ee4d5425ec2b15862284c8e197f57873c041
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-15 13:12:59 +0300

    Rename 'countInto' to 'size' for GroupedDataFrame

--- src/main/kotlin/org/jetbrains/dataframe/api/count.kt
+++ src/main/kotlin/org/jetbrains/dataframe/api/count.kt
@@ -1,7 +1,3 @@
 package org.jetbrains.dataframe
 
-fun <T, G> GroupedDataFrame<T, G>.countInto(columnName: String) = aggregate {
-    nrow into columnName
-}
-
 fun <T> DataFrame<T>.count(predicate: RowFilter<T>) = rows.count { predicate(it, it) }
