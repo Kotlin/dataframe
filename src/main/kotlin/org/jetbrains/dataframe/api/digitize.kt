c69c4e247994b9355d0919d5576bf276b495a8e8
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-15 13:38:57 +0300

    Add 'digitize' operation

---
+++ src/main/kotlin/org/jetbrains/dataframe/api/digitize.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.dataframe.api
+
+import org.jetbrains.dataframe.Predicate
+import org.jetbrains.dataframe.api.columns.ColumnData
+import org.jetbrains.dataframe.map
+
+fun <T: Comparable<T>> ColumnData<T>.digitize(vararg bins: T, right: Boolean = false) = digitize(bins.toList(), right)
+
+fun <T: Comparable<T>> ColumnData<T>.digitize(bins: List<T>, right: Boolean = false): ColumnData<Int> {
+
+    // TODO: use binary search
+    // TODO: support descending order
+    val predicate: (T, T) -> Boolean = if (right) { value, bin -> value <= bin } else { value, bin -> value < bin }
+
+    return map { value ->
+        val index = bins.indexOfFirst { predicate(value, it) }
+        if (index == -1) bins.size
+        else index
+    }
+}
