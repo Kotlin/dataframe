5f1daa714c8faa44b987b7b6b521cec5ff7141bf
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-15 15:42:10 +0300

    Add overload for 'digitize' operation at Double column with integer bin boundaries

--- src/main/kotlin/org/jetbrains/dataframe/Utils.kt
+++ src/main/kotlin/org/jetbrains/dataframe/Utils.kt
@@ -1,5 +1,8 @@
 package org.jetbrains.dataframe
 
+import java.math.BigDecimal
+import kotlin.reflect.KClass
+
 typealias Predicate<T> = (T) -> Boolean
 
 internal infix fun <T> (Predicate<T>).and(other: Predicate<T>): Predicate<T> = { this(it) && other(it) }
@@ -34,3 +37,13 @@ internal fun <T> T.toIterable(getNext: (T) -> T?) = Iterable<T> {
 internal fun <T> List<T>.removeAt(index: Int) = subList(0, index) + subList(index + 1, size)
 
 internal inline fun <reified T> listOfNulls(count: Int) = arrayOfNulls<T>(count).asList()
+
+internal inline fun <reified T: Any> Int.cast() = convert(this, T::class)
+
+internal fun <T: Any> convert(src:Int, targetType: KClass<T>): T = when(targetType) {
+    Double::class -> src.toDouble() as T
+    Long::class -> src.toLong() as T
+    Float::class -> src.toFloat() as T
+    BigDecimal::class -> src.toBigDecimal() as T
+    else -> throw NotImplementedError("Casting int to ${targetType} is not supported")
+}
--- src/main/kotlin/org/jetbrains/dataframe/api/digitize.kt
+++ src/main/kotlin/org/jetbrains/dataframe/api/digitize.kt
@@ -1,8 +1,13 @@
 package org.jetbrains.dataframe
 
 import org.jetbrains.dataframe.api.columns.ColumnData
+import kotlin.reflect.KClass
+
+fun ColumnData<Double>.digitize(vararg bins: Int, right: Boolean = false) = digitize(bins.toList(), Double::class, right)
 
 fun <T: Comparable<T>> ColumnData<T>.digitize(vararg bins: T, right: Boolean = false) = digitize(bins.toList(), right)
+
+fun <T: Comparable<T>> ColumnData<T>.digitize(bins: List<Int>, kclass: KClass<T>, right: Boolean = false): ColumnData<Int> = digitize(bins.toList().map{convert(it, kclass)}, right)
 
 fun <T: Comparable<T>> ColumnData<T>.digitize(bins: List<T>, right: Boolean = false): ColumnData<Int> {
 
