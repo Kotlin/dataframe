33d47d9319e06a702a05b3996bdf3cbe19a85e9f
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-29 00:19:08 +0300

    Update kernel API version

--- src/main/kotlin/org/jetbrains/dataframe/jupyter/Integration.kt
+++ src/main/kotlin/org/jetbrains/dataframe/jupyter/Integration.kt
@@ -9,56 +9,58 @@ import kotlin.reflect.KClass
 
 internal val newDataSchemas = mutableListOf<KClass<*>>()
 
-internal class Integration : JupyterIntegration({
+internal class Integration : JupyterIntegration(){
 
-    render<AnyFrame> { HTML(it.toHTML()) }
-    render<AnyRow> { it.toDataFrame() }
-    render<MapColumn<*>> { it.df }
-    render<AnyCol> { dataFrameOf(listOf(it)) }
-    render<GroupedDataFrame<*, *>> { it.plain() }
+    override fun Builder.onLoaded(notebook: Notebook<*>?) {
 
-    import("org.jetbrains.dataframe.*")
-    import("org.jetbrains.dataframe.io.*")
+        render<AnyFrame> { HTML(it.toHTML()) }
+        render<AnyRow> { it.toDataFrame() }
+        render<MapColumn<*>> { it.df }
+        render<AnyCol> { dataFrameOf(listOf(it)) }
+        render<GroupedDataFrame<*, *>> { it.plain() }
 
-    val codeGen = CodeGenerator.create()
+        import("org.jetbrains.dataframe.*")
+        import("org.jetbrains.dataframe.io.*")
 
-    updateVariable<AnyFrame> { df, property ->
-        codeGen.generate(df, property)?.let {
-            val code = it.with(property.name)
+        val codeGen = CodeGenerator.create()
+
+        updateVariable<AnyFrame> { df, property ->
+            codeGen.generate(df, property)?.let {
+                val code = it.with(property.name)
+                execute(code).name
+            }
+        }
+
+        updateVariable<DataFrameToListNamedStub> { stub, prop ->
+            val code = codeGen.generate(stub).with(prop.name)
             execute(code).name
         }
-    }
 
-    updateVariable<DataFrameToListNamedStub> { stub, prop ->
-        val code = codeGen.generate(stub).with(prop.name)
-        execute(code).name
-    }
+        updateVariable<DataFrameToListTypedStub> { stub, prop ->
+            val code = codeGen.generate(stub).with(prop.name)
+            execute(code).name
+        }
 
-    updateVariable<DataFrameToListTypedStub> { stub, prop ->
-        val code = codeGen.generate(stub).with(prop.name)
-        execute(code).name
-    }
+        fun KotlinKernelHost.addDataSchemas(classes: List<KClass<*>>) {
+            val code = classes.mapNotNull {
+                codeGen.generateExtensionProperties(it)
+            }.joinToString("\n").trim()
 
-    fun KotlinKernelHost.addDataSchemas(classes: List<KClass<*>>){
-        val code = classes.mapNotNull {
-            codeGen.generateExtensionProperties(it)
-        }.joinToString("\n").trim()
+            if (code.isNotEmpty()) {
+                execute(code)
+            }
+        }
 
-        if(code.isNotEmpty()) {
-            execute(code)
+        onClassAnnotation<DataSchema> { addDataSchemas(it) }
+
+        beforeCellExecution {
+            if (newDataSchemas.isNotEmpty()) {
+                addDataSchemas(newDataSchemas)
+                newDataSchemas.clear()
+            }
         }
     }
-
-    onClassAnnotation<DataSchema> { addDataSchemas(it) }
-
-    beforeCellExecution {
-        if(newDataSchemas.isNotEmpty())
-        {
-            addDataSchemas(newDataSchemas)
-            newDataSchemas.clear()
-        }
-    }
-})
+}
 
 fun KotlinKernelHost.useDataSchemas(vararg schemaClasses: KClass<*>){
     newDataSchemas.addAll(schemaClasses)
