48fa0057504228bea397e190c7e77e3c39c822c0
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-03-03 02:21:04 +0300

    'Replace' operation: allow to pass new columns as arguments, add tests

--- src/main/kotlin/org/jetbrains/dataframe/api/replace.kt
+++ src/main/kotlin/org/jetbrains/dataframe/api/replace.kt
@@ -1,17 +1,32 @@
 package org.jetbrains.dataframe
 
 import org.jetbrains.dataframe.api.columns.DataColumn
+import kotlin.reflect.KProperty
 
 fun <T, C> DataFrame<T>.replace(selector: ColumnsSelector<T, C>) = ReplaceCause(this, selector)
+fun <T, C> DataFrame<T>.replace(vararg cols: ColumnReference<C>) = replace { cols.toColumns() }
+fun <T, C> DataFrame<T>.replace(vararg cols: KProperty<C>) = replace { cols.toColumns() }
+fun <T> DataFrame<T>.replace(vararg cols: String) = replace { cols.toColumns() }
+fun <T, C> DataFrame<T>.replace(cols: Iterable<ColumnReference<C>>) = replace { cols.toColumnSet() }
 
 data class ReplaceCause<T, C>(val df: DataFrame<T>, val selector: ColumnsSelector<T, C>)
 
-fun <T, C> ReplaceCause<T, C>.with(transform: DataFrameBase<T>.(DataColumn<C>)->DataColumn<*>): DataFrame<T> {
+fun <T, C> ReplaceCause<T, C>.with(vararg columns: AnyCol) = with(columns.toList())
 
-    val removed = df.doRemove(selector)
-    val toInsert = removed.removedColumns.map {
+fun <T, C> ReplaceCause<T, C>.with(newColumns: List<AnyCol>): DataFrame<T> {
+    var index = 0
+    return with {
+        require(index < newColumns.size) { "Insufficient number of new columns in 'replace': ${newColumns.size} instead of ${df.getColumns(selector).size}" }
+        newColumns[index++]
+    }
+}
+
+fun <T, C> ReplaceCause<T, C>.with(transform: DataFrameBase<T>.(DataColumn<C>)->AnyCol): DataFrame<T> {
+
+    val removeResult = df.doRemove(selector)
+    val toInsert = removeResult.removedColumns.map {
         val newCol = transform(df, it.data.column as DataColumn<C>)
         ColumnToInsert(it.pathFromRoot().dropLast(1) + newCol.name, it, newCol)
     }
-    return removed.df.doInsert(toInsert)
+    return removeResult.df.doInsert(toInsert)
 }
