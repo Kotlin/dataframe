4692506971051d324e38736c3c98f5f89521e5fa
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-03-06 22:01:23 +0300

    Support cell format overriding

--- src/main/kotlin/org/jetbrains/dataframe/api/format.kt
+++ src/main/kotlin/org/jetbrains/dataframe/api/format.kt
@@ -1,12 +1,7 @@
-package org.jetbrains.dataframe.io
+package org.jetbrains.dataframe
 
 import kotlinx.serialization.json.JsonObject
-import org.jetbrains.dataframe.AnyCol
-import org.jetbrains.dataframe.ColumnsSelector
-import org.jetbrains.dataframe.DataFrame
-import org.jetbrains.dataframe.DataRow
-import org.jetbrains.dataframe.RowCellFilter
-import org.jetbrains.dataframe.getColumnsWithPaths
+import org.jetbrains.dataframe.io.toHTML
 import org.jetbrains.kotlinx.jupyter.api.DisplayResult
 import org.jetbrains.kotlinx.jupyter.api.HTML
 
@@ -25,21 +20,37 @@ private fun componentWise(color1: RGBColor, color2: RGBColor, f: (Short, Short) 
 )
 
 interface CellAttributes {
-    fun attributes(): String
+
+    fun attributes(): List<Pair<String, String>>
+
+}
+
+infix fun CellAttributes?.and(other: CellAttributes?): CellAttributes? = when {
+    other == null -> this
+    this == null -> other
+    else -> MergedAttributes(listOf(this, other))
+}
+
+internal class SingleAttribute(val key: String, val value: String) : CellAttributes {
+    override fun attributes() = listOf(key to value)
+}
+
+internal class MergedAttributes(private val attributes: List<CellAttributes>) : CellAttributes {
+    override fun attributes() = attributes.flatMap { it.attributes() }.toMap().toList()
 }
 
 object FormatReceiver {
 
-    internal class SingleAttribute(val attribute: String) : CellAttributes {
+    fun rgb(r: Short, g: Short, b: Short) = RGBColor(r, g, b)
 
-        constructor(attributeName: String, attributeValue: String): this("${attributeName}:${attributeValue}")
-
-        override fun attributes() = attribute
-    }
-
-    internal class MergedAttributes(private val attributes: List<CellAttributes>): CellAttributes {
-        override fun attributes() = attributes.joinToString(";") { it.attributes() }
-    }
+    val black = rgb(0, 0, 0)
+    val white = rgb(255, 255, 255)
+    val green = rgb(0, 255, 0)
+    val red = rgb(255, 0, 0)
+    val blue = rgb(0, 0, 255)
+    val gray = rgb(128, 128, 128)
+    val darkGray = rgb(169, 169, 169)
+    val lightGray = rgb(211, 211, 211)
 
     internal fun attribute(name: String, value: String): CellAttributes = SingleAttribute(name, value)
 
@@ -49,34 +60,29 @@ object FormatReceiver {
     fun textColor(color: RGBColor) = attribute("color", color.encode())
     fun textColor(r: Short, g: Short, b: Short) = textColor(RGBColor(r, g, b))
 
-    fun rgb(r: Short, g: Short, b: Short) = RGBColor(r, g, b)
-
     val italic = attribute("font-style", "italic")
 
     val bold = attribute("font-weight", "bold")
 
-    val white = rgb(0,0,0)
-    val green = rgb(0,255,0)
-    val red = rgb(255,0,0)
-    val blue = rgb(0,0,255)
-
-    val none: CellAttributes = SingleAttribute("")
-
     val underline = attribute("text-decoration", "underline")
-
-    infix fun CellAttributes.and(other: CellAttributes): CellAttributes = MergedAttributes(listOf(this, other))
 
     fun linear(value: Number, from: Pair<Number, RGBColor>, to: Pair<Number, RGBColor>): RGBColor {
         val a = from.first.toDouble()
         val b = to.first.toDouble()
-        if(a < b) return linearGradient(value.toDouble(), a, from.second, b, to.second)
+        if (a < b) return linearGradient(value.toDouble(), a, from.second, b, to.second)
         return linearGradient(value.toDouble(), b, to.second, a, from.second)
     }
 }
 
-internal fun linearGradient(x: Double, minValue: Double, minColor: RGBColor, maxValue: Double, maxColor: RGBColor): RGBColor {
-    if(x < minValue) return minColor
-    if(x > maxValue) return maxColor
+internal fun linearGradient(
+    x: Double,
+    minValue: Double,
+    minColor: RGBColor,
+    maxValue: Double,
+    maxColor: RGBColor
+): RGBColor {
+    if (x < minValue) return minColor
+    if (x > maxValue) return maxColor
     val t = (x - minValue) / (maxValue - minValue)
     return componentWise(minColor, maxColor) { cmin, cmax ->
         (cmin + t * (cmax - cmin)).toInt().toShort()
@@ -85,10 +91,10 @@ internal fun linearGradient(x: Double, minValue: Double, minColor: RGBColor, max
 
 typealias RowColFormatter<T> = (DataRow<T>, AnyCol) -> CellAttributes?
 
-class FrameRender<T>(
-    private val df: DataFrame<T>,
-    private val formatter: RowColFormatter<T>? = null,
-): DisplayResult {
+class FormattedFrame<T>(
+    internal val df: DataFrame<T>,
+    internal val formatter: RowColFormatter<T>? = null,
+) : DisplayResult {
 
     override fun toJson(additionalMetadata: JsonObject) = HTML(df.toHTML(formatter = formatter)).toJson()
 }
@@ -96,30 +102,37 @@ class FrameRender<T>(
 data class ColorClause<T, C>(
     val df: DataFrame<T>,
     val selector: ColumnsSelector<T, C>? = null,
+    val oldFormatter: RowColFormatter<T>? = null,
     val filter: RowCellFilter<T, C> = { true },
 )
+
+fun <T, C> FormattedFrame<T>.format(selector: ColumnsSelector<T, C>) = ColorClause(df, selector, formatter)
 fun <T, C> DataFrame<T>.format(selector: ColumnsSelector<T, C>) = ColorClause(this, selector)
 
+fun <T> FormattedFrame<T>.format() = ColorClause<T, Any?>(df, null, formatter)
 fun <T> DataFrame<T>.format() = ColorClause<T, Any?>(this)
 
 fun <T, C> ColorClause<T, C>.where(filter: RowCellFilter<T, C>) = copy(filter = filter)
 
 typealias CellFormatter<V> = FormatReceiver.(V) -> CellAttributes?
 
-fun <T, C: Number?> ColorClause<T, C>.linearBg(from: Pair<Number, RGBColor>, to: Pair<Number, RGBColor>) = with {
+fun <T, C : Number?> ColorClause<T, C>.linearBg(from: Pair<Number, RGBColor>, to: Pair<Number, RGBColor>) = with {
     if (it != null)
         background(linear(it, from, to))
     else null
 }
 
-fun <T, C> ColorClause<T, C>.with(formatter: CellFormatter<C>): FrameRender<T> {
-    val columns = if(selector != null) df.getColumnsWithPaths(selector).mapNotNull { if(it.depth == 0) it.name else null }.toSet() else null
-    return FrameRender(df) { row, col ->
+fun <T, C> ColorClause<T, C>.with(formatter: CellFormatter<C>): FormattedFrame<T> {
+    val columns =
+        if (selector != null) df.getColumnsWithPaths(selector).mapNotNull { if (it.depth == 0) it.name else null }
+            .toSet() else null
+    return FormattedFrame(df) { row, col ->
+        val oldAttributes = oldFormatter?.invoke(row, col)
         if (columns == null || columns.contains(col.name())) {
             val value = row[col] as C
-            if (filter == null || filter(row, value))
-                formatter(FormatReceiver, value)
-            else null
-        } else null
+            if (filter == null || filter(row, value)) {
+                oldAttributes and formatter(FormatReceiver, value)
+            } else oldAttributes
+        } else oldAttributes
     }
 }
