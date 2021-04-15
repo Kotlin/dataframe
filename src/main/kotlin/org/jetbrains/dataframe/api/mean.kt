948b84338ee90d727dc4c01907adae95497ddfaa
devcrocod <devcrocod@gmail.com>
2021-04-15 12:56:59 +0300

    add support for float, short and byte to `std`, `mean`, `min`, `max`, `sum` add check for non-numeric columns refactor `mean`

--- src/main/kotlin/org/jetbrains/dataframe/api/mean.kt
+++ src/main/kotlin/org/jetbrains/dataframe/api/mean.kt
@@ -2,6 +2,7 @@ package org.jetbrains.dataframe
 
 import org.jetbrains.dataframe.columns.ColumnReference
 import org.jetbrains.dataframe.columns.DataColumn
+import org.jetbrains.dataframe.columns.isNumber
 import java.math.BigDecimal
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
@@ -19,7 +20,9 @@ inline fun <T, G, reified R : Number> GroupedDataFrame<T, G>.mean(columnName: St
 
 fun <T> DataFrame<T>.mean(): DataRow<T> {
     return columns().map {
-        column(it.name(), listOf((it as DataColumn<Number>).mean()))
+        it.takeIf { it.isNumber() }?.let {
+            column(it.name(), listOf((it as DataColumn<Number>).mean()))
+        } ?: column(it.name(), listOf(""))
     }.asDataFrame<T>()[0]
 }
 
@@ -36,7 +39,8 @@ fun <T, G> GroupedDataFrame<T, G>.mean(): DataFrame<T> {
 
 fun <T : Number> Iterable<T>.mean(clazz: KClass<T>) = when (clazz) {
     Double::class -> (this as Iterable<Double>).mean()
-    Int::class -> (this as Iterable<Int>).mean()
+    Float::class -> (this as Iterable<Float>).mean()
+    Int::class, Short::class, Byte::class -> (this as Iterable<Int>).mean()
     Long::class -> (this as Iterable<Long>).mean()
     BigDecimal::class -> (this as Iterable<BigDecimal>).mean()
     else -> throw IllegalArgumentException()
@@ -45,40 +49,49 @@ fun <T : Number> Iterable<T>.mean(clazz: KClass<T>) = when (clazz) {
 fun <T: Number> DataColumn<T?>.mean(): Double = (if(hasNulls) values.filterNotNull() else (values as Iterable<T>)).mean(type.jvmErasure as KClass<T>)
 
 @JvmName("doubleMean")
-fun Iterable<Double>.mean(): Double {
-    val counter = Counter()
-    return computeSize(counter).sum() / counter.value.zeroToOne()
-}
+fun Iterable<Double>.mean(): Double =
+    if (this is Collection) {
+        sum() / size.zeroToOne()
+    } else {
+        var count = 0
+        sumByDouble { count++;it } / count.zeroToOne()
+    }
+
+@JvmName("floatMean")
+fun Iterable<Float>.mean(): Double =
+    if (this is Collection) {
+        sumByDouble { it.toDouble() } / size.zeroToOne()
+    } else {
+        var count = 0
+        sumByDouble { count++;it.toDouble() } / count.zeroToOne()
+    }
 
 @JvmName("intMean")
-fun Iterable<Int>.mean(): Double {
-    val counter = Counter()
-    return computeSize(counter).map { it.toDouble() }.sum() / counter.value.zeroToOne()
-}
+fun Iterable<Int>.mean(): Double =
+    if (this is Collection) {
+        sumByDouble { it.toDouble() } / size.zeroToOne()
+    } else {
+        var count = 0
+        sumByDouble { count++;it.toDouble() } / count.zeroToOne()
+    }
 
 @JvmName("longMean")
-fun Iterable<Long>.mean(): Double {
-    val counter = Counter()
-    return computeSize(counter).sum().toDouble() / counter.value.zeroToOne()
-}
+fun Iterable<Long>.mean(): Double =
+    if (this is Collection) {
+        sumByDouble { it.toDouble() } / size.zeroToOne()
+    } else {
+        var count = 0
+        sumByDouble { count++;it.toDouble() } / count.zeroToOne()
+    }
 
 @JvmName("bigDecimalMean")
-fun Iterable<BigDecimal>.mean(): Double {
-    val counter = Counter()
-    return computeSize(counter).sum().toDouble() / counter.value.zeroToOne()
-}
+fun Iterable<BigDecimal>.mean(): Double =
+    if (this is Collection) {
+        sum().toDouble() / size.zeroToOne()
+    } else {
+        var count = 0
+        sumOf { count++;it }.toDouble() / count.zeroToOne()
+    }
 
 internal fun Int.zeroToOne() = if (this == 0) 1 else this
-
-internal class Counter(var value: Int = 0) {
-    operator fun inc(): Counter {
-        value++
-        return this
-    }
-}
-
-internal fun <T> Iterable<T>.computeSize(counter: Counter) = map {
-    counter.inc()
-    it
-}
 
