696528db25bfc56191ba31ff481f2ad0267c3b6d
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-04-30 01:53:30 +0300

    Make 'isEmpty' true not only for no columns, but also for no rows

--- src/main/kotlin/org/jetbrains/dataframe/api/isEmpty.kt
+++ src/main/kotlin/org/jetbrains/dataframe/api/isEmpty.kt
@@ -1,4 +1,6 @@
 package org.jetbrains.dataframe
 
 fun AnyRow.isEmpty() = owner.columns().all { it[index] == null }
-fun AnyFrame.isEmpty() = ncol() == 0
+fun AnyFrame.isEmpty() = ncol == 0 || nrow == 0
+fun AnyRow.isNotEmpty() = !isEmpty()
+fun AnyFrame.isNotEmpty() = !isEmpty()
