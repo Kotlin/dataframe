17cf055e46a06b2a1710e42cb1a691f6bd23a72e
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-06-17 17:39:17 +0300

    Use 'Many' type when reading json arrays

--- src/main/kotlin/org/jetbrains/dataframe/api/aggregation/DataColumnAggregations.kt
+++ src/main/kotlin/org/jetbrains/dataframe/api/aggregation/DataColumnAggregations.kt
@@ -14,17 +14,35 @@ interface DataColumnAggregations<out T>: BaseColumn<T> {
 
 // region inlines
 
-fun <T: Number> DataColumn<T?>.mean(skipNa: Boolean = false): Double = Aggregators.mean(skipNa).aggregate(this) ?: Double.NaN
+// region min
+
+fun <T: Comparable<T>> DataColumn<T?>.min() = asSequence().filterNotNull().minOrNull()
+
+fun <T, R : Comparable<R>> DataColumn<T>.minBy(selector: (T) -> R) = values.asSequence().minByOrNull(selector)
+
+// endregion
+
+// region max
+
+fun <T: Comparable<T>> DataColumn<T?>.max() = asSequence().filterNotNull().maxOrNull()
+
+fun <T, R : Comparable<R>> DataColumn<T>.maxBy(selector: (T) -> R) = values.asSequence().maxByOrNull(selector)
+
+// endregion
+
+// region sum
+
+fun <T : Number> DataColumn<T>.sum() = values.sum(typeClass)
 
 inline fun <T, reified R : Number> DataColumn<T>.sumOf(crossinline expression: (T) -> R): R? =
     Aggregators.sum.cast<R>().of(this, expression)
 
-fun <T: Comparable<T>> DataColumn<T?>.min() = asSequence().filterNotNull().minOrNull()
-fun <T: Comparable<T>> DataColumn<T?>.max() = asSequence().filterNotNull().maxOrNull()
+// endregion
 
-fun <T : Number> DataColumn<T>.sum() = values.sum(typeClass)
+// region mean
 
-fun <T, R : Comparable<R>> DataColumn<T>.minBy(selector: (T) -> R) = values.asSequence().minByOrNull(selector)
-fun <T, R : Comparable<R>> DataColumn<T>.maxBy(selector: (T) -> R) = values.asSequence().maxByOrNull(selector)
+fun <T: Number> DataColumn<T?>.mean(skipNa: Boolean = false): Double = Aggregators.mean(skipNa).aggregate(this) ?: Double.NaN
+
+// endregion
 
 // endregion
