1dcf52fb83b2e813f65782dc0a62ae347dc1ea32
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-07-02 13:11:55 +0300

    Specify gradle plugin implementation in build file

---
+++ plugins/gradle/generate-data-schema/src/main/kotlin/org/jetbrains/dataframe/gradle/SchemaGeneratorPlugin.kt
@@ -0,0 +1,10 @@
+package org.jetbrains.dataframe.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SchemaGeneratorPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+
+    }
+}
