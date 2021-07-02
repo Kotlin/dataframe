7322aa7b783b377939a9bf053f565650f87a593a
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-07-02 13:11:55 +0300

    Specify gradle plugin implementation in build file

--- plugins/gradle/generate-data-schema/build.gradle.kts
+++ plugins/gradle/generate-data-schema/build.gradle.kts
@@ -8,6 +8,15 @@ repositories {
     maven(url="https://jitpack.io")
 }
 
+gradlePlugin {
+    plugins {
+        create("schemaGeneratorPlugin") {
+            id = "org.jetbrains.dataframe.schema-generator"
+            implementationClass = "org.jetbrains.dataframe.gradle.SchemaGeneratorPlugin"
+        }
+    }
+}
+
 dependencies {
     implementation("org.jetbrains.kotlinx:dataframe:0.7.3-dev-277-0.10.0.53")
 }
