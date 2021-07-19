cbec7b57bd1fbd3f480712baf44734fbafe785bc
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-07-19 13:58:24 +0300

    Make utility functions internal

--- plugins/gradle/generate-data-schema/src/main/kotlin/org/jetbrains/dataframe/gradle/ExtractFileName.kt
+++ plugins/gradle/generate-data-schema/src/main/kotlin/org/jetbrains/dataframe/gradle/ExtractFileName.kt
@@ -4,17 +4,17 @@ import java.io.File
 import java.net.MalformedURLException
 import java.net.URL
 
-fun extractFileName(url: URL): String? {
+internal fun extractFileName(url: URL): String? {
     return url.path.takeIf { it.isNotEmpty() }
         ?.substringAfterLast("/")
         ?.substringBeforeLast(".")
 }
 
-fun extractFileName(file: File): String {
+internal fun extractFileName(file: File): String {
     return file.nameWithoutExtension
 }
 
-fun extractFileName(path: String): String? {
+internal fun extractFileName(path: String): String? {
     return try {
         val url = URL(path)
         extractFileName(url)
