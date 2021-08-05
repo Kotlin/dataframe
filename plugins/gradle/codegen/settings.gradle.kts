62c9a5802f0c95c392c26b67f5161a7893778c1a
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-08-05 13:40:22 +0300

    Rename module schema-generator -> gradle-plugin

--- plugins/gradle/codegen/settings.gradle.kts
+++ plugins/gradle/codegen/settings.gradle.kts
@@ -5,5 +5,5 @@ includeBuild("../../../") {
         substitute(module("org.jetbrains.kotlinx:dataframe:0.7.3-dev-277-0.10.0.53")).using(project(":"))
     }
 }
-include(":generate-data-schema")
+include(":gradle-plugin")
 include(":symbol-processor")
