c4157d4b2ed4056541188339dd8b8a4cc58abeb2
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-08-27 14:26:03 +0300

    Remove google() repository as it is no longer required

--- plugins/gradle/codegen/symbol-processor/build.gradle.kts
+++ plugins/gradle/codegen/symbol-processor/build.gradle.kts
@@ -5,15 +5,12 @@ plugins {
 
 repositories {
     mavenCentral()
-    // TODO: Remove when kotlin-compile-testing update its KSP dependency to 1.5.21.1.0.0-beta07+
-    // since this version is published on the mavenCentral
-    google()
 }
 
 dependencies {
     implementation(libs.ksp.api)
     testImplementation(kotlin("test"))
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.2")
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.2")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.3")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.3")
     testImplementation("io.kotest:kotest-assertions-core:4.6.0")
 }
