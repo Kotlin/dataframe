7b6d9711a0f2ceaa5e577de0a1907947998c95bf
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-09-14 15:51:04 +0300

    Extract extensions code generation in base class to reuse in KSP

--- src/main/kotlin/org/jetbrains/dataframe/internal/codeGen/GeneratedField.kt
+++ src/main/kotlin/org/jetbrains/dataframe/internal/codeGen/GeneratedField.kt
@@ -3,12 +3,31 @@ package org.jetbrains.dataframe.internal.codeGen
 import org.jetbrains.dataframe.ColumnKind
 import org.jetbrains.dataframe.internal.schema.ColumnSchema
 
+public sealed interface ColumnInfo
+public class ValueColumn(public val typeFqName: String) : ColumnInfo
+public object FrameColumn : ColumnInfo
+public object ColumnGroup : ColumnInfo
+
+public interface BaseField {
+    public val fieldName: String
+    public val columnName: String
+    public val markerName: String?
+    public val nullable: Boolean
+    public val columnInfo: ColumnInfo
+}
+
 public data class GeneratedField(
-    val fieldName: String,
-    val columnName: String,
+    override val fieldName: String,
+    override val columnName: String,
     val overrides: Boolean,
     val columnSchema: ColumnSchema,
-    val markerName: String?
-) {
+    override val markerName: String?
+) : BaseField {
     val columnKind: ColumnKind get() = columnSchema.kind
+    override val nullable: Boolean = columnSchema.nullable
+    override val columnInfo: ColumnInfo = when (columnKind) {
+        ColumnKind.Value -> ValueColumn((columnSchema as ColumnSchema.Value).type.toString())
+        ColumnKind.Group -> ColumnGroup
+        ColumnKind.Frame -> FrameColumn
+    }
 }
