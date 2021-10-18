9c7ee0b4d3186113d6f5ca121651403fe81c1116
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-10-18 18:43:50 +0300

    Move refactored

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/DataFrameSchemaModification.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/DataFrameSchemaModification.kt
@@ -18,22 +18,19 @@ import org.jetbrains.kotlinx.dataframe.columns.ColumnPath
 import org.jetbrains.kotlinx.dataframe.columns.ColumnReference
 import org.jetbrains.kotlinx.dataframe.columns.ColumnWithPath
 import org.jetbrains.kotlinx.dataframe.columns.SingleColumn
-import org.jetbrains.kotlinx.dataframe.columns.UnresolvedColumnsPolicy
 import org.jetbrains.kotlinx.dataframe.columns.size
 import org.jetbrains.kotlinx.dataframe.dataFrameOf
-import org.jetbrains.kotlinx.dataframe.impl.DataFrameReceiver
 import org.jetbrains.kotlinx.dataframe.impl.DataRowImpl
 import org.jetbrains.kotlinx.dataframe.impl.api.ColumnToInsert
+import org.jetbrains.kotlinx.dataframe.impl.api.MoveDslImpl
+import org.jetbrains.kotlinx.dataframe.impl.api.afterOrBefore
 import org.jetbrains.kotlinx.dataframe.impl.api.flattenImpl
 import org.jetbrains.kotlinx.dataframe.impl.api.insertImpl
+import org.jetbrains.kotlinx.dataframe.impl.api.moveInto
+import org.jetbrains.kotlinx.dataframe.impl.api.moveTo
 import org.jetbrains.kotlinx.dataframe.impl.api.removeImpl
-import org.jetbrains.kotlinx.dataframe.impl.columns.resolveSingle
 import org.jetbrains.kotlinx.dataframe.impl.columns.toColumnSet
-import org.jetbrains.kotlinx.dataframe.impl.columns.toColumnWithPath
 import org.jetbrains.kotlinx.dataframe.impl.columns.toColumns
-import org.jetbrains.kotlinx.dataframe.impl.columns.tree.ColumnPosition
-import org.jetbrains.kotlinx.dataframe.impl.columns.tree.TreeNode
-import org.jetbrains.kotlinx.dataframe.impl.columns.tree.getOrPut
 import org.jetbrains.kotlinx.dataframe.impl.removeAt
 import org.jetbrains.kotlinx.dataframe.impl.toColumnPath
 import org.jetbrains.kotlinx.dataframe.index
@@ -202,15 +199,12 @@ public fun <T, C> ReplaceCause<T, C>.with(transform: DataFrameBase<T>.(DataColum
 
 // region move
 
-public fun <T, C> DataFrame<T>.move(cols: Iterable<ColumnReference<C>>): MoveColsClause<T, C> = move { cols.toColumnSet() }
-public fun <T, C> DataFrame<T>.move(vararg cols: ColumnReference<C>): MoveColsClause<T, C> = move { cols.toColumns() }
-public fun <T> DataFrame<T>.move(vararg cols: String): MoveColsClause<T, Any?> = move { cols.toColumns() }
-public fun <T> DataFrame<T>.move(vararg cols: ColumnPath): MoveColsClause<T, Any?> = move { cols.toColumns() }
-public fun <T, C> DataFrame<T>.move(vararg cols: KProperty<C>): MoveColsClause<T, C> = move { cols.toColumns() }
-public fun <T, C> DataFrame<T>.move(selector: ColumnsSelector<T, C>): MoveColsClause<T, C> {
-    val (df, removed) = removeImpl(selector)
-    return MoveColsClause(df, this, removed)
-}
+public fun <T, C> DataFrame<T>.move(cols: Iterable<ColumnReference<C>>): MoveClause<T, C> = move { cols.toColumnSet() }
+public fun <T, C> DataFrame<T>.move(vararg cols: ColumnReference<C>): MoveClause<T, C> = move { cols.toColumns() }
+public fun <T> DataFrame<T>.move(vararg cols: String): MoveClause<T, Any?> = move { cols.toColumns() }
+public fun <T> DataFrame<T>.move(vararg cols: ColumnPath): MoveClause<T, Any?> = move { cols.toColumns() }
+public fun <T, C> DataFrame<T>.move(vararg cols: KProperty<C>): MoveClause<T, C> = move { cols.toColumns() }
+public fun <T, C> DataFrame<T>.move(selector: ColumnsSelector<T, C>): MoveClause<T, C> = MoveClause(this, selector)
 
 public fun <T> DataFrame<T>.moveTo(newColumnIndex: Int, selector: ColumnsSelector<T, *>): DataFrame<T> = move(selector).to(newColumnIndex)
 public fun <T> DataFrame<T>.moveTo(newColumnIndex: Int, cols: Iterable<Column>): DataFrame<T> = moveTo(newColumnIndex) { cols.toColumnSet() }
@@ -230,7 +224,7 @@ public fun <T> DataFrame<T>.moveToRight(vararg cols: String): DataFrame<T> = mov
 public fun <T> DataFrame<T>.moveToRight(vararg cols: Column): DataFrame<T> = moveToRight { cols.toColumns() }
 public fun <T> DataFrame<T>.moveToRight(vararg cols: KProperty<*>): DataFrame<T> = moveToRight { cols.toColumns() }
 
-public interface DataFrameForMove<T> : DataFrameBase<T> {
+public interface MoveDsl<T> : DataFrameBase<T> {
 
     public fun path(vararg columns: String): ColumnPath = pathOf(*columns)
 
@@ -239,29 +233,18 @@ public interface DataFrameForMove<T> : DataFrameBase<T> {
     public operator fun SingleColumn<*>.plus(column: String): ColumnPath = addPath(pathOf(column))
 }
 
-internal class MoveReceiver<T>(df: DataFrame<T>) : DataFrameReceiver<T>(df, false), DataFrameForMove<T> {
-
-    override fun SingleColumn<*>.addPath(columnPath: ColumnPath): ColumnPath {
-        return this.resolveSingle(this@MoveReceiver, UnresolvedColumnsPolicy.Create)!!.path + columnPath
-    }
+public fun <T, C> MoveClause<T, C>.under(parentPath: MoveDsl<T>.(ColumnWithPath<C>) -> ColumnPath): DataFrame<T> {
+    val receiver = MoveDslImpl(df)
+    return into { parentPath(receiver, it) + it.name }
 }
 
-public fun <T, C> MoveColsClause<T, C>.under(parentPath: DataFrameForMove<T>.(ColumnWithPath<C>) -> ColumnPath): DataFrame<T> {
-    val receiver = MoveReceiver(df)
-    val columnsToInsert = removed.map {
-        val col = it.column
-        ColumnToInsert(parentPath(receiver, col) + it.name, col.data, it)
-    }
-    return df.insertImpl(columnsToInsert)
-}
-
-public fun <T, C> MoveColsClause<T, C>.toTop(
-    groupNameExpression: DataFrameForMove<T>.(ColumnWithPath<C>) -> String = { it.name() }
+public fun <T, C> MoveClause<T, C>.toTop(
+    groupNameExpression: MoveDsl<T>.(ColumnWithPath<C>) -> String = { it.name() }
 ): DataFrame<T> =
     into { pathOf(groupNameExpression(it)) }
 
-public fun <T, C> MoveColsClause<T, C>.intoIndexed(
-    newPathExpression: DataFrameForMove<T>.(ColumnWithPath<C>, Int) -> ColumnPath
+public fun <T, C> MoveClause<T, C>.intoIndexed(
+    newPathExpression: MoveDsl<T>.(ColumnWithPath<C>, Int) -> ColumnPath
 ): DataFrame<T> {
     var counter = 0
     return into { col ->
@@ -269,33 +252,24 @@ public fun <T, C> MoveColsClause<T, C>.intoIndexed(
     }
 }
 
-public fun <T, C> MoveColsClause<T, C>.into(newPathExpression: DataFrameForMove<T>.(ColumnWithPath<C>) -> ColumnPath): DataFrame<T> {
-    val receiver = MoveReceiver(df)
-    val columnsToInsert = removed.map {
-        val col = it.column
-        ColumnToInsert(newPathExpression(receiver, col), col.data, it)
-    }
-    return df.insertImpl(columnsToInsert)
-}
+public fun <T, C> MoveClause<T, C>.into(newPathExpression: MoveDsl<T>.(ColumnWithPath<C>) -> ColumnPath): DataFrame<T> = moveInto(newPathExpression)
 
-public fun <T, C> MoveColsClause<T, C>.into(vararg path: String): DataFrame<T> = into(path.toColumnPath())
-public fun <T, C> MoveColsClause<T, C>.into(path: ColumnPath): DataFrame<T> = into { path }
+public fun <T, C> MoveClause<T, C>.into(vararg path: String): DataFrame<T> = into(path.toColumnPath())
+public fun <T, C> MoveClause<T, C>.into(path: ColumnPath): DataFrame<T> = into { path }
 
-public fun <T, C> MoveColsClause<T, C>.under(vararg path: String): DataFrame<T> = under(path.toColumnPath())
-public fun <T, C> MoveColsClause<T, C>.under(path: ColumnPath): DataFrame<T> = under { path }
-public fun <T, C> MoveColsClause<T, C>.under(groupRef: ColumnGroupReference): DataFrame<T> = under(groupRef.path())
+public fun <T, C> MoveClause<T, C>.under(vararg path: String): DataFrame<T> = under(path.toColumnPath())
+public fun <T, C> MoveClause<T, C>.under(path: ColumnPath): DataFrame<T> = under { path }
+public fun <T, C> MoveClause<T, C>.under(groupRef: ColumnGroupReference): DataFrame<T> = under(groupRef.path())
 
-public fun <T, C> MoveColsClause<T, C>.to(columnIndex: Int): DataFrame<T> {
-    val newColumnList = df.columns().subList(0, columnIndex) + removed.map { it.data.column as DataColumn<C> } + df.columns().subList(columnIndex, df.ncol())
-    return newColumnList.toDataFrame()
-}
+public fun <T, C> MoveClause<T, C>.to(columnIndex: Int): DataFrame<T> = moveTo(columnIndex)
 
-public fun <T, C> MoveColsClause<T, C>.after(columnPath: ColumnPath): DataFrame<T> = after { columnPath.toColumnAccessor() }
-public fun <T, C> MoveColsClause<T, C>.after(column: Column): DataFrame<T> = after { column }
-public fun <T, C> MoveColsClause<T, C>.after(column: KProperty<*>): DataFrame<T> = after { column.toColumnAccessor() }
-public fun <T, C> MoveColsClause<T, C>.after(column: String): DataFrame<T> = after { column.toColumnAccessor() }
-public fun <T, C> MoveColsClause<T, C>.after(column: ColumnSelector<T, *>): DataFrame<T> = afterOrBefore(column, true)
+public fun <T, C> MoveClause<T, C>.after(columnPath: ColumnPath): DataFrame<T> = after { columnPath.toColumnAccessor() }
+public fun <T, C> MoveClause<T, C>.after(column: Column): DataFrame<T> = after { column }
+public fun <T, C> MoveClause<T, C>.after(column: KProperty<*>): DataFrame<T> = after { column.toColumnAccessor() }
+public fun <T, C> MoveClause<T, C>.after(column: String): DataFrame<T> = after { column.toColumnAccessor() }
+public fun <T, C> MoveClause<T, C>.after(column: ColumnSelector<T, *>): DataFrame<T> = afterOrBefore(column, true)
 
+// TODO: implement 'before'
 /*
 fun <T, C> MoveColsClause<T, C>.before(columnPath: ColumnPath) = before { columnPath.toColumnDef() }
 fun <T, C> MoveColsClause<T, C>.before(column: Column) = before { column }
@@ -304,37 +278,10 @@ fun <T, C> MoveColsClause<T, C>.before(column: String) = before { column.toColum
 fun <T, C> MoveColsClause<T, C>.before(column: ColumnSelector<T, *>) = afterOrBefore(column, false)
 */
 
-// TODO: support 'before' mode
-public fun <T, C> MoveColsClause<T, C>.afterOrBefore(column: ColumnSelector<T, *>, isAfter: Boolean): DataFrame<T> {
-    val refPath = originalDf.getColumnWithPath(column).path
-    val removeRoot = removed.first().getRoot()
+public fun <T, C> MoveClause<T, C>.toLeft(): DataFrame<T> = to(0)
+public fun <T, C> MoveClause<T, C>.toRight(): DataFrame<T> = to(df.ncol())
 
-    val refNode = removeRoot.getOrPut(refPath) {
-        val parentPath = it.dropLast(1)
-        val parent = if (parentPath.isEmpty()) originalDf else originalDf.getColumnGroup(parentPath)
-        val index = parent.getColumnIndex(it.last())
-        val col = df.col(index)
-        ColumnPosition(index, false, col)
-    }
-
-    val parentPath = refPath.dropLast(1)
-    val toInsert = removed.map {
-        val path = parentPath + it.name
-        ColumnToInsert(path, it.column.data, refNode)
-    }
-    return df.insertImpl(toInsert)
-}
-
-public fun <T, C> MoveColsClause<T, C>.toLeft(): DataFrame<T> = to(0)
-public fun <T, C> MoveColsClause<T, C>.toRight(): DataFrame<T> = to(df.ncol())
-public class MoveColsClause<T, C> internal constructor(
-    internal val df: DataFrame<T>,
-    internal val originalDf: DataFrame<T>,
-    internal val removed: List<TreeNode<ColumnPosition>>
-) {
-
-    internal val TreeNode<ColumnPosition>.column get() = toColumnWithPath<C>(originalDf)
-}
+public class MoveClause<T, C>(internal val df: DataFrame<T>, internal val selector: ColumnsSelector<T, C>)
 
 // endregion
 
