4eea4e36533a1f7406ef881e933cb9fe34fdd0a4
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-10-19 16:43:59 +0300

    Update KSP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ include("plugins:symbol-processor")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("ksp", "1.5.21-1.0.0-beta07")
+            version("ksp", "1.5.30-1.0.0")
             alias("ksp-gradle").to("com.google.devtools.ksp", "symbol-processing-gradle-plugin").versionRef("ksp")
             alias("ksp-api").to("com.google.devtools.ksp", "symbol-processing-api").versionRef("ksp")
         }
