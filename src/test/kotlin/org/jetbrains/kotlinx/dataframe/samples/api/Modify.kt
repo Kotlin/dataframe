cc2f30859be04128b6df711e4031035463527671
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-10-29 20:17:02 +0300

    Add sample for 'convert'

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Modify.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Modify.kt
@@ -1,15 +1,19 @@
 package org.jetbrains.kotlinx.dataframe.samples.api
 
 import org.jetbrains.kotlinx.dataframe.api.at
+import org.jetbrains.kotlinx.dataframe.api.convert
 import org.jetbrains.kotlinx.dataframe.api.dfsOf
 import org.jetbrains.kotlinx.dataframe.api.movingAverage
 import org.jetbrains.kotlinx.dataframe.api.notNull
+import org.jetbrains.kotlinx.dataframe.api.print
+import org.jetbrains.kotlinx.dataframe.api.toMany
 import org.jetbrains.kotlinx.dataframe.api.update
 import org.jetbrains.kotlinx.dataframe.api.where
 import org.jetbrains.kotlinx.dataframe.api.with
-import org.jetbrains.kotlinx.dataframe.api.withValue
 import org.jetbrains.kotlinx.dataframe.api.withNull
+import org.jetbrains.kotlinx.dataframe.api.withValue
 import org.junit.Test
+import kotlin.streams.toList
 
 class Modify : TestBase() {
 
@@ -18,6 +22,18 @@ class Modify : TestBase() {
         // SampleStart
         df.update { age }.with { it * 2 }
         df.update { dfsOf<String>() }.with { it.uppercase() }
+        df.update { city }.where { name.firstName == "Alice" }.withValue("Paris")
+        df.update { weight }.at(1..4).notNull { it / 2 }
+        df.update { name.lastName and age }.at(1,3,4).withNull()
+        df.update { age }.with { movingAverage(2) { age }.toInt() }
+        // SampleEnd
+    }
+
+    @Test
+    fun convert() {
+        // SampleStart
+        df.convert { age }.with { it.toDouble() }
+        df.convert { dfsOf<String>() }.with { it.toCharArray().toList() }.print()
         df.update { city }.where { name.firstName == "Alice" }.withValue("Paris")
         df.update { weight }.at(1..4).notNull { it / 2 }
         df.update { name.lastName and age }.at(1,3,4).withNull()
