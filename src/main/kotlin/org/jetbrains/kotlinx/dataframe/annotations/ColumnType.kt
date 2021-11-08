ce3f79eb1b6252a21740d55b16c21ef831f74fbe
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-11-08 13:38:22 +0300

    Remove ColumnType annotation

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/annotations/ColumnType.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.kotlinx.dataframe.annotations
-
-import org.jetbrains.kotlinx.dataframe.AnyCol
-import kotlin.reflect.KClass
-
-@Target(AnnotationTarget.PROPERTY)
-public annotation class ColumnType(val type: KClass<out AnyCol>)
