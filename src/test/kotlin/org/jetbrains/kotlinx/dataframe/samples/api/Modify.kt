58de24aae1614a51482bb8f8407285ba68b9421b
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-11 13:48:59 +0300

    Update docs for `move`

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Modify.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Modify.kt
@@ -156,27 +156,28 @@ class Modify : TestBase() {
 
         df.move { weight }.to(1)
 
-        // name -> info.name
-        df.move { age }.into { pathOf("info", it.name) }
-
-        // firstName -> fullName.firstName
-        // lastName -> fullName.lastName
+        // age -> info.age
+        // weight -> info.weight
+        df.move { age and weight }.into { pathOf("info", it.name) }
+        df.move { age and weight }.into { "info"[it.name] }
         df.move { age and weight }.under("info")
 
         // name.firstName -> fullName.first
         // name.lastName -> fullName.last
         df.move { name.firstName and name.lastName }.into { pathOf("fullName", it.name.dropLast(4)) }
 
-        dataFrameOf("a.b.c", "a.d.e")(1, 2)
-            .move { all() }.into { it.name.split(".").toPath() }
+        // a|b|c -> a.b.c
+        // a|d|e -> a.d.e
+        dataFrameOf("a|b|c", "a|d|e")(0, 0)
+            .move { all() }.into { it.name.split("|").toPath() }
 
         // name.firstName -> firstName
         // name.lastName -> lastName
         df.move { name.cols() }.toTop()
 
-        // group1.default.name -> defaultData
-        // group2.field.name -> fieldData
-        df.move { dfs { it.name == "data" } }.toTop { it.parent!!.name + "Data" }
+        // a.b.e -> be
+        // c.d.e -> de
+        df.move { dfs { it.name == "e" } }.toTop { it.parent!!.name + it.name }
         // SampleEnd
     }
 
