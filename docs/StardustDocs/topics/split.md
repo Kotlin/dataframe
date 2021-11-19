8e109efe56e51986952fb4f315475d598a2bd7e8
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-19 18:18:40 +0300

    Update `split into columns` sample

--- docs/StardustDocs/topics/split.md
+++ docs/StardustDocs/topics/split.md
@@ -91,13 +91,14 @@ merged.split { name }
 <!---FUN splitFrameColumn-->
 
 ```kotlin
-val df1 = dataFrameOf("a", "b")(
-    1, 2,
-    3, 4
+val df1 = dataFrameOf("a", "b", "c")(
+    1, 2, 3,
+    4, 5, 6
 )
 val df2 = dataFrameOf("a", "b")(
     5, 6,
-    7, 8
+    7, 8,
+    9, 10
 )
 val group by columnOf(df1, df2)
 val id by columnOf("x", "y")
--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Modify.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Modify.kt
@@ -364,13 +364,14 @@ class Modify : TestBase() {
     @Test
     fun splitFrameColumn() {
         // SampleStart
-        val df1 = dataFrameOf("a", "b")(
-            1, 2,
-            3, 4
+        val df1 = dataFrameOf("a", "b", "c")(
+            1, 2, 3,
+            4, 5, 6
         )
         val df2 = dataFrameOf("a", "b")(
             5, 6,
-            7, 8
+            7, 8,
+            9, 10
         )
         val group by columnOf(df1, df2)
         val id by columnOf("x", "y")
