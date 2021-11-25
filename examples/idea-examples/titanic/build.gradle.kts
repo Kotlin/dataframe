e84aa86f66c562aeea8feae8a9c21d57d89154e0
devcrocod <devcrocod@gmail.com>
2021-11-25 13:22:03 +0300

    add idea titanic example

---
+++ examples/idea-examples/titanic/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm")
+}
+
+repositories {
+    mavenCentral()
+}
+
+application.mainClass.set("samples.ml.TitanicKt")
+
+dependencies {
+    implementation(project(":"))
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-api:0.3.0")
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-dataset:0.3.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
