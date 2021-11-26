2c8825d4cd70e60c5ee6f352749295bad6a1e2c8
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-27 00:31:16 +0300

    Move `movies` example into `org.jetbrains.kotlinx.dataframe` package. Add example with interface and extension properties generation.

--- examples/idea-examples/movies/src/main/kotlin/org/jetbrains/kotlinx/dataframe/examples/movies/moviesWithColumnAccessor.kt
+++ examples/idea-examples/movies/src/main/kotlin/org/jetbrains/kotlinx/dataframe/examples/movies/moviesWithColumnAccessor.kt
@@ -1,31 +1,28 @@
-package samples.movies
+package org.jetbrains.kotlinx.dataframe.examples.movies
 
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.*
 import org.jetbrains.kotlinx.dataframe.column
 import org.jetbrains.kotlinx.dataframe.io.read
 
-private const val pathToCsv = "examples/idea-examples/movies/src/main/resources/ml-latest/movies.csv"
+private const val pathToCsv = "examples/idea-examples/movies/src/main/resources/movies.csv"
 
 fun main() {
     val genres by column<String>()
     val title by column<String>()
+    val year by column<Int>()
 
-    val df = DataFrame
+    DataFrame
         .read(pathToCsv)
         .split { genres }.by("|").inplace()
         .split { title }.by {
             listOf(
-                "\\s*\\(\\d{4}\\)\\s*$".toRegex().replace(it, ""),
+                """\s*\(\d{4}\)\s*$""".toRegex().replace(it, ""),
                 "\\d{4}".toRegex().findAll(it).lastOrNull()?.value?.toIntOrNull() ?: -1
             )
-        }.into("title", "year")
-
-    val year by column<Int>()
-
-    df
+        }.into(title, year)
         .explode { genres }
-        .filter { year >= 0 && it[genres] != "(no genres listed)" }
+        .filter { year() >= 0 && genres() != "(no genres listed)" }
         .groupBy { year }
         .sortBy { year }
         .pivot(inward = false) { genres }
--- examples/idea-examples/movies/src/main/kotlin/org/jetbrains/kotlinx/dataframe/examples/movies/moviesWithInterface.kt
+++ examples/idea-examples/movies/src/main/kotlin/org/jetbrains/kotlinx/dataframe/examples/movies/moviesWithInterface.kt
@@ -1,28 +1,31 @@
-package samples.movies
+package org.jetbrains.kotlinx.dataframe.examples.movies
 
 import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
 import org.jetbrains.kotlinx.dataframe.api.*
 import org.jetbrains.kotlinx.dataframe.io.read
 
-data class Movie(val movieId: String, val title: String, val genres: String)
+@DataSchema
+interface Movie{
+    val movieId: String
+    val title: String
+    val genres: String
+}
 
-private const val pathToCsv = "examples/idea-examples/movies/src/main/resources/ml-latest/movies.csv"
+private const val pathToCsv = "examples/idea-examples/movies/src/main/resources/movies.csv"
 
 fun main() {
-    val df1 = DataFrame
+    DataFrame
         .read(pathToCsv).convertTo<Movie>()
-        .split(Movie::genres).by("|").inplace()
-        .split(Movie::title).by {
+        .split { genres }.by("|").inplace()
+        .split { title }.by {
             listOf(
-                "\\s*\\(\\d{4}\\)\\s*$".toRegex().replace(it, ""),
+                """\s*\(\d{4}\)\s*$""".toRegex().replace(it, ""),
                 "\\d{4}".toRegex().findAll(it).lastOrNull()?.value?.toIntOrNull() ?: -1
             )
         }.into("title", "year")
-
-
-    df1
         .explode("genres")
-        .filter { "year"<Int>() >= 0 && it[Movie::genres] != "(no genres listed)" }
+        .filter { "year"<Int>() >= 0 && genres != "(no genres listed)" }
         .groupBy("year")
         .sortBy("year")
         .pivot("genres", inward = false)
