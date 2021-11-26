34bc97b075868563d8d03e168d381a7f9eb00f9a
devcrocod <devcrocod@gmail.com>
2021-11-26 20:28:39 +0300

    fix write json for primitive types

---
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/io/JsonTests.kt
@@ -0,0 +1,21 @@
+package org.jetbrains.kotlinx.dataframe.io
+
+import io.kotest.matchers.shouldBe
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.dataFrameOf
+import org.junit.Test
+
+class JsonTests {
+
+    @Test
+    fun `write df with primitive types`() {
+        val df = dataFrameOf("colInt", "colDouble?", "colBoolean?")(
+            1, 1.0, true,
+            2, null, false,
+            3, 3.0, null
+        )
+
+        val res = DataFrame.readJsonStr(df.writeJsonStr())
+        res shouldBe df
+    }
+}
