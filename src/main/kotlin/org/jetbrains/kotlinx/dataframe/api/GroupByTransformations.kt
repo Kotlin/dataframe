633367049870cc5d447c90a7bc897705e8caf19c
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-27 16:18:35 +0300

    Add `pivotCount` and `pivotMatches` for `groupBy` and `groupBy.aggregate`

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/GroupByTransformations.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/GroupByTransformations.kt
@@ -11,24 +11,7 @@ import org.jetbrains.kotlinx.dataframe.impl.aggregation.PivotGroupByImpl
 import org.jetbrains.kotlinx.dataframe.impl.columns.toColumns
 import kotlin.reflect.KProperty
 
-// region pivot GroupBy
 
-public fun <G> GroupBy<*, G>.pivot(inward: Boolean? = null, columns: ColumnsSelector<G, *>): PivotGroupBy<G> = PivotGroupByImpl(this, columns, inward)
-public fun <G> GroupBy<*, G>.pivot(vararg columns: Column, inward: Boolean? = null): PivotGroupBy<G> = pivot(inward) { columns.toColumns() }
-public fun <G> GroupBy<*, G>.pivot(vararg columns: String, inward: Boolean? = null): PivotGroupBy<G> = pivot(inward) { columns.toColumns() }
-public fun <G> GroupBy<*, G>.pivot(vararg columns: KProperty<*>, inward: Boolean? = null): PivotGroupBy<G> = pivot(inward) { columns.toColumns() }
-
-// endregion
-
-// region pivot inside GroupBy aggregate
-
-public fun <T> AggregateGroupedDsl<T>.pivot(inward: Boolean? = null, columns: ColumnsSelector<T, *>): PivotGroupBy<T> =
-    GroupByAggregatePivotImpl(this, columns, inward)
-public fun <T> AggregateGroupedDsl<T>.pivot(vararg columns: String, inward: Boolean? = null): PivotGroupBy<T> = pivot(inward) { columns.toColumns() }
-public fun <T> AggregateGroupedDsl<T>.pivot(vararg columns: Column, inward: Boolean? = null): PivotGroupBy<T> = pivot(inward) { columns.toColumns() }
-public fun <T> AggregateGroupedDsl<T>.pivot(vararg columns: KProperty<*>, inward: Boolean? = null): PivotGroupBy<T> = pivot(inward) { columns.toColumns() }
-
-// endregion
 
 public fun <T, G> GroupBy<T, G>.into(column: String): DataFrame<T> = toDataFrame(column)
 public fun <T> GroupBy<T, *>.into(column: ColumnAccessor<AnyFrame>): DataFrame<T> = toDataFrame(column.name())
