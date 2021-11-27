9919916d0339f87253ab039124d337e0de768fa8
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-28 02:00:43 +0300

    Fix bugs in `flatten`. Remove separator from `flatten`.

---
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/flatten.kt
@@ -0,0 +1,41 @@
+package org.jetbrains.kotlinx.dataframe.api
+
+import io.kotest.matchers.shouldBe
+import org.jetbrains.kotlinx.dataframe.dataFrameOf
+import org.junit.Test
+
+class FlattenTests {
+
+    @Test
+    fun `flatten names`() {
+        val df = dataFrameOf("a", "b", "c")(1, 2, 3)
+        val grouped = df.group("a", "b").into("d")
+        grouped.flatten() shouldBe df
+        grouped.add("a") { 0 }.flatten().columnNames() shouldBe listOf("a1", "b", "c", "a")
+    }
+
+    @Test
+    fun `flatten nested`() {
+        val df = dataFrameOf("a", "b", "c", "d")(1, 2, 3, 4)
+        val grouped = df.group("a", "b").into("e")
+            .group("e", "c").into("f")
+
+        grouped.flatten() shouldBe df
+        val flattened = grouped.flatten { "f"["e"] }
+        flattened.columnNames() shouldBe listOf("f", "d")
+        flattened.ungroup("f") shouldBe df
+
+        grouped.flatten { "f"["e"] and "f" } shouldBe df
+    }
+
+    @Test
+    fun `flatten with parent name conflict`() {
+        val df = dataFrameOf("a", "b", "c", "d")(1, 2, 3, 4)
+        val grouped = df.group("a", "b").into("e")
+            .group("e", "c").into("f")
+            .rename { "f"["e"] }.into("a")
+        val flattened = grouped.flatten { "f"["a"] }
+        flattened.getColumnGroup("f").columnNames() shouldBe listOf("a", "b", "c")
+        flattened.ungroup("f") shouldBe df
+    }
+}
