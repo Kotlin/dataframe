cd0b2e1357a60cf0e208f67a05746ea193e819fb
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-11-28 05:02:18 +0300

    Use new API from testkit

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/jupyter/SampleNotebooksTests.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/jupyter/SampleNotebooksTests.kt
@@ -1,9 +1,7 @@
 package org.jetbrains.kotlinx.dataframe.jupyter
 
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.serializer
+import org.jetbrains.kotlinx.jupyter.testkit.notebook.JupyterNotebookParser
+import org.jetbrains.kotlinx.jupyter.testkit.notebook.JupyterOutput
 import org.junit.Ignore
 import org.junit.Test
 import java.io.File
@@ -53,7 +51,7 @@ class SampleNotebooksTests : DataFrameJupyterTest() {
         cleanup: () -> Unit = {}
     ) {
         val notebookFile = File(notebookPath)
-        val notebook = parser.decodeFromString(serializer<JupyterNotebook>(), notebookFile.readText())
+        val notebook = JupyterNotebookParser.parse(notebookFile)
         val codeCellsData = notebook.cells.mapNotNull {
             if (it.cell_type == "code") {
                 CodeCellData(it.source.joinToString(""), it.outputs)
@@ -89,31 +87,7 @@ class SampleNotebooksTests : DataFrameJupyterTest() {
         val outputs: List<JupyterOutput>,
     )
 
-    @Serializable
-    data class JupyterNotebook(
-        val cells: List<JupyterCell>,
-        val metadata: JsonObject? = null,
-    )
-
-    @Serializable
-    data class JupyterCell(
-        val cell_type: String,
-        val id: String? = null,
-        val metadata: JsonObject? = null,
-        val source: List<String> = emptyList(),
-        val outputs: List<JupyterOutput> = emptyList(),
-    )
-
-    @Serializable
-    data class JupyterOutput(
-        val data: JsonObject? = null,
-        val execution_count: Int? = null,
-        val metadata: JsonObject? = null,
-        val output_type: String,
-    )
-
     companion object {
-        val parser = Json { ignoreUnknownKeys = true }
         const val jupyterExamplesPath = "examples/jupyter-notebooks"
 
         fun testFile(folder: String, fileName: String) = fileName to "$jupyterExamplesPath/$folder/$fileName"
