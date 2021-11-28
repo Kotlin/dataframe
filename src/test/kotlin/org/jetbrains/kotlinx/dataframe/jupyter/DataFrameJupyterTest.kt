0ffdbeb5ec86c24a3726d5a9ab99c295523bad43
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-11-28 05:02:18 +0300

    Use new API from testkit

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/jupyter/DataFrameJupyterTest.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/jupyter/DataFrameJupyterTest.kt
@@ -1,49 +1,11 @@
 package org.jetbrains.kotlinx.dataframe.jupyter
 
-import org.jetbrains.kotlinx.jupyter.ReplForJupyterImpl
-import org.jetbrains.kotlinx.jupyter.api.libraries.LibraryDefinition
-import org.jetbrains.kotlinx.jupyter.api.libraries.LibraryReference
-import org.jetbrains.kotlinx.jupyter.api.libraries.Variable
-import org.jetbrains.kotlinx.jupyter.api.libraries.libraryDefinition
-import org.jetbrains.kotlinx.jupyter.defaultRepositories
-import org.jetbrains.kotlinx.jupyter.dependencies.ResolverConfig
-import org.jetbrains.kotlinx.jupyter.libraries.ChainedLibraryResolver
-import org.jetbrains.kotlinx.jupyter.libraries.EmptyResolutionInfoProvider
-import org.jetbrains.kotlinx.jupyter.libraries.LibraryResolver
-import org.jetbrains.kotlinx.jupyter.testkit.ClasspathLibraryResolver
 import org.jetbrains.kotlinx.jupyter.testkit.JupyterReplTestCase
 import org.jetbrains.kotlinx.jupyter.testkit.ReplProvider
 
-typealias LibraryFilter = (String?) -> Boolean
-
-private val specialLibraries = listOf("dataframe")
-private val specialLibrariesFilter: LibraryFilter = { it in specialLibraries }
-private val emptyLibraryDefinition = libraryDefinition {}
-
-class ToEmptyResolver(parent: LibraryResolver?, private val resolveToEmpty: LibraryFilter) : ChainedLibraryResolver(parent) {
-    override fun tryResolve(reference: LibraryReference, arguments: List<Variable>): LibraryDefinition? {
-        return if (resolveToEmpty(reference.name)) return emptyLibraryDefinition else null
-    }
-}
-
-private val testLibraryResolver = run {
-    var res: LibraryResolver = ClasspathLibraryResolver(null) { !specialLibrariesFilter(it) }
-    res = ToEmptyResolver(res, specialLibrariesFilter)
-    res
-}
-
-private val testReplProvider = ReplProvider { classpath ->
-    ReplForJupyterImpl(
-        EmptyResolutionInfoProvider,
-        classpath,
-        isEmbedded = true,
-        resolverConfig = ResolverConfig(defaultRepositories, testLibraryResolver)
-    ).apply {
-        eval { librariesScanner.addLibrariesFromClassLoader(ToEmptyResolver::class.java.classLoader, this) }
-    }
-}
-
-abstract class DataFrameJupyterTest : JupyterReplTestCase(testReplProvider)
+abstract class DataFrameJupyterTest : JupyterReplTestCase(
+    ReplProvider.forLibrariesTesting(listOf("dataframe"))
+)
 
 fun interface CodeReplacer {
     fun replace(code: String): String
