68d9fc3f1f556f64cf357d14124aff504cefba88
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-30 00:25:35 +0300

    Formatting

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/Nulls.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/Nulls.kt
@@ -7,7 +7,6 @@ import org.jetbrains.kotlinx.dataframe.Column
 import org.jetbrains.kotlinx.dataframe.ColumnsSelector
 import org.jetbrains.kotlinx.dataframe.DataColumn
 import org.jetbrains.kotlinx.dataframe.DataFrame
-import org.jetbrains.kotlinx.dataframe.DataRow
 import org.jetbrains.kotlinx.dataframe.columns.ColumnKind
 import org.jetbrains.kotlinx.dataframe.columns.ColumnReference
 import org.jetbrains.kotlinx.dataframe.impl.columns.toColumnSet
