a2bab4783825a69e700b7d61ef1d82e1b1e0fc0c
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-30 00:25:26 +0300

    Fix bug in `createDataFrame`: handle exceptions thrown in property reading.

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/createDataFrame.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/createDataFrame.kt
@@ -2,12 +2,13 @@ package org.jetbrains.kotlinx.dataframe.api
 
 import io.kotest.matchers.shouldBe
 import org.jetbrains.kotlinx.dataframe.dataFrameOf
+import org.jetbrains.kotlinx.dataframe.impl.getType
 import org.junit.Test
 
 class CreateDataFrameTests {
 
     @Test
-    public fun `visibility test`() {
+    fun `visibility test`() {
         class Data {
             private val a = 1
             protected val b = 2
@@ -16,5 +17,20 @@ class CreateDataFrameTests {
         }
 
         listOf(Data()).createDataFrame() shouldBe dataFrameOf("d")(4)
+    }
+
+    @Test
+    fun `exception test`() {
+        class Data {
+            val a: Int get() = error("Error")
+            val b = 1
+        }
+
+        val df = listOf(Data()).createDataFrame()
+        df.ncol() shouldBe 2
+        df.nrow() shouldBe 1
+        df.columnTypes() shouldBe listOf(getType<IllegalStateException>(), getType<Int>())
+        (df["a"][0] is IllegalStateException) shouldBe true
+        df["b"][0] shouldBe 1
     }
 }
