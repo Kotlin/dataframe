27e86801f361858ddf0e681a6e0553cf7c97b890
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-29 22:23:44 +0300

    Add missing `mean` overloads for PivotGroupBy

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Repository.Generated.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Repository.Generated.kt
@@ -1,7 +1,7 @@
 @file:Suppress(
-    "RemoveRedundantBackticks",
-    "RemoveRedundantQualifierName",
-    "unused", "ObjectPropertyName",
+    "RemoveRedundantBackticks", 
+    "RemoveRedundantQualifierName", 
+    "unused", "ObjectPropertyName", 
     "UNCHECKED_CAST", "PropertyName",
     "ClassName"
 )
@@ -11,12 +11,12 @@ import org.jetbrains.kotlinx.dataframe.annotations.*
 
 // GENERATED. DO NOT EDIT MANUALLY
 @DataSchema
-interface Repository {
-    @ColumnName("full_name")
+interface Repository{
+	@ColumnName("full_name")
     val `full_name`: kotlin.String
-    @ColumnName("html_url")
+	@ColumnName("html_url")
     val `html_url`: java.net.URL
-    @ColumnName("stargazers_count")
+	@ColumnName("stargazers_count")
     val `stargazers_count`: kotlin.Int
     val topics: kotlin.String
     val watchers: kotlin.Int
