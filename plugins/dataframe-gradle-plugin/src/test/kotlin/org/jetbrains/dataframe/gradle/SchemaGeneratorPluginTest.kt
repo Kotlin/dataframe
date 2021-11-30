47a76b29b53b8f21e7bd42ea1c300aff4d25bc46
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-11-30 15:29:21 +0300

    Resolve data of type String relatively to project build dir

--- plugins/dataframe-gradle-plugin/src/test/kotlin/org/jetbrains/dataframe/gradle/SchemaGeneratorPluginTest.kt
+++ plugins/dataframe-gradle-plugin/src/test/kotlin/org/jetbrains/dataframe/gradle/SchemaGeneratorPluginTest.kt
@@ -173,6 +173,88 @@ internal class SchemaGeneratorPluginTest {
     }
 
     @Test
+    fun `data is string and relative path`() {
+        val (_, result) = runGradleBuild(":generateDataFrameTest") { buildDir ->
+            File(dataDir).copyRecursively(File(buildDir, "data"))
+            """
+            import org.jetbrains.dataframe.gradle.SchemaGeneratorExtension 
+               
+            plugins {
+                kotlin("jvm") version "1.6.0"
+                id("org.jetbrains.kotlin.plugin.dataframe")
+            }
+            
+            repositories {
+                mavenCentral() 
+            }
+
+            dataframes {
+                schema {
+                    data = "data/ghost.json"
+                    name = "Test"
+                    packageName = "org.test"
+                }
+            }
+            """.trimIndent()
+        }
+        result.task(":generateDataFrameTest")?.outcome shouldBe TaskOutcome.SUCCESS
+    }
+
+    @Test
+    fun `data is string and absolute path`() {
+        val (_, result) = runGradleBuild(":generateDataFrameTest") { buildDir ->
+            """
+            import org.jetbrains.dataframe.gradle.SchemaGeneratorExtension 
+               
+            plugins {
+                kotlin("jvm") version "1.6.0"
+                id("org.jetbrains.kotlin.plugin.dataframe")
+            }
+            
+            repositories {
+                mavenCentral() 
+            }
+
+            dataframes {
+                schema {
+                    data = "$dataDir/ghost.json"
+                    name = "Test"
+                    packageName = "org.test"
+                }
+            }
+            """.trimIndent()
+        }
+        result.task(":generateDataFrameTest")?.outcome shouldBe TaskOutcome.SUCCESS
+    }
+
+    @Test
+    fun `data is string and url`() {
+        val (_, result) = runGradleBuild(":generateDataFrameTest") { buildDir ->
+            """
+            import org.jetbrains.dataframe.gradle.SchemaGeneratorExtension 
+               
+            plugins {
+                kotlin("jvm") version "1.6.0"
+                id("org.jetbrains.kotlin.plugin.dataframe")
+            }
+            
+            repositories {
+                mavenCentral() 
+            }
+
+            dataframes {
+                schema {
+                    data = "https://raw.githubusercontent.com/Kotlin/dataframe/8ea139c35aaf2247614bb227756d6fdba7359f6a/data/ghost.json"
+                    name = "Test"
+                    packageName = "org.test"
+                }
+            }
+            """.trimIndent()
+        }
+        result.task(":generateDataFrameTest")?.outcome shouldBe TaskOutcome.SUCCESS
+    }
+
+    @Test
     fun `most specific sourceSet is used in the packageName inference`() {
         val project = makeProject()
         project.plugins.apply(SchemaGeneratorPlugin::class.java)
