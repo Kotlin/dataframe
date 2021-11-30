ca1ff56a5c5d7ac809e6b49cc45dc26c8b0757b1
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-30 17:51:40 +0300

    Support `invoke` for column accessors and strings in `aggregate` receiver. Fix bug with ColumnPath resolve in DataFrameReceiver.

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/ColumnsSelectionDsl.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/ColumnsSelectionDsl.kt
@@ -46,9 +46,9 @@ public interface ColumnSelectionDsl<out T> : ColumnsContainer<T> {
 
     public operator fun <C> ColumnReference<C>.invoke(newName: String): ColumnReference<C> = renamedReference(newName)
 
-    public operator fun <C> ColumnPath.invoke(): ColumnAccessor<C> = toColumnAccessor().cast()
+    public operator fun <C> ColumnPath.invoke(): DataColumn<C> = getColumn(this).cast()
 
-    public operator fun <C> String.invoke(): ColumnAccessor<C> = toColumnOf()
+    public operator fun <C> String.invoke(): DataColumn<C> = getColumn(this).cast()
 
     public operator fun String.get(column: String): ColumnPath = pathOf(this, column)
 
