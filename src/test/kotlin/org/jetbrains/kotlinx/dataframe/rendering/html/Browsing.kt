df764a65b9029d5c6beaa40d9cd7d0b6c07e1c49
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-11-30 12:47:41 +0300

    Add util for HTML browsing

---
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/rendering/html/Browsing.kt
@@ -0,0 +1,18 @@
+package org.jetbrains.kotlinx.dataframe.rendering.html
+
+import org.jetbrains.kotlinx.dataframe.api.groupBy
+import org.jetbrains.kotlinx.dataframe.api.into
+import org.jetbrains.kotlinx.dataframe.dataFrameOf
+import org.junit.Ignore
+import org.junit.Test
+
+class Browsing {
+
+    @Ignore
+    @Test
+    fun test() {
+        dataFrameOf("a", "b", "c").randomInt(100, 1..5)
+            .groupBy("a").into("g")
+            .browse()
+    }
+}
