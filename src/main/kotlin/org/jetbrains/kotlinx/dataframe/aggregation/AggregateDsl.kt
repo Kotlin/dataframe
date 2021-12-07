fb83114a52c5e818dbf6b0a6183c2d60092a7d25
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-12-07 09:56:26 +0300

    Add missing `into` overloads in `aggregate` DSL.

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/aggregation/AggregateDsl.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/aggregation/AggregateDsl.kt
@@ -2,14 +2,22 @@ package org.jetbrains.kotlinx.dataframe.aggregation
 
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.ColumnSelectionDsl
+import org.jetbrains.kotlinx.dataframe.api.name
+import org.jetbrains.kotlinx.dataframe.columns.ColumnAccessor
 import org.jetbrains.kotlinx.dataframe.impl.aggregation.ValueWithDefault
 import org.jetbrains.kotlinx.dataframe.impl.aggregation.receivers.internal
+import org.jetbrains.kotlinx.dataframe.impl.columnName
 import org.jetbrains.kotlinx.dataframe.impl.getType
 import org.jetbrains.kotlinx.dataframe.pathOf
+import kotlin.reflect.KProperty
 
 public abstract class AggregateDsl<out T> : DataFrame<T>, ColumnSelectionDsl<T> {
 
     public inline infix fun <reified R> R.into(name: String): NamedValue = internal().yield(pathOf(name), this, getType<R>())
+
+    public inline infix fun <reified R> R.into(column: ColumnAccessor<R>): NamedValue = internal().yield(pathOf(column.name()), this, getType<R>())
+
+    public inline infix fun <reified R> R.into(column: KProperty<R>): NamedValue = internal().yield(pathOf(column.columnName), this, getType<R>())
 
     public infix fun <R> R.default(defaultValue: R): Any = when (this) {
         is NamedValue -> this.also { it.default = defaultValue }
