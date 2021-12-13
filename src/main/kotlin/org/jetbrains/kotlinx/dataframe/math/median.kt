2b0868e7c59d25cdb204b2e77299accc6ee2a8e3
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-12-13 23:48:53 +0300

    formatting

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/math/median.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/math/median.kt
@@ -2,7 +2,6 @@ package org.jetbrains.kotlinx.dataframe.math
 
 import org.jetbrains.kotlinx.dataframe.impl.asList
 import java.math.BigDecimal
-import kotlin.random.Random
 import kotlin.reflect.KType
 
 public inline fun <reified T : Comparable<T>> Iterable<T?>.median(type: KType): T? {
@@ -12,12 +11,12 @@ public inline fun <reified T : Comparable<T>> Iterable<T?>.median(type: KType): 
     val index = size / 2
     if (index == 0 || size % 2 == 1) return list.quickSelect(index)
     return when (type.classifier) {
-        Double::class -> ((list.quickSelect(index-1) as Double + list.quickSelect(index) as Double) / 2.0) as T
-        Int::class -> ((list.quickSelect(index-1) as Int + list.quickSelect(index) as Int) / 2) as T
-        Long::class -> ((list.quickSelect(index-1) as Long + list.quickSelect(index) as Long) / 2L) as T
-        Byte::class -> ((list.quickSelect(index-1) as Byte + list.quickSelect(index) as Byte) / 2).toByte() as T
-        BigDecimal::class -> ((list.quickSelect(index-1) as BigDecimal + list.quickSelect(index) as BigDecimal) / BigDecimal(2)) as T
-        else -> list.quickSelect(index-1)
+        Double::class -> ((list.quickSelect(index - 1) as Double + list.quickSelect(index) as Double) / 2.0) as T
+        Int::class -> ((list.quickSelect(index - 1) as Int + list.quickSelect(index) as Int) / 2) as T
+        Long::class -> ((list.quickSelect(index - 1) as Long + list.quickSelect(index) as Long) / 2L) as T
+        Byte::class -> ((list.quickSelect(index - 1) as Byte + list.quickSelect(index) as Byte) / 2).toByte() as T
+        BigDecimal::class -> ((list.quickSelect(index - 1) as BigDecimal + list.quickSelect(index) as BigDecimal) / BigDecimal(2)) as T
+        else -> list.quickSelect(index - 1)
     }
 }
 
--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/statistics/quickSelect.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/statistics/quickSelect.kt
@@ -16,18 +16,18 @@ class QuickSelectTests {
     }
 
     @Test
-    fun short(){
-        val list = listOf(2,5,1)
+    fun short() {
+        val list = listOf(2, 5, 1)
         list.quickSelect(0) shouldBe 1
         list.quickSelect(1) shouldBe 2
         list.quickSelect(2) shouldBe 5
     }
 
     @Test
-    fun long(){
+    fun long() {
         val random = Random(120)
         val list = (0..20).shuffled(random)
-        for(i in list)
+        for (i in list)
             list.quickSelect(i) shouldBe i
     }
 }
