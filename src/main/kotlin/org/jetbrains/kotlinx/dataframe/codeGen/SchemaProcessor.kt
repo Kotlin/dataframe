23454542a12806e26275569e8a95c2909ee4fc9e
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2021-12-18 22:11:29 +0300

    Add option to normalize field names of generated code
    Reasons for that:
    symbols with whitespaces in names don't compile on android for dex format < 40 version
    underscores, caps and whitespaces do not follow kotlin code-style

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/codeGen/SchemaProcessor.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/codeGen/SchemaProcessor.kt
@@ -1,6 +1,9 @@
 package org.jetbrains.kotlinx.dataframe.codeGen
 
+import org.jetbrains.kotlinx.dataframe.impl.DELIMITED_STRING_REGEX
+import org.jetbrains.kotlinx.dataframe.impl.DELIMITERS_REGEX
 import org.jetbrains.kotlinx.dataframe.impl.codeGen.SchemaProcessorImpl
+import org.jetbrains.kotlinx.dataframe.impl.toCamelCaseByDelimiters
 import org.jetbrains.kotlinx.dataframe.schema.DataFrameSchema
 
 internal interface SchemaProcessor {
@@ -16,6 +19,24 @@ internal interface SchemaProcessor {
     ): Marker
 
     companion object {
-        fun create(namePrefix: String, existingMarkers: Iterable<Marker> = emptyList()) = SchemaProcessorImpl(existingMarkers, namePrefix)
+        fun create(
+            namePrefix: String,
+            existingMarkers: Iterable<Marker> = emptyList(),
+            normalizeFieldNames: Boolean = false
+        ): SchemaProcessorImpl {
+            return if (normalizeFieldNames) {
+                SchemaProcessorImpl(existingMarkers, namePrefix, fieldNameNormalizer)
+            } else {
+                SchemaProcessorImpl(existingMarkers, namePrefix)
+            }
+        }
+
+        private val fieldNameNormalizer: (String) -> String = {
+            if (it matches DELIMITED_STRING_REGEX) {
+                it.lowercase().toCamelCaseByDelimiters(DELIMITERS_REGEX)
+            } else {
+                it
+            }
+        }
     }
 }
