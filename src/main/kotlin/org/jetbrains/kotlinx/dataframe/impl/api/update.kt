9ef01e9c3f91f2830dcaa014a05ebcf34c75fedd
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-12-20 16:01:44 +0300

    Support `new()` function in `update{}.with{}` for getting updated values in rows.

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/api/update.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/api/update.kt
@@ -4,12 +4,13 @@ import org.jetbrains.kotlinx.dataframe.AnyFrame
 import org.jetbrains.kotlinx.dataframe.AnyRow
 import org.jetbrains.kotlinx.dataframe.DataColumn
 import org.jetbrains.kotlinx.dataframe.DataFrame
-import org.jetbrains.kotlinx.dataframe.DataRow
 import org.jetbrains.kotlinx.dataframe.RowValueFilter
 import org.jetbrains.kotlinx.dataframe.Selector
+import org.jetbrains.kotlinx.dataframe.api.AddDataRow
+import org.jetbrains.kotlinx.dataframe.api.AddDataRowImpl
 import org.jetbrains.kotlinx.dataframe.api.UpdateClause
 import org.jetbrains.kotlinx.dataframe.api.cast
-import org.jetbrains.kotlinx.dataframe.api.forEachRow
+import org.jetbrains.kotlinx.dataframe.api.indices
 import org.jetbrains.kotlinx.dataframe.api.name
 import org.jetbrains.kotlinx.dataframe.api.replace
 import org.jetbrains.kotlinx.dataframe.api.toDataFrame
@@ -18,14 +19,13 @@ import org.jetbrains.kotlinx.dataframe.columns.ColumnGroup
 import org.jetbrains.kotlinx.dataframe.columns.FrameColumn
 import org.jetbrains.kotlinx.dataframe.columns.size
 import org.jetbrains.kotlinx.dataframe.impl.createDataCollector
-import org.jetbrains.kotlinx.dataframe.index
 import org.jetbrains.kotlinx.dataframe.type
 import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.withNullability
 import kotlin.reflect.jvm.jvmErasure
 
 @PublishedApi
-internal fun <T, C> UpdateClause<T, C>.updateImpl(expression: (DataRow<T>, DataColumn<C>, C) -> C?): DataFrame<T> = df.replace(columns).with { it.updateImpl(df, filter, expression) }
+internal fun <T, C> UpdateClause<T, C>.updateImpl(expression: (AddDataRow<T>, DataColumn<C>, C) -> C?): DataFrame<T> = df.replace(columns).with { it.updateImpl(df, filter, expression) }
 
 internal fun <T, C> UpdateClause<T, C>.updateWithValuePerColumnImpl(selector: Selector<DataColumn<C>, C>) = df.replace(columns).with {
     val value = selector(it, it)
@@ -36,16 +36,18 @@ internal fun <T, C> UpdateClause<T, C>.updateWithValuePerColumnImpl(selector: Se
 internal fun <T, C> DataColumn<C>.updateImpl(
     df: DataFrame<T>,
     filter: RowValueFilter<T, C>?,
-    expression: (DataRow<T>, DataColumn<C>, C) -> C?
+    expression: (AddDataRow<T>, DataColumn<C>, C) -> C?
 ): DataColumn<C> {
     val collector = createDataCollector<C>(size, type)
     val src = this
     if (filter == null) {
-        df.forEachRow { row ->
-            collector.add(expression(row, src, src[row.index]))
+        df.indices().forEach { rowIndex ->
+            val row = AddDataRowImpl(rowIndex, df, collector.values)
+            collector.add(expression(row, src, src[rowIndex]))
         }
     } else {
-        df.forEachRow { row ->
+        df.indices().forEach { rowIndex ->
+            val row = AddDataRowImpl(rowIndex, df, collector.values)
             val currentValue = row[src]
             val newValue =
                 if (filter.invoke(row, currentValue)) expression(row, src, currentValue) else currentValue
