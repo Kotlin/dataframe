710da583c6eada4acc5962523e620856920098fe
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-01-12 14:54:34 +0300

    Add test for an incorrectly inferred type

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/io/TypeInferenceTest.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/io/TypeInferenceTest.kt
@@ -1,7 +1,9 @@
 package org.jetbrains.kotlinx.dataframe.io
 
 import io.kotest.matchers.shouldBe
+import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
+import org.jetbrains.kotlinx.dataframe.api.schema
 import org.junit.Test
 import kotlin.reflect.typeOf
 
@@ -15,5 +17,18 @@ class TypeInferenceTest {
     fun `private subtypes`() {
         val df = dataFrameOf("col")(B(), B())
         df["col"].type() shouldBe typeOf<A>()
+    }
+
+    @Test
+    fun `json property named 'value'`() {
+        val df = DataFrame.readJsonStr(
+            """
+                {
+                  "key": "icon",
+                  "value": "str"
+                }
+            """.trimIndent()
+        )
+        df.schema().columns["value"]?.type shouldBe typeOf<String>()
     }
 }
