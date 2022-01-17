868e33bae1d5271a9d49287bb184accfe935ccad
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2022-01-17 21:02:20 +0300

    Fix NPE bug in `describe`

---
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/describe.kt
@@ -0,0 +1,14 @@
+package org.jetbrains.kotlinx.dataframe.api
+
+import io.kotest.matchers.shouldBe
+import org.junit.Test
+
+class DescribeTests {
+
+    @Test
+    fun `describe all nulls`(){
+        val a by columnOf(1, null)
+        val df = dataFrameOf(a).drop(1)
+        df.describe()["min"][0] shouldBe null
+    }
+}
