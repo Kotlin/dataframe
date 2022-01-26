f70380025f84738411312678b2ccf9ceb6724cfd
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2022-01-26 15:16:28 +0300

    Display `...showing only top x of y rows` message for nested dataframes.

--- src/main/resources/addTable.js
+++ src/main/resources/addTable.js
@@ -1,3 +1,3 @@
 /*<!--*/
-call_DataFrame(function() { DataFrame.addTable({ cols: ___COLUMNS___, id: ___ID___, rootId: ___ROOT___ } ) });
+call_DataFrame(function() { DataFrame.addTable({ cols: ___COLUMNS___, id: ___ID___, rootId: ___ROOT___, totalRows: ___NROW___ } ) });
 /*-->*/
