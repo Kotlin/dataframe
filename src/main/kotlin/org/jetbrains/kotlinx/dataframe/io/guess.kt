628e2b47279c5b81c22ba48b8d072acad0861199
Stanislav Poslavsky <stvlpos@mail.ru>
2022-03-09 22:27:16 +0200

    Supprt for TSV guess

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/guess.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/guess.kt
@@ -10,12 +10,14 @@ import java.net.URL
 
 public enum class SupportedFormats {
     CSV,
+    TSV,
     JSON
 }
 
 internal fun guessFormat(file: File): SupportedFormats? = when (file.extension.toLowerCase()) {
     "json" -> SupportedFormats.JSON
     "csv" -> SupportedFormats.CSV
+    "tsv" -> SupportedFormats.TSV
     else -> null
 }
 
@@ -23,17 +25,23 @@ internal fun guessFormat(url: URL): SupportedFormats? = guessFormat(url.path)
 
 internal fun guessFormat(url: String): SupportedFormats? = when {
     url.endsWith(".csv") -> SupportedFormats.CSV
+    url.endsWith(".tsv") -> SupportedFormats.TSV
     url.endsWith(".json") -> SupportedFormats.JSON
     else -> null
 }
 
 public fun DataFrame.Companion.read(file: File): AnyFrame = when (guessFormat(file)) {
     SupportedFormats.CSV -> readCSV(file)
+    SupportedFormats.TSV -> readTSV(file)
     SupportedFormats.JSON -> readJson(file)
     else -> try {
         readCSV(file)
     } catch (e: Exception) {
-        readJson(file)
+        try {
+            readTSV(file)
+        } catch (e: Exception) {
+            readJson(file)
+        }
     }
 }
 
@@ -41,11 +49,16 @@ public fun DataRow.Companion.read(file: File): AnyRow = DataFrame.read(file).sin
 
 public fun DataFrame.Companion.read(url: URL): AnyFrame = when (guessFormat(url)) {
     SupportedFormats.CSV -> readCSV(url)
+    SupportedFormats.TSV -> readTSV(url)
     SupportedFormats.JSON -> readJson(url)
     else -> try {
         readCSV(url)
     } catch (e: Exception) {
-        readJson(url)
+        try {
+            readTSV(url)
+        } catch (e: Exception) {
+            readJson(url)
+        }
     }
 }
 
@@ -53,11 +66,16 @@ public fun DataRow.Companion.read(url: URL): AnyRow = DataFrame.read(url).single
 
 public fun DataFrame.Companion.read(path: String): AnyFrame = when (guessFormat(path)) {
     SupportedFormats.CSV -> readCSV(path)
+    SupportedFormats.TSV -> readTSV(path)
     SupportedFormats.JSON -> readJson(path)
     else -> try {
         readCSV(path)
     } catch (e: Exception) {
-        readJson(path)
+        try {
+            readTSV(path)
+        } catch (e: Exception) {
+            readJson(path)
+        }
     }
 }
 
