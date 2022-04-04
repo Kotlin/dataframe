1c329422ba14b2ce58d4b21299347a201e90829d
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-04-04 18:27:04 +0300

    Support writing excel

--- src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/xlsx.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/xlsx.kt
@@ -1,17 +1,28 @@
 package org.jetbrains.kotlinx.dataframe.io
 
+import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toJavaLocalDateTime
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.CellType
+import org.apache.poi.ss.usermodel.RichTextString
 import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.apache.poi.ss.util.CellReference
 import org.jetbrains.kotlinx.dataframe.AnyFrame
+import org.jetbrains.kotlinx.dataframe.AnyRow
+import org.jetbrains.kotlinx.dataframe.ColumnsSelector
 import org.jetbrains.kotlinx.dataframe.DataColumn
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
+import org.jetbrains.kotlinx.dataframe.api.forEachRow
+import org.jetbrains.kotlinx.dataframe.api.select
 import java.io.File
 import java.io.InputStream
 import java.net.URL
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
 
 public fun DataFrame.Companion.readExcel(
     url: URL,
@@ -79,5 +90,90 @@ public fun DataFrame.Companion.readExcel(
             DataColumn.createWithTypeInference(name, values)
         }
         dataFrameOf(columns)
+    }
+}
+
+public fun <T> DataFrame<T>.writeExcel(
+    path: String,
+    columnsSelector: ColumnsSelector<T, *> = { all() },
+    sheetName: String? = null,
+    writeHeader: Boolean = true
+) {
+    return writeExcel(File(path), columnsSelector, sheetName, writeHeader)
+}
+
+public fun <T> DataFrame<T>.writeExcel(
+    file: File,
+    columnsSelector: ColumnsSelector<T, *> = { all() },
+    sheetName: String? = null,
+    writeHeader: Boolean = true
+) {
+    val wb = HSSFWorkbook()
+    val sheet = if (sheetName != null) {
+        wb.createSheet(sheetName)
+    } else {
+        wb.createSheet()
+    }
+
+    var i = 0
+    val columns = select(columnsSelector)
+
+    if (writeHeader) {
+        val row = sheet.createRow(i)
+        columns.columnNames().forEachIndexed { index, s ->
+            val cell = row.createCell(index)
+            cell.setCellValue(s)
+        }
+        i++
+    }
+
+    columns.forEachRow {
+        val row = sheet.createRow(i)
+        it.values().forEachIndexed { index, any ->
+            val cell = row.createCell(index)
+            when (any) {
+                is AnyRow -> {
+                    cell.setCellValue(any.toJson())
+                }
+                is AnyFrame ->  {
+                    cell.setCellValue(any.toJson())
+                }
+                is Number -> {
+                    cell.setCellValue(any.toDouble())
+                }
+                is LocalDate -> {
+                    cell.setCellValue(any)
+                }
+                is LocalDateTime -> {
+                    cell.setCellValue(any)
+                }
+                is Boolean -> {
+                    cell.setCellValue(any)
+                }
+                is Calendar -> {
+                    cell.setCellValue(any.time)
+                }
+                is Date -> {
+                    cell.setCellValue(any)
+                }
+                is RichTextString -> {
+                    cell.setCellValue(any)
+                }
+                is String -> {
+                    cell.setCellValue(any)
+                }
+                is kotlinx.datetime.LocalDate -> {
+                    cell.setCellValue(any.toJavaLocalDate())
+                }
+                is kotlinx.datetime.LocalDateTime -> {
+                    cell.setCellValue(any.toJavaLocalDateTime())
+                }
+            }
+        }
+        i++
+    }
+
+    file.outputStream().use {
+        wb.write(it)
     }
 }
