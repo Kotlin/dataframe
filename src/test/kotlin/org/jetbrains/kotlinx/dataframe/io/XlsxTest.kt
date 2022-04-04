c5238385acf7ba2fc1c6b202f93bafd05a39871b
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-04-04 13:51:17 +0300

    Move test data to resource folder

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/io/XlsxTest.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/io/XlsxTest.kt
@@ -4,37 +4,38 @@ import io.kotest.matchers.shouldBe
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
 import org.jetbrains.kotlinx.dataframe.api.toColumn
+import org.jetbrains.kotlinx.dataframe.testResource
 import org.junit.Test
 
 class XlsxTest {
 
     @Test
     fun `numerical columns`() {
-        DataFrame.readExcel("data/sample.xls", "Sheet1") shouldBe dataFrameOf("col1", "col2")(1.0, 2.0)
+        DataFrame.readExcel(testResource("sample.xls"), "Sheet1") shouldBe dataFrameOf("col1", "col2")(1.0, 2.0)
     }
 
     @Test
     fun `column with empty values`() {
-        val readExcel = DataFrame.readExcel("data/sample1.xls", "Sheet1")
+        val readExcel = DataFrame.readExcel(testResource("sample1.xls"), "Sheet1")
         readExcel shouldBe dataFrameOf("col1", "col2")(1.0, null)
         readExcel["col2"].hasNulls() shouldBe true
     }
 
     @Test
     fun `column with empty header`() {
-        val df = DataFrame.readExcel("data/sample2.xlsx", "Sheet1", "A:C")
+        val df = DataFrame.readExcel(testResource("sample2.xlsx"), "Sheet1", "A:C")
         df shouldBe dataFrameOf("col1", "col2", "C")(1.0, null, 3.0)
     }
 
     @Test
     fun `limit row number`() {
-        val df = DataFrame.readExcel("data/sample4.xls", "Sheet1", rowsCount = 5)
+        val df = DataFrame.readExcel(testResource("sample4.xls"), "Sheet1", rowsCount = 5)
         val column = List(5) { (it + 1).toDouble() }.toColumn("col1")
         df shouldBe dataFrameOf(column)
     }
 
     @Test
     fun `first sheet is default sheet`() {
-        DataFrame.readExcel("data/sample.xls", "Sheet1") shouldBe DataFrame.readExcel("data/sample.xls")
+        DataFrame.readExcel(testResource("sample.xls"), "Sheet1") shouldBe DataFrame.readExcel(testResource("sample.xls"))
     }
 }
