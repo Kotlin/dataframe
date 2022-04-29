65050c3328530fe9aa1530c96d5c9c79cac3ea91
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2022-04-29 22:15:06 +0300

    Fix default column selector for `explode`

--- src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/explode.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/explode.kt
@@ -8,8 +8,24 @@ class ExplodeTests {
     @Test
     fun `explode into`() {
         val df = dataFrameOf("a" to listOf(1), "b" to listOf(listOf(2, 3)))
-        val imploded = df.explode { "b" into "c" }
+        val exploded = df.explode { "b" into "c" }
         val expected = dataFrameOf("a" to listOf(1, 1), "c" to listOf(2, 3))
-        imploded shouldBe expected
+        exploded shouldBe expected
+    }
+
+    @Test
+    fun `explode list and duplicate value`() {
+        val exploded = dataFrameOf("a", "b")(1, listOf(2, 3)).explode()
+        exploded shouldBe dataFrameOf("a", "b")(1, 2, 1, 3)
+    }
+
+    @Test
+    fun `explode list and frame column`() {
+        val exploded = dataFrameOf("a", "b", "c", "d")(1, listOf(2, 3), dataFrameOf("x", "y")(4, 5, 6, 7), listOf(8))
+            .explode().ungroup("c")
+        exploded shouldBe dataFrameOf("a", "b", "x", "y", "d")(
+            1, 2, 4, 5, 8,
+            1, 3, 6, 7, null
+        )
     }
 }
