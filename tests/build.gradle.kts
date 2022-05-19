f41e32ecf188ffff0d7ac91e49181f6911c89522
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-05-19 18:58:42 +0300

    Remove preprocessor from root module

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -1,10 +1,12 @@
+import org.jetbrains.dataframe.gradle.DataSchemaVisibility
+
 plugins {
     id("java")
     kotlin("jvm")
     kotlin("plugin.dataframe")
+    id("io.github.devcrocod.korro")
+    id("org.jmailen.kotlinter")
 }
-
-group = "org.jetbrains.kotlinx"
 
 repositories {
     mavenCentral()
@@ -25,5 +27,78 @@ kotlin.sourceSets {
     }
     test {
         kotlin.srcDir("build/generated/ksp/test/kotlin/")
+    }
+}
+
+korro {
+    docs = fileTree(rootProject.rootDir) {
+        include("docs/StardustDocs/topics/*.md")
+    }
+
+    samples = fileTree(project.projectDir) {
+        include("src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/*.kt")
+        include("src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/*.kt")
+    }
+
+    groupSamples {
+
+        beforeSample.set("<tab title=\"NAME\">\n")
+        afterSample.set("\n</tab>")
+
+        funSuffix("_properties") {
+            replaceText("NAME", "Properties")
+        }
+        funSuffix("_accessors") {
+            replaceText("NAME", "Accessors")
+        }
+        funSuffix("_strings") {
+            replaceText("NAME", "Strings")
+        }
+        beforeGroup.set("<tabs>\n")
+        afterGroup.set("</tabs>")
+    }
+}
+
+tasks.lintKotlinMain {
+    exclude("**/*keywords*/**")
+    exclude {
+        it.name.endsWith(".Generated.kt")
+    }
+    exclude {
+        it.name.endsWith("\$Extensions.kt")
+    }
+}
+
+tasks.lintKotlinTest {
+    exclude {
+        it.name.endsWith(".Generated.kt")
+    }
+    exclude {
+        it.name.endsWith("\$Extensions.kt")
+    }
+    enabled = true
+}
+
+kotlinter {
+    ignoreFailures = false
+    reporters = arrayOf("checkstyle", "plain")
+    experimentalRules = true
+    disabledRules = arrayOf(
+        "no-wildcard-imports",
+        "experimental:spacing-between-declarations-with-annotations",
+        "experimental:enum-entry-name-case",
+        "experimental:argument-list-wrapping",
+        "experimental:annotation",
+        "max-line-length",
+        "filename"
+    )
+}
+
+dataframes {
+    schema {
+        sourceSet = "test"
+        visibility = DataSchemaVisibility.IMPLICIT_PUBLIC
+        data = "https://raw.githubusercontent.com/Kotlin/dataframe/master/data/jetbrains_repositories.csv"
+        name = "org.jetbrains.kotlinx.dataframe.samples.api.Repository"
     }
 }
