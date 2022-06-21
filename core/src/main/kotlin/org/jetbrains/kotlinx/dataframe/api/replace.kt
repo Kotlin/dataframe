167a9e40196a61ade145c6e05f79e8e0f796e2c0
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-06-21 15:54:25 +0300

    Fix misspell

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/replace.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/replace.kt
@@ -14,11 +14,11 @@ import org.jetbrains.kotlinx.dataframe.impl.columns.toColumnSet
 import org.jetbrains.kotlinx.dataframe.impl.columns.toColumns
 import kotlin.reflect.KProperty
 
-public fun <T, C> DataFrame<T>.replace(columns: ColumnsSelector<T, C>): ReplaceCause<T, C> = ReplaceCause(this, columns)
-public fun <T> DataFrame<T>.replace(vararg columns: String): ReplaceCause<T, Any?> = replace { columns.toColumns() }
-public fun <T, C> DataFrame<T>.replace(vararg columns: ColumnReference<C>): ReplaceCause<T, C> = replace { columns.toColumns() }
-public fun <T, C> DataFrame<T>.replace(vararg columns: KProperty<C>): ReplaceCause<T, C> = replace { columns.toColumns() }
-public fun <T, C> DataFrame<T>.replace(columns: Iterable<ColumnReference<C>>): ReplaceCause<T, C> = replace { columns.toColumnSet() }
+public fun <T, C> DataFrame<T>.replace(columns: ColumnsSelector<T, C>): ReplaceClause<T, C> = ReplaceClause(this, columns)
+public fun <T> DataFrame<T>.replace(vararg columns: String): ReplaceClause<T, Any?> = replace { columns.toColumns() }
+public fun <T, C> DataFrame<T>.replace(vararg columns: ColumnReference<C>): ReplaceClause<T, C> = replace { columns.toColumns() }
+public fun <T, C> DataFrame<T>.replace(vararg columns: KProperty<C>): ReplaceClause<T, C> = replace { columns.toColumns() }
+public fun <T, C> DataFrame<T>.replace(columns: Iterable<ColumnReference<C>>): ReplaceClause<T, C> = replace { columns.toColumnSet() }
 
 public fun <T> DataFrame<T>.replaceAll(
     vararg valuePairs: Pair<Any?, Any?>,
@@ -28,11 +28,11 @@ public fun <T> DataFrame<T>.replaceAll(
     return update(columns).with { map[it] ?: it }
 }
 
-public data class ReplaceCause<T, C>(val df: DataFrame<T>, val columns: ColumnsSelector<T, C>)
+public data class ReplaceClause<T, C>(val df: DataFrame<T>, val columns: ColumnsSelector<T, C>)
 
-public fun <T, C> ReplaceCause<T, C>.with(vararg columns: AnyCol): DataFrame<T> = with(columns.toList())
+public fun <T, C> ReplaceClause<T, C>.with(vararg columns: AnyCol): DataFrame<T> = with(columns.toList())
 
-public fun <T, C> ReplaceCause<T, C>.with(newColumns: List<AnyCol>): DataFrame<T> {
+public fun <T, C> ReplaceClause<T, C>.with(newColumns: List<AnyCol>): DataFrame<T> {
     var index = 0
     return with {
         require(index < newColumns.size) { "Insufficient number of new columns in 'replace': ${newColumns.size} instead of ${df[columns].size}" }
@@ -40,7 +40,7 @@ public fun <T, C> ReplaceCause<T, C>.with(newColumns: List<AnyCol>): DataFrame<T
     }
 }
 
-public fun <T, C> ReplaceCause<T, C>.with(transform: ColumnsContainer<T>.(DataColumn<C>) -> AnyBaseCol): DataFrame<T> {
+public fun <T, C> ReplaceClause<T, C>.with(transform: ColumnsContainer<T>.(DataColumn<C>) -> AnyBaseCol): DataFrame<T> {
     val removeResult = df.removeImpl(columns = columns)
     val toInsert = removeResult.removedColumns.map {
         val newCol = transform(df, it.data.column as DataColumn<C>)
