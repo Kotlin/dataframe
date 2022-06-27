79c0757ce651da4aad6b7a0cbda61805718c361e
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-06-27 15:07:46 +0300

    Improve error message for row.getColumnGroup and fix warnings

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/columns/ColumnKind.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/columns/ColumnKind.kt
@@ -1,7 +1,13 @@
 package org.jetbrains.kotlinx.dataframe.columns
 
 public enum class ColumnKind {
-    Value,
-    Group,
-    Frame
+    Value {
+        override fun toString(): String = "ValueColumn"
+    },
+    Group {
+        override fun toString(): String = "ColumnGroup"
+    },
+    Frame {
+        override fun toString(): String = "FrameColumn"
+    }
 }
