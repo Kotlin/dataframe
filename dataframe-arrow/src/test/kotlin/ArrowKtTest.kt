8920518bf40034e6b7e09c45e275d2debaa2bf6a
Kopilov Aleksandr <kopilov.ad@gmail.com>
2022-07-07 15:14:07 +0300

    New test draft, keep nullables in schema

--- dataframe-arrow/src/test/kotlin/ArrowKtTest.kt
+++ dataframe-arrow/src/test/kotlin/ArrowKtTest.kt
@@ -1,34 +1,44 @@
 import io.kotest.matchers.shouldBe
 import org.apache.arrow.vector.util.Text
+import org.jetbrains.kotlinx.dataframe.DataColumn
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.columnOf
 import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
 import org.jetbrains.kotlinx.dataframe.api.toColumn
 import org.jetbrains.kotlinx.dataframe.io.readArrowFeather
+import org.jetbrains.kotlinx.dataframe.io.readArrowIPC
 import org.junit.Test
 import java.net.URL
+import kotlin.reflect.typeOf
 
 internal class ArrowKtTest {
 
     fun testResource(resourcePath: String): URL = ArrowKtTest::class.java.classLoader.getResource(resourcePath)!!
 
     fun testArrowFeather(name: String) = testResource("$name.feather")
+    fun testArrowIPC(name: String) = testResource("$name.ipc")
 
     @Test
     fun testReadingFromFile() {
         val feather = testArrowFeather("data-arrow_2.0.0_uncompressed")
         val df = DataFrame.readArrowFeather(feather)
-        val a by columnOf("one")
-        val b by columnOf(2.0)
+        val a by listOf("one" as String?).toColumn()
+        val b by listOf(2.0 as Double?).toColumn()
         val c by listOf(
             mapOf(
                 "c1" to Text("inner"),
                 "c2" to 4.0,
                 "c3" to 50.0
-            ) as Map<String, Any?>
+            ) as Map<String, Any?>?
         ).toColumn()
-        val d by columnOf("four")
+        val d by listOf("four" as String?).toColumn()
         val expected = dataFrameOf(a, b, c, d)
         df shouldBe expected
+    }
+
+    @Test
+    fun testReadingAllTypesAsEstimated() {
+        assertEstimations(DataFrame.readArrowFeather(testArrowFeather("test.arrow")))
+        assertEstimations(DataFrame.readArrowIPC(testArrowIPC("test.arrow")))
     }
 }
