8ccd85b27f0c4bc133280ed4f3043fcdc0d8df67
Kopilov Aleksandr <kopilov.ad@gmail.com>
2022-07-11 16:58:44 +0300

    Nullable checking beta

--- dataframe-arrow/src/test/kotlin/exampleEstimatesAssertions.kt
+++ dataframe-arrow/src/test/kotlin/exampleEstimatesAssertions.kt
@@ -9,12 +9,13 @@ import java.time.LocalTime
 import java.time.ZoneOffset
 import kotlin.math.absoluteValue
 import kotlin.math.pow
+import kotlin.reflect.full.withNullability
 import kotlin.reflect.typeOf
 
 /**
  * Assert that we have got the same data that was originally saved on example creation.
  */
-internal fun assertEstimations(exampleFrame: AnyFrame) {
+internal fun assertEstimations(exampleFrame: AnyFrame, nullable: Boolean, withNulls: Boolean) {
     /**
      * In [exampleFrame] we get two concatenated batches. To assert the estimations, we should transform frame row number to batch row number
      */
@@ -22,124 +23,137 @@ internal fun assertEstimations(exampleFrame: AnyFrame) {
         val firstBatchSize = 100;
         return if (iFrame < firstBatchSize) iFrame else iFrame - firstBatchSize
     }
+
+    fun expectedNull(rowNumber: Int): Boolean {
+        return (rowNumber + 1) % 5 == 0;
+    }
+
+    fun assertValueOrNull(rowNumber: Int, actual: Any?, expected: Any) {
+        if (withNulls && expectedNull(rowNumber)) {
+            actual shouldBe null
+        } else {
+            actual shouldBe expected
+        }
+    }
+
     val asciiStringCol = exampleFrame["asciiString"] as DataColumn<String?>
-    asciiStringCol.type() shouldBe typeOf<String?>()
+    asciiStringCol.type() shouldBe typeOf<String>().withNullability(nullable)
     asciiStringCol.forEachIndexed { i, element ->
-        element shouldBe "Test Example ${iBatch(i)}"
+        assertValueOrNull(iBatch(i), element, "Test Example ${iBatch(i)}")
     }
 
     val utf8StringCol = exampleFrame["utf8String"] as DataColumn<String?>
-    utf8StringCol.type() shouldBe typeOf<String?>()
+    utf8StringCol.type() shouldBe typeOf<String>().withNullability(nullable)
     utf8StringCol.forEachIndexed { i, element ->
-        element shouldBe "Тестовый пример ${iBatch(i)}"
+        assertValueOrNull(iBatch(i), element, "Тестовый пример ${iBatch(i)}")
     }
 
     val largeStringCol = exampleFrame["largeString"] as DataColumn<String?>
-    largeStringCol.type() shouldBe typeOf<String?>()
+    largeStringCol.type() shouldBe typeOf<String>().withNullability(nullable)
     largeStringCol.forEachIndexed { i, element ->
-        element shouldBe "Test Example Should Be Large ${iBatch(i)}"
+        assertValueOrNull(iBatch(i), element, "Test Example Should Be Large ${iBatch(i)}")
     }
 
     val booleanCol = exampleFrame["boolean"] as DataColumn<Boolean?>
-    booleanCol.type() shouldBe typeOf<Boolean?>()
+    booleanCol.type() shouldBe typeOf<Boolean>().withNullability(nullable)
     booleanCol.forEachIndexed { i, element ->
-        element shouldBe (iBatch(i) % 2 == 0)
+        assertValueOrNull(iBatch(i), element, iBatch(i) % 2 == 0)
     }
 
     val byteCol = exampleFrame["byte"] as DataColumn<Byte?>
-    byteCol.type() shouldBe typeOf<Byte?>()
+    byteCol.type() shouldBe typeOf<Byte>().withNullability(nullable)
     byteCol.forEachIndexed { i, element ->
-        element shouldBe (iBatch(i) * 10).toByte()
+        assertValueOrNull(iBatch(i), element, (iBatch(i) * 10).toByte())
     }
 
     val shortCol = exampleFrame["short"] as DataColumn<Short?>
-    shortCol.type() shouldBe typeOf<Short?>()
+    shortCol.type() shouldBe typeOf<Short>().withNullability(nullable)
     shortCol.forEachIndexed { i, element ->
-        element shouldBe (iBatch(i) * 1000).toShort()
+        assertValueOrNull(iBatch(i), element, (iBatch(i) * 1000).toShort())
     }
 
     val intCol = exampleFrame["int"] as DataColumn<Int?>
-    intCol.type() shouldBe typeOf<Int?>()
+    intCol.type() shouldBe typeOf<Int>().withNullability(nullable)
     intCol.forEachIndexed { i, element ->
-        element shouldBe (iBatch(i) * 100000000).toInt()
+        assertValueOrNull(iBatch(i), element, iBatch(i) * 100000000)
     }
 
     val longCol = exampleFrame["longInt"] as DataColumn<Long?>
-    longCol.type() shouldBe typeOf<Long?>()
+    longCol.type() shouldBe typeOf<Long>().withNullability(nullable)
     longCol.forEachIndexed { i, element ->
-        element shouldBe iBatch(i) * 100000000000000000L
+        assertValueOrNull(iBatch(i), element, iBatch(i) * 100000000000000000L)
     }
 
     val unsignedByteCol = exampleFrame["unsigned_byte"] as DataColumn<Short?>
-    unsignedByteCol.type() shouldBe typeOf<Short?>()
+    unsignedByteCol.type() shouldBe typeOf<Short>().withNullability(nullable)
     unsignedByteCol.forEachIndexed { i, element ->
-        element shouldBe (iBatch(i) * 10 % (Byte.MIN_VALUE.toShort() * 2).absoluteValue).toShort()
+        assertValueOrNull(iBatch(i), element, (iBatch(i) * 10 % (Byte.MIN_VALUE.toShort() * 2).absoluteValue).toShort())
     }
 
     val unsignedShortCol = exampleFrame["unsigned_short"] as DataColumn<Int?>
-    unsignedShortCol.type() shouldBe typeOf<Int?>()
+    unsignedShortCol.type() shouldBe typeOf<Int>().withNullability(nullable)
     unsignedShortCol.forEachIndexed { i, element ->
-        element shouldBe (iBatch(i) * 1000 % (Short.MIN_VALUE.toInt() * 2).absoluteValue)
+        assertValueOrNull(iBatch(i), element, iBatch(i) * 1000 % (Short.MIN_VALUE.toInt() * 2).absoluteValue)
     }
 
     val unsignedIntCol = exampleFrame["unsigned_int"] as DataColumn<Long?>
-    unsignedIntCol.type() shouldBe typeOf<Long?>()
+    unsignedIntCol.type() shouldBe typeOf<Long>().withNullability(nullable)
     unsignedIntCol.forEachIndexed { i, element ->
-        element shouldBe (iBatch(i).toLong() * 100000000 % (Int.MIN_VALUE.toLong() * 2).absoluteValue)
+        assertValueOrNull(iBatch(i), element, iBatch(i).toLong() * 100000000 % (Int.MIN_VALUE.toLong() * 2).absoluteValue)
     }
 
     val unsignedLongIntCol = exampleFrame["unsigned_longInt"] as DataColumn<BigInteger?>
-    unsignedLongIntCol.type() shouldBe typeOf<BigInteger?>()
+    unsignedLongIntCol.type() shouldBe typeOf<BigInteger>().withNullability(nullable)
     unsignedLongIntCol.forEachIndexed { i, element ->
-        element shouldBe (iBatch(i).toBigInteger() * 100000000000000000L.toBigInteger() % (Long.MIN_VALUE.toBigInteger() * 2.toBigInteger()).abs())
+        assertValueOrNull(iBatch(i), element, iBatch(i).toBigInteger() * 100000000000000000L.toBigInteger() % (Long.MIN_VALUE.toBigInteger() * 2.toBigInteger()).abs())
     }
 
     val floatCol = exampleFrame["float"] as DataColumn<Float?>
-    floatCol.type() shouldBe typeOf<Float?>()
+    floatCol.type() shouldBe typeOf<Float>().withNullability(nullable)
     floatCol.forEachIndexed { i, element ->
-        element shouldBe (2.0f.pow(iBatch(i).toFloat()))
+        assertValueOrNull(iBatch(i), element, 2.0f.pow(iBatch(i).toFloat()))
     }
 
     val doubleCol = exampleFrame["double"] as DataColumn<Double?>
-    doubleCol.type() shouldBe typeOf<Double?>()
+    doubleCol.type() shouldBe typeOf<Double>().withNullability(nullable)
     doubleCol.forEachIndexed { i, element ->
-        element shouldBe (2.0.pow(iBatch(i).toDouble()))
+        assertValueOrNull(iBatch(i), element, 2.0.pow(iBatch(i)))
     }
 
     val dateCol = exampleFrame["date32"] as DataColumn<LocalDate?>
-    dateCol.type() shouldBe typeOf<LocalDate?>()
+    dateCol.type() shouldBe typeOf<LocalDate>().withNullability(nullable)
     dateCol.forEachIndexed { i, element ->
-        element shouldBe LocalDate.ofEpochDay(iBatch(i).toLong() * 30)
+        assertValueOrNull(iBatch(i), element, LocalDate.ofEpochDay(iBatch(i).toLong() * 30))
     }
 
     val datetimeCol = exampleFrame["date64"] as DataColumn<LocalDateTime?>
-    datetimeCol.type() shouldBe typeOf<LocalDateTime?>()
+    datetimeCol.type() shouldBe typeOf<LocalDateTime>().withNullability(nullable)
     datetimeCol.forEachIndexed { i, element ->
-        element shouldBe LocalDateTime.ofEpochSecond(iBatch(i).toLong() * 60 * 60 * 24 * 30, 0, ZoneOffset.UTC)
+        assertValueOrNull(iBatch(i), element, LocalDateTime.ofEpochSecond(iBatch(i).toLong() * 60 * 60 * 24 * 30, 0, ZoneOffset.UTC))
     }
 
     val timeSecCol = exampleFrame["time32_seconds"] as DataColumn<LocalTime?>
-    timeSecCol.type() shouldBe typeOf<LocalTime?>()
+    timeSecCol.type() shouldBe typeOf<LocalTime>().withNullability(nullable)
     timeSecCol.forEachIndexed { i, element ->
-        element shouldBe LocalTime.ofSecondOfDay(iBatch(i).toLong())
+        assertValueOrNull(iBatch(i), element, LocalTime.ofSecondOfDay(iBatch(i).toLong()))
     }
 
     val timeMilliCol = exampleFrame["time32_milli"] as DataColumn<LocalTime?>
-    timeMilliCol.type() shouldBe typeOf<LocalTime?>()
+    timeMilliCol.type() shouldBe typeOf<LocalTime>().withNullability(nullable)
     timeMilliCol.forEachIndexed { i, element ->
-        element shouldBe LocalTime.ofNanoOfDay(iBatch(i).toLong() * 1000_000)
+        assertValueOrNull(iBatch(i), element, LocalTime.ofNanoOfDay(iBatch(i).toLong() * 1000_000))
     }
 
     val timeMicroCol = exampleFrame["time64_micro"] as DataColumn<LocalTime?>
-    timeMicroCol.type() shouldBe typeOf<LocalTime?>()
+    timeMicroCol.type() shouldBe typeOf<LocalTime>().withNullability(nullable)
     timeMicroCol.forEachIndexed { i, element ->
-        element shouldBe LocalTime.ofNanoOfDay(iBatch(i).toLong() * 1000)
+        assertValueOrNull(iBatch(i), element, LocalTime.ofNanoOfDay(iBatch(i).toLong() * 1000))
     }
 
     val timeNanoCol = exampleFrame["time64_nano"] as DataColumn<LocalTime?>
-    timeNanoCol.type() shouldBe typeOf<LocalTime?>()
+    timeNanoCol.type() shouldBe typeOf<LocalTime>().withNullability(nullable)
     timeNanoCol.forEachIndexed { i, element ->
-        element shouldBe LocalTime.ofNanoOfDay(iBatch(i).toLong())
+        assertValueOrNull(iBatch(i), element, LocalTime.ofNanoOfDay(iBatch(i).toLong()))
     }
 
 }
