d797fb484590824e7b8f898f5e437284694bcfad
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-07-29 15:41:46 +0300

    implement add.kt

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/plugin/add.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/plugin/add.kt
@@ -1,3 +1,10 @@
+package org.jetbrains.kotlinx.dataframe.plugin
+
+import org.jetbrains.kotlinx.dataframe.annotations.AbstractInterpreter
+import org.jetbrains.kotlinx.dataframe.annotations.Arguments
+import org.jetbrains.kotlinx.dataframe.annotations.TypeApproximation
+import org.jetbrains.kotlinx.dataframe.api.Infer
+
 internal class Add0 : AbstractInterpreter<PluginDataFrameSchema>() {
     val Arguments.name: String by string()
     val Arguments.infer: Infer by enum()
@@ -5,6 +12,6 @@ internal class Add0 : AbstractInterpreter<PluginDataFrameSchema>() {
     val Arguments.receiver: PluginDataFrameSchema by dataFrame()
 
     override fun Arguments.interpret(): PluginDataFrameSchema {
-        TODO()
+        return TODO()
     }
 }
