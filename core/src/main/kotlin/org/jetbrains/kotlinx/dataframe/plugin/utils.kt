9ff8d0bc8fb8b5988d66ed570d7c3908d45460ba
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-08-19 18:21:20 +0300

    Add utils to test dataframe operations and their plugin counterparts in REPL

---
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/plugin/utils.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.kotlinx.dataframe.plugin
+
+import org.jetbrains.kotlinx.dataframe.ColumnsSelector
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.getColumnsWithPaths
+
+@Suppress("UnusedReceiverParameter")
+public fun <T, C> DataFrame<T>.makeSelector(selector: ColumnsSelector<T, C>): ColumnsSelector<T, C> = selector
+
+public fun <T, C> ColumnsSelector<T, C>.toColumnsWithPath(df: DataFrame<T>): List<ColumnPathApproximation> = df
+    .getColumnsWithPaths(this)
+    .map { ColumnPathApproximation(it.path.path) }
