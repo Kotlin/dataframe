26790b170b13e661f5939a2c5cca48b5beb03590
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-11-23 20:22:40 +0200

    support type arguments in runtime schema -> compile time schema conversion

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/plugin/toPluginDataFrameSchema.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/plugin/toPluginDataFrameSchema.kt
@@ -1,20 +1,15 @@
 package org.jetbrains.kotlinx.dataframe.plugin
 
 import org.jetbrains.kotlinx.dataframe.KotlinTypeFacade
-import org.jetbrains.kotlinx.dataframe.annotations.TypeApproximationImpl
 import org.jetbrains.kotlinx.dataframe.schema.ColumnSchema
 import org.jetbrains.kotlinx.dataframe.schema.DataFrameSchema
-import kotlin.reflect.KClass
 
 public val KotlinTypeFacade.toPluginDataFrameSchema: DataFrameSchema.() -> PluginDataFrameSchema get() =  {
     PluginDataFrameSchema(
         columns = columns.map { (name, columnSchema) ->
             when (columnSchema) {
                 is ColumnSchema.Value -> {
-                    val classifier = columnSchema.type.classifier ?: error("")
-                    val klass = classifier as? KClass<*> ?: error("$columnSchema")
-                    val fqName = klass.qualifiedName ?: error("")
-                    val type = TypeApproximationImpl(fqName, columnSchema.nullable)
+                    val type = from(columnSchema.type)
                     SimpleCol(name, type)
                     // class Data(val i: Int) // local class
                     // val df = dataFrameOf("col")(Data(1))
--- plugins/type-api/src/main/kotlin/org/jetbrains/kotlinx/dataframe/Main.kt
+++ plugins/type-api/src/main/kotlin/org/jetbrains/kotlinx/dataframe/Main.kt
@@ -4,7 +4,12 @@ import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.symbols.impl.ConeClassLikeLookupTagImpl
 import org.jetbrains.kotlin.fir.types.ConeClassLikeType
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
+import org.jetbrains.kotlin.fir.types.ConeKotlinTypeProjectionIn
+import org.jetbrains.kotlin.fir.types.ConeKotlinTypeProjectionOut
 import org.jetbrains.kotlin.fir.types.ConeNullability
+import org.jetbrains.kotlin.fir.types.ConeStarProjection
+import org.jetbrains.kotlin.fir.types.ConeTypeProjection
+import org.jetbrains.kotlin.fir.types.constructClassLikeType
 import org.jetbrains.kotlin.fir.types.constructType
 import org.jetbrains.kotlin.fir.types.impl.ConeClassLikeTypeImpl
 import org.jetbrains.kotlin.fir.types.isNullable
@@ -14,6 +19,10 @@ import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.name.StandardClassIds
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+import kotlin.reflect.KTypeProjection
+import kotlin.reflect.KVariance
 
 val DF_CLASS_ID: ClassId
     get() = ClassId.topLevel(FqName.fromSegments(listOf("org", "jetbrains", "kotlinx", "dataframe", "DataFrame")))
@@ -59,6 +68,47 @@ interface KotlinTypeFacade {
             )
         ).constructType(emptyArray(), nullable)
         return Marker(type)
+    }
+
+    fun from(type: KType): Marker {
+        return Marker(fromImpl(type))
+    }
+
+    private fun fromImpl(type: KType): ConeClassLikeType {
+        val classId = type.classId()
+        val coneType = classId.constructClassLikeType(
+            typeArguments = type.arguments.mapToConeTypeProjection(),
+            isNullable = type.isMarkedNullable
+        )
+        return coneType
+    }
+
+    fun KType.classId(): ClassId {
+        val classifier = classifier ?: error("")
+        val klass = classifier as? KClass<*> ?: error("")
+        val fqName = klass.qualifiedName ?: error("")
+        return ClassId(
+            FqName(fqName.substringBeforeLast(".", missingDelimiterValue = "")),
+            Name.identifier(fqName.substringAfterLast("."))
+        )
+    }
+
+    private fun List<KTypeProjection>.mapToConeTypeProjection(): Array<out ConeTypeProjection> {
+        return Array(size) {
+            val typeProjection = get(it)
+            val type = typeProjection.type
+            val variance = typeProjection.variance
+            if (type != null && variance != null) {
+                val coneType = fromImpl(type)
+                when (variance) {
+                    KVariance.INVARIANT -> coneType
+                    KVariance.IN -> ConeKotlinTypeProjectionIn(coneType)
+                    KVariance.OUT -> ConeKotlinTypeProjectionOut(coneType)
+                }
+            } else {
+                ConeStarProjection
+            }
+        }
     }
 
     fun Marker.changeNullability(map: (Boolean) -> Boolean): Marker {
