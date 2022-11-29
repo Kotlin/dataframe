7bb2ef71c3f9672623fc721be1b33b4b14966522
Kopilov Aleksandr <kopilov.ad@gmail.com>
2022-11-29 13:35:17 +0300

    Prettify per linter

--- dataframe-arrow/src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/ConvertingMismatch.kt
+++ dataframe-arrow/src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/ConvertingMismatch.kt
@@ -16,49 +16,66 @@ public sealed class ConvertingMismatch(
     public open val cause: Exception?
 ) {
 
-    public sealed class WideningMismatch(column: String): ConvertingMismatch(column, null, null) {
-        public data class AddedColumn(override val column: String): WideningMismatch(column) {
+    public sealed class WideningMismatch(column: String) : ConvertingMismatch(column, null, null) {
+        public data class AddedColumn(override val column: String) : WideningMismatch(column) {
             override fun toString(): String = "Added column \"$column\" not described in target schema"
         }
-        public data class RejectedColumn(override val column: String): WideningMismatch(column) {
+        public data class RejectedColumn(override val column: String) : WideningMismatch(column) {
             override fun toString(): String = "Column \"$column\" is not described in target schema and was ignored"
         }
     }
-    public sealed class NarrowingMismatch(column: String): ConvertingMismatch(column, null, null) {
-        public data class NotPresentedColumnIgnored(override val column: String): NarrowingMismatch(column) {
+
+    public sealed class NarrowingMismatch(column: String) : ConvertingMismatch(column, null, null) {
+        public data class NotPresentedColumnIgnored(override val column: String) : NarrowingMismatch(column) {
             override fun toString(): String = "Not nullable column \"$column\" is not presented in actual data, saving as is"
         }
-        public data class NotPresentedColumnError(override val column: String): NarrowingMismatch(column) {
+        public data class NotPresentedColumnError(override val column: String) : NarrowingMismatch(column) {
             override fun toString(): String = "Not nullable column \"$column\" is not presented in actual data, can not save"
         }
     }
-    public sealed class TypeConversionNotFound(column: String, cause: TypeConverterNotFoundException): ConvertingMismatch(column, null, cause) {
-        public data class ConversionNotFoundIgnored(override val column: String, override val cause: TypeConverterNotFoundException): TypeConversionNotFound(column, cause) {
+
+    public sealed class TypeConversionNotFound(
+        column: String,
+        cause: TypeConverterNotFoundException
+    ) : ConvertingMismatch(column, null, cause) {
+        public data class ConversionNotFoundIgnored(override val column: String, override val cause: TypeConverterNotFoundException) : TypeConversionNotFound(column, cause) {
             override fun toString(): String = "${cause.message} for column \"$column\", saving as is"
         }
-        public data class ConversionNotFoundError(override val column: String, val e: TypeConverterNotFoundException): TypeConversionNotFound(column, e) {
+        public data class ConversionNotFoundError(override val column: String, val e: TypeConverterNotFoundException) : TypeConversionNotFound(column, e) {
             override fun toString(): String = "${e.message} for column \"$column\", can not save"
         }
     }
-    public sealed class TypeConversionFail(column: String, row: Int?, public override val cause: CellConversionException): ConvertingMismatch(column, row, cause) {
-        public data class ConversionFailIgnored(override val column: String, override val row: Int?, override val cause: CellConversionException): TypeConversionFail(column, row, cause) {
+
+    public sealed class TypeConversionFail(
+        column: String, row: Int?,
+        public override val cause: CellConversionException
+    ) : ConvertingMismatch(column, row, cause) {
+        public data class ConversionFailIgnored(override val column: String, override val row: Int?, override val cause: CellConversionException) : TypeConversionFail(column, row, cause) {
             override fun toString(): String = "${cause.message}, saving as is"
         }
-        public data class ConversionFailError(override val column: String, override val row: Int?, override val cause: CellConversionException): TypeConversionFail(column, row, cause) {
+        public data class ConversionFailError(override val column: String, override val row: Int?, override val cause: CellConversionException) : TypeConversionFail(column, row, cause) {
             override fun toString(): String = "${cause.message}, can not save"
         }
     }
-    public data class SavedAsString(override val column: String, val type: Class<*>): ConvertingMismatch(column, null, null) {
+
+    public data class SavedAsString(
+        override val column: String,
+        val type: Class<*>
+    ) : ConvertingMismatch(column, null, null) {
         override fun toString(): String = "Column \"$column\" has type ${type.canonicalName}, will be saved as String\""
     }
-    public sealed class NullableMismatch(column: String, row: Int?): ConvertingMismatch(column, row, null) {
-        public data class NullValueIgnored(override val column: String, override val row: Int?): NullableMismatch(column, row) {
+
+    public sealed class NullableMismatch(
+        column: String,
+        row: Int?
+    ) : ConvertingMismatch(column, row, null) {
+        public data class NullValueIgnored(override val column: String, override val row: Int?) : NullableMismatch(column, row) {
             override fun toString(): String = "Column \"$column\" contains nulls in row $row but expected not nullable, saving as is"
         }
-        public data class NullValueError(override val column: String, override val row: Int?): NullableMismatch(column, row) {
+        public data class NullValueError(override val column: String, override val row: Int?) : NullableMismatch(column, row) {
             override fun toString(): String = "Column \"$column\" contains nulls in row $row but expected not nullable, can not save"
         }
     }
 }
 
-public class ConvertingException(public val mismatchCase: ConvertingMismatch): IllegalArgumentException(mismatchCase.toString(), mismatchCase.cause)
+public class ConvertingException(public val mismatchCase: ConvertingMismatch) : IllegalArgumentException(mismatchCase.toString(), mismatchCase.cause)
