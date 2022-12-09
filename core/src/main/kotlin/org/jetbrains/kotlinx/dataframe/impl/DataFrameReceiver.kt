78b3e8bd1318ac6b7632e9c95fc49d638ba83706
Jolan Rensen <jolan.rensen@jetbrains.com>
2022-12-09 12:59:44 +0100

    Small refactorings and added docs

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/DataFrameReceiver.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/DataFrameReceiver.kt
@@ -36,12 +36,17 @@ internal open class DataFrameReceiver<T>(
     private val unresolvedColumnsPolicy: UnresolvedColumnsPolicy
 ) : DataFrameReceiverBase<T>(source.unbox()), SingleColumn<DataRow<T>> {
 
-    private fun <R> DataColumn<R>?.check(path: ColumnPath): DataColumn<R>? =
+    private fun <R> DataColumn<R>?.check(path: ColumnPath): DataColumn<R> =
         when (this) {
             null -> when (unresolvedColumnsPolicy) {
-                UnresolvedColumnsPolicy.Create, UnresolvedColumnsPolicy.Skip -> MissingColumnGroup<Any>(path, this@DataFrameReceiver).asDataColumn().cast()
+                UnresolvedColumnsPolicy.Create, UnresolvedColumnsPolicy.Skip -> MissingColumnGroup<Any>(
+                    path,
+                    this@DataFrameReceiver
+                ).asDataColumn().cast()
+
                 UnresolvedColumnsPolicy.Fail -> error("Column $path not found")
             }
+
             is MissingDataColumn -> this
             is ColumnGroup<*> -> ColumnGroupWithParent(null, this).asDataColumn().cast()
             else -> this
