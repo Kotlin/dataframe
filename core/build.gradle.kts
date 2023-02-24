15026b7e0264e276f4a1f72c28220d7503ef5692
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-02-24 12:48:30 +0100

    updated doc processor gradle plugin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("org.jetbrains.kotlinx.kover")
     id("org.jmailen.kotlinter")
     id("org.jetbrains.kotlinx.dataframe")
-    id("com.github.jolanrensen.docProcessorGradlePlugin") version "v0.0.19"
+    id("com.github.jolanrensen.docProcessorGradlePlugin") version "v0.0.20"
 //    id("nl.jolanrensen.docProcessor") version "1.0-SNAPSHOT"
 }
 
