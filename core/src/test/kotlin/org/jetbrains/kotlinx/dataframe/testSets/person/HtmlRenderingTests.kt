b00b8b4ff679868146cf0984b10f5ca1e1fee030
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2023-03-17 21:06:09 +0200

    Introduce DataFrameHtmlData API

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/testSets/person/HtmlRenderingTests.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/testSets/person/HtmlRenderingTests.kt
@@ -7,8 +7,6 @@ import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
 import org.jetbrains.kotlinx.dataframe.api.group
 import org.jetbrains.kotlinx.dataframe.api.into
 import org.jetbrains.kotlinx.dataframe.api.parse
-import org.jetbrains.kotlinx.dataframe.io.initHtml
-import org.jetbrains.kotlinx.dataframe.io.toHTML
 import org.jetbrains.kotlinx.dataframe.io.toStandaloneHTML
 import org.jetbrains.kotlinx.jupyter.findNthSubstring
 import org.junit.Ignore
@@ -20,7 +18,7 @@ class HtmlRenderingTests : BaseTest() {
 
     fun AnyFrame.browse() {
         val file = File("temp.html") // File.createTempFile("df_rendering", ".html")
-        file.writeText(toHTML(extraHtml = initHtml()).toString())
+        file.writeText(toStandaloneHTML().toString())
         val uri = file.toURI()
         val desktop = Desktop.getDesktop()
         desktop.browse(uri)
@@ -36,7 +34,7 @@ class HtmlRenderingTests : BaseTest() {
     fun `render url`() {
         val address = "http://www.google.com"
         val df = dataFrameOf("url")(address).parse()
-        val html = df.toStandaloneHTML()
+        val html = df.toStandaloneHTML().toString()
         html shouldContain "href"
         html.findNthSubstring(address, 2) shouldNotBe -1
     }
