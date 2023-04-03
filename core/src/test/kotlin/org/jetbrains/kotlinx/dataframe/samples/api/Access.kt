ac7f4655185e6e5442e00eb773d28ff2ffc58891
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-04-03 19:58:03 +0200

    added first and last to docs

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Access.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Access.kt
@@ -812,6 +812,14 @@ class Access : TestBase() {
         df.select { drop(2) }
         df.select { dropLast(2) }
 
+        // find the first column satisfying the condition
+        df.select { first { it.name().startsWith("year") } }
+
+        // find the last column inside a column group satisfying the condition
+        df.select {
+            group("name").last { it.name().endsWith("Name") }
+        }
+
         // depth-first-search traversal of all columns, excluding ColumnGroups from result
         df.select { allDfs() }
 
