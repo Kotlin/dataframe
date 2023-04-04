139fd0f59b0b1a2d9d9520728678e9b59c1b4b63
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-04-04 15:01:39 +0200

    fixed single function with docs

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/ColumnsSelectionDsl.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/ColumnsSelectionDsl.kt
@@ -11,7 +11,6 @@ import org.jetbrains.kotlinx.dataframe.DataColumn
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.DataRow
 import org.jetbrains.kotlinx.dataframe.Predicate
-import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
 import org.jetbrains.kotlinx.dataframe.columns.ColumnAccessor
 import org.jetbrains.kotlinx.dataframe.columns.ColumnGroup
 import org.jetbrains.kotlinx.dataframe.columns.ColumnPath
@@ -38,12 +37,11 @@ import org.jetbrains.kotlinx.dataframe.impl.columns.changePath
 import org.jetbrains.kotlinx.dataframe.impl.columns.createColumnSet
 import org.jetbrains.kotlinx.dataframe.impl.columns.getAt
 import org.jetbrains.kotlinx.dataframe.impl.columns.getChildrenAt
-import org.jetbrains.kotlinx.dataframe.impl.columns.single
+import org.jetbrains.kotlinx.dataframe.impl.columns.singleImpl
 import org.jetbrains.kotlinx.dataframe.impl.columns.top
 import org.jetbrains.kotlinx.dataframe.impl.columns.transform
 import org.jetbrains.kotlinx.dataframe.impl.columns.transformSingle
 import org.jetbrains.kotlinx.dataframe.impl.columns.tree.dfs
-import org.jetbrains.kotlinx.dataframe.io.read
 import kotlin.reflect.KProperty
 import kotlin.reflect.KType
 import kotlin.reflect.typeOf
@@ -208,6 +206,7 @@ public interface ColumnsSelectionDsl<out T> : ColumnSelectionDsl<T>, SingleColum
      *
      * @param [condition\] The optional [ColumnFilter] condition that the column must adhere to.
      * @return A [SingleColumn] containing the first column that adheres to the given [condition\].
+     * @throws [NoSuchElementException\] if no column adheres to the given [condition\].
      * @see [last\]
      */
     private interface CommonFirstDocs {
@@ -224,7 +223,7 @@ public interface ColumnsSelectionDsl<out T> : ColumnSelectionDsl<T>, SingleColum
      * `df.`[select][select]` { `[colsOf][colsOf]`<`[Int][Int]`>().`[first][first]`() }`
      */
     public fun <C> ColumnSet<C>.first(condition: ColumnFilter<C> = { true }): SingleColumn<C> =
-        transform { listOf(it.first(condition)) }.single()
+        transform { listOf(it.first(condition)) }.singleImpl()
 
     /**
      * @include [CommonFirstDocs]
@@ -259,7 +258,7 @@ public interface ColumnsSelectionDsl<out T> : ColumnSelectionDsl<T>, SingleColum
      * @arg [CommonFirstDocs.Examples]
      * `df.`[select][select]` { Type::myColumnGroup.`[first][first]` { it.`[name][ColumnReference.name]`().`[startsWith][String.startsWith]`("year") } }`
      */
-    public fun KProperty<*>.first(condition: ColumnFilter<*>): SingleColumn<*> =
+    public fun KProperty<*>.first(condition: ColumnFilter<*> = { true }): SingleColumn<*> =
         getColumnGroup(this).first(condition)
 
 
@@ -273,6 +272,7 @@ public interface ColumnsSelectionDsl<out T> : ColumnSelectionDsl<T>, SingleColum
      *
      * @param [condition\] The optional [ColumnFilter] condition that the column must adhere to.
      * @return A [SingleColumn] containing the last column that adheres to the given [condition\].
+     * @throws [NoSuchElementException\] if no column adheres to the given [condition\].
      * @see [first\]
      */
     private interface CommonLastDocs {
@@ -289,7 +289,7 @@ public interface ColumnsSelectionDsl<out T> : ColumnSelectionDsl<T>, SingleColum
      * `df.`[select][select]` { `[colsOf][colsOf]`<`[Int][Int]`>().`[first][last]`() }`
      */
     public fun <C> ColumnSet<C>.last(condition: ColumnFilter<C> = { true }): SingleColumn<C> =
-        transform { listOf(it.last(condition)) }.single()
+        transform { listOf(it.last(condition)) }.singleImpl()
 
     /**
      * @include [CommonLastDocs]
@@ -324,13 +324,76 @@ public interface ColumnsSelectionDsl<out T> : ColumnSelectionDsl<T>, SingleColum
      * @arg [CommonLastDocs.Examples]
      * `df.`[select][select]` { Type::myColumnGroup.`[last][last]` { it.`[name][ColumnReference.name]`().`[startsWith][String.startsWith]`("year") } }`
      */
-    public fun KProperty<*>.last(condition: ColumnFilter<*>): SingleColumn<*> =
+    public fun KProperty<*>.last(condition: ColumnFilter<*> = { true }): SingleColumn<*> =
         getColumnGroup(this).last(condition)
 
-    public fun <C> ColumnSet<C>.single(condition: ColumnFilter<C>): SingleColumn<C> =
-        transform { listOf(it.single(condition)) }.single()
 
-    public fun SingleColumn<AnyRow>.col(index: Int): SingleColumn<Any?> = getChildrenAt(index).single()
+    /**
+     * ## Single
+     * Returns the single column in this [ColumnSet] or [ColumnGroup] that adheres to the given [condition\].
+     *
+     * For example:
+     *
+     * {@includeArg [Examples]}
+     *
+     * @param [condition\] The optional [ColumnFilter] condition that the column must adhere to.
+     * @return A [SingleColumn] containing the single column that adheres to the given [condition\].
+     * @throws [NoSuchElementException\] if no column adheres to the given [condition\].
+     * @throws [IllegalArgumentException\] if more than one column adheres to the given [condition\].
+     */
+    private interface CommonSingleDocs {
+
+        /** Examples key */
+        interface Examples
+    }
+
+    /**
+     * @include [CommonSingleDocs]
+     * @arg [CommonSingleDocs.Examples]
+     * `df.`[select][select]` { `[colsOf][colsOf]`<`[String][String]`>().`[single][single]` { it.`[name][ColumnReference.name]`().`[startsWith][String.startsWith]`("year") } }`
+     *
+     * `df.`[select][select]` { `[colsOf][colsOf]`<`[Int][Int]`>().`[single][single]`() }`
+     */
+    public fun <C> ColumnSet<C>.single(condition: ColumnFilter<C> = { true }): SingleColumn<C> =
+        transform { listOf(it.single(condition)) }.singleImpl()
+
+    /**
+     * @include [CommonSingleDocs]
+     * @arg [CommonSingleDocs.Examples]
+     * `df.`[select][select]` { `[single][single]` { it.`[name][ColumnReference.name]`().`[startsWith][String.startsWith]`("year") } }`
+     *
+     * `df.`[select][select]` { `[single][single]`() }`
+     */
+    public fun single(condition: ColumnFilter<*> = { true }): SingleColumn<*> =
+        all().single(condition)
+
+    /**
+     * @include [CommonSingleDocs]
+     * @arg [CommonSingleDocs.Examples]
+     * `df.`[select][select]` { myColumnGroup.`[single][single]` { it.`[name][ColumnReference.name]`().`[startsWith][String.startsWith]`("year") } }`
+     *
+     * `df.`[select][select]` { myColumnGroup.`[single][single]`() }`
+     */
+    public fun ColumnGroupReference.single(condition: ColumnFilter<*> = { true }): SingleColumn<*> =
+        all().single(condition)
+
+    /**
+     * @include [CommonSingleDocs]
+     * @arg [CommonSingleDocs.Examples]
+     * `df.`[select][select]` { "myColumnGroup".`[single][single]` { it.`[name][ColumnReference.name]`().`[startsWith][String.startsWith]`("year") } }`
+     */
+    public fun String.single(condition: ColumnFilter<*> = { true }): SingleColumn<*> =
+        getColumnGroup(this).single(condition)
+
+    /**
+     * @include [CommonSingleDocs]
+     * @arg [CommonSingleDocs.Examples]
+     * `df.`[select][select]` { Type::myColumnGroup.`[single][single]` { it.`[name][ColumnReference.name]`().`[startsWith][String.startsWith]`("year") } }`
+     */
+    public fun KProperty<*>.single(condition: ColumnFilter<*> = { true }): SingleColumn<*> =
+        getColumnGroup(this).single(condition)
+
+    public fun SingleColumn<AnyRow>.col(index: Int): SingleColumn<Any?> = getChildrenAt(index).singleImpl()
 
     public operator fun <C> ColumnSet<C>.get(index: Int): SingleColumn<C> = getAt(index)
 
