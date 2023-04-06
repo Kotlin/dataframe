af2a32b3518c9bd82f0ef1d5f4d6165ee338e056
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2023-04-06 20:45:18 +0300

    resize iframes in all possible states

--- docs/StardustDocs/cfg/resize-iframes.js
+++ docs/StardustDocs/cfg/resize-iframes.js
@@ -5,7 +5,7 @@
             el.height = h === 0 ? 0 : h + 41;
         }
 
-        function observeIFrame(el) {
+        function doObserveIFrame(el) {
             resize_iframe_out(el)
             let observer = new MutationObserver(function (mutations) {
                 // if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
@@ -17,18 +17,32 @@
 
         }
 
+        function observeIFrame(el) {
+            if (el.contentWindow && el.contentWindow.performance && el.contentWindow.performance.timing.loadEventEnd === 0) {
+                el.addEventListener('load', () => doObserveIFrame(el), true)
+            } else {
+                doObserveIFrame(el)
+            }
+        }
+
         let iframes = document.querySelectorAll('iframe');
 
         for (let i = 0; i < iframes.length; i++) {
             observeIFrame(iframes[i])
-        //     resize_iframe_out(iframes[i])
-        //     let observer = new MutationObserver(function (mutations) {
-        //     // if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
-        //         resize_iframe_out(iframes[i])
-        //     // }
-        // });
-        //
-        //     observer.observe(iframes[i].contentDocument.documentElement, {childList: true, subtree: true, characterData: true, attributes: true});
         }
+
+        let bodyObserver = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                for (let i = 0; i < mutation.addedNodes.length; i++) {
+                    let addedNode = mutation.addedNodes[i];
+                    if (addedNode.tagName === 'IFRAME') {
+                        console.log("added iframe")
+                        observeIFrame(addedNode);
+                    }
+                }
+            });
+        });
+
+        bodyObserver.observe(document.body, {childList: true, subtree: true, attributes: true});
     }
 </script>
