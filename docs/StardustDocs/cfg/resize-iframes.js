ce4379d66bcb9c128b223e6dabf8d89a5c6391df
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2023-04-06 19:55:12 +0300

    refactor script

--- docs/StardustDocs/cfg/resize-iframes.js
+++ docs/StardustDocs/cfg/resize-iframes.js
@@ -5,17 +5,30 @@
             el.height = h === 0 ? 0 : h + 41;
         }
 
+        function observeIFrame(el) {
+            resize_iframe_out(el)
+            let observer = new MutationObserver(function (mutations) {
+                // if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+                resize_iframe_out(el)
+                // }
+            });
+
+            observer.observe(el.contentDocument.documentElement, {childList: true, subtree: true, characterData: true, attributes: true});
+
+        }
+
         let iframes = document.querySelectorAll('iframe');
 
         for (let i = 0; i < iframes.length; i++) {
-            resize_iframe_out(iframes[i])
-            let observer = new MutationObserver(function (mutations) {
-            // if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
-                resize_iframe_out(iframes[i])
-            // }
-        });
-
-            observer.observe(iframes[i].contentDocument.documentElement, {childList: true, subtree: true, characterData: true, attributes: true});
+            observeIFrame(iframes[i])
+        //     resize_iframe_out(iframes[i])
+        //     let observer = new MutationObserver(function (mutations) {
+        //     // if (mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+        //         resize_iframe_out(iframes[i])
+        //     // }
+        // });
+        //
+        //     observer.observe(iframes[i].contentDocument.documentElement, {childList: true, subtree: true, characterData: true, attributes: true});
         }
     }
 </script>
