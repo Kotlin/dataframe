c2866e0db662e901788b707080ec296457af996e
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2023-04-07 20:49:05 +0300

    traverse newly added nodes to find iframes
    keep logging for now to catch possible bugs

--- docs/StardustDocs/cfg/resize-iframes.js
+++ docs/StardustDocs/cfg/resize-iframes.js
@@ -18,9 +18,17 @@
         }
 
         function observeIFrame(el) {
+            console.log("el.contentWindow: " + el.contentWindow)
+            console.log("el.contentWindow.document: " + el.contentWindow.document)
+            console.log("el.contentWindow.document.body: " + el.contentWindow.document.body)
+            console.log("el.contentWindow.performance: " + el.contentWindow.performance)
+            console.log("el.contentWindow.performance.timing.loadEventEnd: " + el.contentWindow.performance.timing.loadEventEnd)
+
             if (el.contentWindow && el.contentWindow.performance && el.contentWindow.performance.timing.loadEventEnd === 0) {
+                console.log("ready path")
                 el.addEventListener('load', () => doObserveIFrame(el), true)
             } else {
+                console.log("not ready path")
                 doObserveIFrame(el)
             }
         }
@@ -35,14 +43,29 @@
             mutations.forEach(function (mutation) {
                 for (let i = 0; i < mutation.addedNodes.length; i++) {
                     let addedNode = mutation.addedNodes[i];
+                    console.log(addedNode.tagName)
                     if (addedNode.tagName === 'IFRAME') {
-                        console.log("added iframe")
+                        console.log("IFRAME loaded")
                         observeIFrame(addedNode);
+                    } else if (addedNode.tagName === 'SECTION') {
+                        let iframes = [];
+
+                        function traverse(node) {
+                            if (node.tagName === 'IFRAME') {
+    observeIFrame(node);
+                        }
+
+                        for (let i = 0; i < node.children.length; i++) {
+                            traverse(node.children[i]);
+                        }
+                    }
+
+                        traverse(addedNode);
                     }
                 }
             });
         });
 
-        bodyObserver.observe(document.body, {childList: true, subtree: true, attributes: true});
+        bodyObserver.observe(document.documentElement || document.body, {childList: true, subtree: true, attributes: true});
     }
 </script>
