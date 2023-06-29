d447799e52f22f46cbb86132a5eda382204ab732
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-06-29 18:06:06 +0200

    added map function for TreeNode. improved renameImpl according to reviews and using new map function

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/api/rename.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/api/rename.kt
@@ -2,14 +2,16 @@ package org.jetbrains.kotlinx.dataframe.impl.api
 
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.RenameClause
+import org.jetbrains.kotlinx.dataframe.api.asColumnGroup
 import org.jetbrains.kotlinx.dataframe.api.cast
 import org.jetbrains.kotlinx.dataframe.api.getColumnsWithPaths
-import org.jetbrains.kotlinx.dataframe.api.insert
-import org.jetbrains.kotlinx.dataframe.api.under
+import org.jetbrains.kotlinx.dataframe.api.toDataFrame
+import org.jetbrains.kotlinx.dataframe.columns.ColumnGroup
 import org.jetbrains.kotlinx.dataframe.columns.ColumnKind
 import org.jetbrains.kotlinx.dataframe.columns.ColumnWithPath
 import org.jetbrains.kotlinx.dataframe.impl.columns.tree.allChildrenNotNull
 import org.jetbrains.kotlinx.dataframe.impl.columns.tree.collectTree
+import org.jetbrains.kotlinx.dataframe.impl.columns.tree.map
 import org.jetbrains.kotlinx.dataframe.kind
 
 internal fun <T, C> RenameClause<T, C>.renameImpl(newNames: Array<out String>): DataFrame<T> {
@@ -18,29 +20,40 @@ internal fun <T, C> RenameClause<T, C>.renameImpl(newNames: Array<out String>): 
 }
 
 internal fun <T, C> RenameClause<T, C>.renameImpl(transform: (ColumnWithPath<C>) -> String): DataFrame<T> {
-    val selectedColumns = df.getColumnsWithPaths(columns)
+    // get all selected columns and their paths
+    val selectedColumnsWithPath = df.getColumnsWithPaths(columns)
+        .associateBy { it.data }
+    // gather a tree of all columns where the nodes will be renamed
     val tree = df.getColumnsWithPaths { all().rec() }.collectTree()
 
     // perform rename in nodes
     tree.allChildrenNotNull().forEach { node ->
-        val column = selectedColumns.find { it.data == node.data } ?: return@forEach
-        val newName = transform(column)
-        node.name = newName
+        // Check if the current node/column is a selected column and, if so, get its ColumnWithPath
+        val column = selectedColumnsWithPath[node.data] ?: return@forEach
+        // Use the found selected ColumnWithPath to query for the new name
+        val newColumnName = transform(column)
+        node.name = newColumnName
     }
 
-    // build up a new DataFrame using the modified names
-    var newDf = DataFrame.empty(df.rowsCount()).cast<T>()
-    tree.allChildrenNotNull().forEach { node ->
-        val path = node.pathFromRoot().dropLast(1)
-        val col = node.data.rename(node.name)
-
-        when (col.kind) {
+    // use the mapping function to convert the tree to a ColumnGroup/ValueColumn structure
+    // The result will be a ColumnGroup, since the root node's data is null
+    val renamedDfAsColumnGroup = tree.map { node, children ->
+        val col = node.data
+        when (col?.kind) {
+            // if the column is a value column or a frame column, rename it using the node's (new) name
             ColumnKind.Value, ColumnKind.Frame ->
-                newDf = newDf.insert(col).under(path)
+                col.rename(node.name)
 
-            ColumnKind.Group -> Unit
+            // if the column is a group column, create a new column group using the node's (new) name and children
+            // if the column is null, node is the root, so we'll create a column group as well
+            ColumnKind.Group, null ->
+                children
+                    .toDataFrame()
+                    .asColumnGroup(node.name)
         }
-    }
+    } as ColumnGroup<*>
 
-    return newDf
+    // convert the created ColumnGroup to a DataFrame
+    val renamedDf = renamedDfAsColumnGroup.columns().toDataFrame()
+    return renamedDf.cast()
 }
