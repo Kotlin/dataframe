d66323a3c02a967c69e7208c60f2ff2137ee551c
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-07-24 17:45:55 +0200

    added deprecation messages instructions to RELEASE_CHECK_LIST.md and updated the file with regions and hints to when a feature will be removed

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/util/deprecationMessages.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/util/deprecationMessages.kt
@@ -1,13 +1,29 @@
 package org.jetbrains.kotlinx.dataframe.util
 
-internal const val DF_READ_DEPRECATION_MESSAGE = "Replaced with `unfold` operation."
+/*
+ * This file contains deprecation messages for the whole core module.
+ * After each release, all messages should be reviewed and updated.
+ * Level.WARNING -> Level.ERROR
+ * Level.ERROR -> Remove
+ */
 
+// region WARNING in 0.10.0, ERROR in 0.11.0
+
+internal const val DF_READ_DEPRECATION_MESSAGE = "Replaced with `unfold` operation. Removed in 0.11.0."
 internal const val DF_READ_REPLACE_MESSAGE = "this.unfold(*columns)"
 
-internal const val ITERABLE_COLUMNS_DEPRECATION_MESSAGE = "Replaced with `toColumnSet()` operation."
+internal const val ITERABLE_COLUMNS_DEPRECATION_MESSAGE = "Replaced with `toColumnSet()` operation. Removed in 0.11.0."
 
-internal const val DIFF_DEPRECATION_MESSAGE = "Replaced to explicitly indicate nullable return value; added a new non-null overload."
+// endregion
 
+// region WARNING in 0.11.0, ERROR in 0.12.0
+
+internal const val DIFF_DEPRECATION_MESSAGE = "Replaced to explicitly indicate nullable return value; added a new non-null overload. Will be removed in 0.12.0."
 internal const val DIFF_REPLACE_MESSAGE = "this.diffOrNull(expression)"
-
 internal const val DIFF_OR_NULL_IMPORT = "org.jetbrains.kotlinx.dataframe.api.diffOrNull"
+
+// endregion
+
+// region WARNING in 0.12.0, ERROR in 0.13.0
+
+// endregion
