4f7cf1fe3f9b2e26811d1d263c74f43823ce6264
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-07-25 13:08:44 +0200

    added update deprecation messages to central file

--- core/generated-sources/src/main/kotlin/org/jetbrains/kotlinx/dataframe/util/deprecationMessages.kt
+++ core/generated-sources/src/main/kotlin/org/jetbrains/kotlinx/dataframe/util/deprecationMessages.kt
@@ -1,13 +1,35 @@
 package org.jetbrains.kotlinx.dataframe.util
 
-internal const val DF_READ_DEPRECATION_MESSAGE = "Replaced with `unfold` operation."
+/*
+ * This file contains deprecation messages for the whole core module.
+ * After each release, all messages should be reviewed and updated.
+ * Level.WARNING -> Level.ERROR
+ * Level.ERROR -> Remove
+ */
 
+// region WARNING in 0.10.0, ERROR in 0.11.0
+
+internal const val DF_READ_DEPRECATION_MESSAGE = "Replaced with `unfold` operation. Removed in 0.11.0."
 internal const val DF_READ_REPLACE_MESSAGE = "this.unfold(*columns)"
 
-internal const val ITERABLE_COLUMNS_DEPRECATION_MESSAGE = "Replaced with `toColumnSet()` operation."
+internal const val ITERABLE_COLUMNS_DEPRECATION_MESSAGE = "Replaced with `toColumnSet()` operation. Removed in 0.11.0."
 
-internal const val DIFF_DEPRECATION_MESSAGE = "Replaced to explicitly indicate nullable return value; added a new non-null overload."
+// endregion
 
+// region WARNING in 0.11.0, ERROR in 0.12.0
+
+internal const val DIFF_DEPRECATION_MESSAGE = "Replaced to explicitly indicate nullable return value; added a new non-null overload. Will be removed in 0.12.0."
 internal const val DIFF_REPLACE_MESSAGE = "this.diffOrNull(expression)"
-
 internal const val DIFF_OR_NULL_IMPORT = "org.jetbrains.kotlinx.dataframe.api.diffOrNull"
+
+internal const val UPDATE_AS_NULLABLE_MESSAGE = "This function is useless unless in combination with `withValue(null)`, but then you can just use `with { null }`. Will be removed in 0.12.0."
+internal const val UPDATE_AS_NULLABLE_REPLACE = "this as Update<T, C?>"
+
+internal const val UPDATE_WITH_VALUE = "Replaced in favor of `with { value }`. Will be removed in 0.12.0."
+internal const val UPDATE_WITH_VALUE_REPLACE = "this.with { value }"
+
+// endregion
+
+// region WARNING in 0.12.0, ERROR in 0.13.0
+
+// endregion
