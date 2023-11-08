eff8fbb2c4179c77d5b14612b05e127ab5736718
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-11-08 12:42:38 +0100

    improve none() test

--- core/generated-sources/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/none.kt
+++ core/generated-sources/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/none.kt
@@ -3,6 +3,8 @@ package org.jetbrains.kotlinx.dataframe.api
 import io.kotest.matchers.shouldBe
 import org.jetbrains.kotlinx.dataframe.ncol
 import org.jetbrains.kotlinx.dataframe.nrow
+import org.jetbrains.kotlinx.dataframe.samples.api.age
+import org.jetbrains.kotlinx.dataframe.samples.api.name
 import org.junit.Test
 
 class NoneTests : ColumnsSelectionDslTests() {
@@ -13,5 +15,11 @@ class NoneTests : ColumnsSelectionDslTests() {
             it.nrow shouldBe 0
             it.ncol shouldBe 0
         }
+
+        listOf(
+            df.select { none() and age },
+            df.select { age },
+            df.select { name.select { none() } and age },
+        ).shouldAllBeEqual()
     }
 }
