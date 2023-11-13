8b96861d70f50806f951498e11105a9bda97802f
Aleksei Zinovev <zaleslaw.sin@gmail.com>
2023-11-13 15:26:22 +0100

    Update Ktor dependency and adjust related imports (#492)

--- plugins/symbol-processor/src/test/kotlin/org/jetbrains/dataframe/ksp/EmbeddedServerRunners.kt
+++ plugins/symbol-processor/src/test/kotlin/org/jetbrains/dataframe/ksp/EmbeddedServerRunners.kt
@@ -1,10 +1,10 @@
 package org.jetbrains.dataframe.ksp
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import java.io.File
 
 fun useHostedFile(file: File, f: (url: String) -> Unit) {
