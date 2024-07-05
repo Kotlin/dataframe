92965448045485e4f8c6e5f2f785e90ea3f3382e
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-07-05 14:55:11 +0300

    Add field name normalization to generateDataClasses

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/codeGen/CodeGenerationTests.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/codeGen/CodeGenerationTests.kt
@@ -331,6 +331,17 @@ class CodeGenerationTests : BaseTest() {
     }
 
     @Test
+    fun `check name normalization for generated data classes`() {
+        dataFrameOf("my_name")(1).generateDataClasses() shouldBe """
+            @DataSchema
+            data class DataEntry(
+                @ColumnName("my_name")
+                val myName: Int
+            )
+        """.trimIndent().toCodeString()
+    }
+
+    @Test
     fun patterns() {
         """^[\d]""".toRegex().matches("3fds")
     }
