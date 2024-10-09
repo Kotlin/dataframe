92ab4dce2e38c2a51a770dab2446294c4086087b
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-10-09 21:40:56 +0300

    Split openapi support in two modules, to avoid adding dependencies needed only for code-generation to project runtime classpath

---
+++ dataframe-openapi-generator/src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/getMarker.kt
@@ -0,0 +1,30 @@
+package org.jetbrains.kotlinx.dataframe.io
+
+import org.jetbrains.kotlinx.dataframe.codeGen.FieldType
+import org.jetbrains.kotlinx.dataframe.codeGen.Marker
+import org.jetbrains.kotlinx.dataframe.codeGen.MarkerVisibility
+import org.jetbrains.kotlinx.dataframe.codeGen.ValidFieldName
+
+internal fun AdditionalProperty.Companion.getMarker(typeArguments: List<String>) =
+    Marker(
+        name = AdditionalProperty::class.qualifiedName!!,
+        isOpen = false,
+        fields = listOf(
+            generatedFieldOf(
+                fieldName = ValidFieldName.of(AdditionalProperty<*>::key.name),
+                columnName = AdditionalProperty<*>::key.name,
+                overrides = false,
+                fieldType = FieldType.ValueFieldType(String::class.qualifiedName!!),
+            ),
+            generatedFieldOf(
+                fieldName = ValidFieldName.of(AdditionalProperty<*>::`value`.name),
+                columnName = AdditionalProperty<*>::`value`.name,
+                overrides = false,
+                fieldType = FieldType.ValueFieldType(Any::class.qualifiedName!! + "?"),
+            ),
+        ),
+        superMarkers = emptyList(),
+        visibility = MarkerVisibility.EXPLICIT_PUBLIC,
+        typeParameters = emptyList(),
+        typeArguments = typeArguments,
+    )
