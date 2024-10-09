02923d76feb77eb0bf3257547781db91107b17ff
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-10-09 21:40:56 +0300

    Split openapi support in two modules, to avoid adding dependencies needed only for code-generation to project runtime classpath

--- dataframe-openapi/build.gradle.kts
+++ dataframe-openapi/build.gradle.kts
@@ -1,41 +1,18 @@
 plugins {
-    with(libs.plugins) {
-        alias(kotlin.jvm)
-        alias(publisher)
-        alias(serialization)
-        alias(kover)
-        alias(ktlint)
-        alias(jupyter.api)
-    }
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.publisher)
+    alias(libs.plugins.jupyter.api)
 }
 
 group = "org.jetbrains.kotlinx"
 
-val jupyterApiTCRepo: String by project
-
 repositories {
-    mavenLocal()
     mavenCentral()
-    maven(jupyterApiTCRepo)
 }
 
 dependencies {
     api(project(":core"))
-
-    implementation(libs.sl4j)
-    implementation(libs.kotlinLogging)
-    implementation(libs.kotlin.reflect)
-    implementation(libs.kotlinpoet)
-    api(libs.swagger) {
-        // Fix for Android
-        exclude("jakarta.validation")
-    }
-
-    testApi(project(":core"))
-    testImplementation(libs.junit)
-    testImplementation(libs.kotestAssertions) {
-        exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
-    }
 }
 
 kotlinPublications {
