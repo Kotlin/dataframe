083c902a7042e7fb97581c3f15f72a7cfb4e5f0f
Jolan Rensen <jolan.rensen@jetbrains.com>
2024-10-14 12:30:44 +0200

    change default flatten parent-child separator to "_"

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/flatten.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/flatten.kt
@@ -9,37 +9,41 @@ import org.jetbrains.kotlinx.dataframe.columns.toColumnSet
 import org.jetbrains.kotlinx.dataframe.impl.api.flattenImpl
 import kotlin.reflect.KProperty
 
+public const val FLATTEN_DEFAULT_SEPARATOR: String = "_"
+
 // region DataFrame
 
 @Refine
 @Interpretable("FlattenDefault")
-public fun <T> DataFrame<T>.flatten(keepParentNameForColumns: Boolean = false, separator: String = "."): DataFrame<T> =
-    flatten(keepParentNameForColumns, separator) { all() }
+public fun <T> DataFrame<T>.flatten(
+    keepParentNameForColumns: Boolean = false,
+    separator: String = FLATTEN_DEFAULT_SEPARATOR,
+): DataFrame<T> = flatten(keepParentNameForColumns, separator) { all() }
 
 @Refine
 @Interpretable("Flatten0")
 public fun <T, C> DataFrame<T>.flatten(
     keepParentNameForColumns: Boolean = false,
-    separator: String = ".",
+    separator: String = FLATTEN_DEFAULT_SEPARATOR,
     columns: ColumnsSelector<T, C>,
 ): DataFrame<T> = flattenImpl(columns, keepParentNameForColumns, separator)
 
 public fun <T> DataFrame<T>.flatten(
     vararg columns: String,
     keepParentNameForColumns: Boolean = false,
-    separator: String = ".",
+    separator: String = FLATTEN_DEFAULT_SEPARATOR,
 ): DataFrame<T> = flatten(keepParentNameForColumns, separator) { columns.toColumnSet() }
 
 public fun <T, C> DataFrame<T>.flatten(
     vararg columns: ColumnReference<C>,
     keepParentNameForColumns: Boolean = false,
-    separator: String = ".",
+    separator: String = FLATTEN_DEFAULT_SEPARATOR,
 ): DataFrame<T> = flatten(keepParentNameForColumns, separator) { columns.toColumnSet() }
 
 public fun <T, C> DataFrame<T>.flatten(
     vararg columns: KProperty<C>,
     keepParentNameForColumns: Boolean = false,
-    separator: String = ".",
+    separator: String = FLATTEN_DEFAULT_SEPARATOR,
 ): DataFrame<T> = flatten(keepParentNameForColumns, separator) { columns.toColumnSet() }
 
 // endregion
