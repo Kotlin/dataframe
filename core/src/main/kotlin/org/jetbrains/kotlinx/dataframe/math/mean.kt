dbe620740dde9fe8edcef4c314fb1fe2b421d103
Jolan Rensen <jolan.rensen@jetbrains.com>
2024-11-06 15:06:18 +0100

    hid public Iterable.mean() overloads; There's `.average()` in stdlib, plus, aside from columns/dfs, creating public mean functions just clutters the public scope. Simplified internal mean logic and function overloads.

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/math/mean.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/math/mean.kt
@@ -2,14 +2,25 @@ package org.jetbrains.kotlinx.dataframe.math
 
 import org.jetbrains.kotlinx.dataframe.api.skipNA_default
 import org.jetbrains.kotlinx.dataframe.impl.renderType
+import org.jetbrains.kotlinx.dataframe.util.MEAN
 import java.math.BigDecimal
 import java.math.BigInteger
 import kotlin.reflect.KType
 import kotlin.reflect.full.withNullability
+import kotlin.reflect.typeOf
+
+@JvmName("meanIterableReified")
+@PublishedApi
+internal inline fun <reified T : Number> Iterable<T>.mean(skipNA: Boolean = skipNA_default): Double =
+    mean(typeOf<T>(), skipNA)
 
 @PublishedApi
 internal fun <T : Number> Iterable<T>.mean(type: KType, skipNA: Boolean = skipNA_default): Double =
     asSequence().mean(type, skipNA)
+
+@JvmName("meanSequenceReified")
+internal inline fun <reified T : Number> Sequence<T>.mean(skipNA: Boolean = skipNA_default): Double =
+    mean(typeOf<T>(), skipNA)
 
 internal fun <T : Number> Sequence<T>.mean(type: KType, skipNA: Boolean = skipNA_default): Double {
     if (type.isMarkedNullable) {
@@ -18,7 +29,7 @@ internal fun <T : Number> Sequence<T>.mean(type: KType, skipNA: Boolean = skipNA
     return when (type.classifier) {
         Double::class -> (this as Sequence<Double>).mean(skipNA)
 
-        Float::class -> (this as Sequence<Float>).mean(skipNA)
+        Float::class -> (this as Sequence<Float>).map { it.toDouble() }.mean(skipNA)
 
         Int::class -> (this as Sequence<Int>).map { it.toDouble() }.mean(false)
 
@@ -42,7 +53,7 @@ internal fun <T : Number> Sequence<T>.mean(type: KType, skipNA: Boolean = skipNA
     }
 }
 
-public fun Sequence<Double>.mean(skipNA: Boolean = skipNA_default): Double {
+private fun Sequence<Double>.mean(skipNA: Boolean = skipNA_default): Double {
     var count = 0
     var sum: Double = 0.toDouble()
     for (element in this) {
@@ -59,8 +70,9 @@ public fun Sequence<Double>.mean(skipNA: Boolean = skipNA_default): Double {
     return if (count > 0) sum / count else Double.NaN
 }
 
+@Deprecated("", level = DeprecationLevel.ERROR)
 @JvmName("meanFloat")
-public fun Sequence<Float>.mean(skipNA: Boolean = skipNA_default): Double {
+internal fun Sequence<Float>.mean(skipNA: Boolean = skipNA_default): Double {
     var count = 0
     var sum: Double = 0.toDouble()
     for (element in this) {
@@ -77,12 +89,15 @@ public fun Sequence<Float>.mean(skipNA: Boolean = skipNA_default): Double {
     return if (count > 0) sum / count else Double.NaN
 }
 
+@Deprecated("", level = DeprecationLevel.HIDDEN)
 @JvmName("doubleMean")
-public fun Iterable<Double>.mean(skipNA: Boolean = skipNA_default): Double = asSequence().mean(skipNA)
+internal fun Iterable<Double>.mean(skipNA: Boolean = skipNA_default): Double = mean(typeOf<Double>(), skipNA)
 
+@Deprecated("", level = DeprecationLevel.HIDDEN)
 @JvmName("floatMean")
-public fun Iterable<Float>.mean(skipNA: Boolean = skipNA_default): Double = asSequence().mean(skipNA)
+internal fun Iterable<Float>.mean(skipNA: Boolean = skipNA_default): Double = mean(typeOf<Float>(), skipNA)
 
+@Deprecated(MEAN, level = DeprecationLevel.HIDDEN)
 @JvmName("intMean")
 public fun Iterable<Int>.mean(): Double =
     if (this is Collection) {
@@ -96,6 +111,7 @@ public fun Iterable<Int>.mean(): Double =
         if (count > 0) sum / count else Double.NaN
     }
 
+@Deprecated(MEAN, level = DeprecationLevel.HIDDEN)
 @JvmName("shortMean")
 public fun Iterable<Short>.mean(): Double =
     if (this is Collection) {
@@ -109,6 +125,7 @@ public fun Iterable<Short>.mean(): Double =
         if (count > 0) sum / count else Double.NaN
     }
 
+@Deprecated(MEAN, level = DeprecationLevel.HIDDEN)
 @JvmName("byteMean")
 public fun Iterable<Byte>.mean(): Double =
     if (this is Collection) {
@@ -122,6 +139,7 @@ public fun Iterable<Byte>.mean(): Double =
         if (count > 0) sum / count else Double.NaN
     }
 
+@Deprecated(MEAN, level = DeprecationLevel.HIDDEN)
 @JvmName("longMean")
 public fun Iterable<Long>.mean(): Double =
     if (this is Collection) {
@@ -135,19 +153,7 @@ public fun Iterable<Long>.mean(): Double =
         if (count > 0) sum / count else Double.NaN
     }
 
-@JvmName("bigIntegerMean")
-public fun Iterable<BigInteger>.mean(): Double =
-    if (this is Collection) {
-        if (size > 0) sumOf { it.toDouble() } / size else Double.NaN
-    } else {
-        var count = 0
-        val sum = sumOf {
-            count++
-            it.toDouble()
-        }
-        if (count > 0) sum / count else Double.NaN
-    }
-
+@Deprecated(MEAN, level = DeprecationLevel.HIDDEN)
 @JvmName("bigDecimalMean")
 public fun Iterable<BigDecimal>.mean(): Double =
     if (this is Collection) {
