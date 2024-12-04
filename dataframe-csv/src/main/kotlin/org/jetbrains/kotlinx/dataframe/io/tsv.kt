579802c5bc9fa7cdfff7d963505654d5e51cf336
Jolan Rensen <jolan.rensen@jetbrains.com>
2024-12-04 13:04:36 +0100

    org.jetbrains.kotlinx.dataframe.io.Csv clashes with CSV on windows...

--- dataframe-csv/src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/tsv.kt
+++ dataframe-csv/src/main/kotlin/org/jetbrains/kotlinx/dataframe/io/tsv.kt
@@ -10,7 +10,7 @@ import java.io.File
 import java.io.InputStream
 import kotlin.reflect.typeOf
 
-public class Tsv(private val delimiter: Char = DelimParams.TSV_DELIMITER) : SupportedDataFrameFormat {
+public class TsvDeephaven(private val delimiter: Char = DelimParams.TSV_DELIMITER) : SupportedDataFrameFormat {
     override fun readDataFrame(stream: InputStream, header: List<String>): DataFrame<*> =
         DataFrame.readTsv(inputStream = stream, header = header)
 
