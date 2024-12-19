69c239bc110b6fa38d27b8b134a6b50acdfce9e3
Jolan Rensen <jolan.rensen@jetbrains.com>
2024-12-19 16:07:01 +0100

    clarifying some things regarding Configurations in KeywordsGeneratorPlugin

--- plugins/keywords-generator/src/main/kotlin/org/jetbrains/dataframe/keywords/KeywordsGeneratorPlugin.kt
+++ plugins/keywords-generator/src/main/kotlin/org/jetbrains/dataframe/keywords/KeywordsGeneratorPlugin.kt
@@ -2,6 +2,8 @@ package org.jetbrains.dataframe.keywords
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.DependencyScopeConfiguration
+import org.gradle.api.artifacts.ResolvableConfiguration
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.kotlin.dsl.get
@@ -9,15 +11,17 @@ import org.gradle.kotlin.dsl.register
 import org.jetbrains.kotlinx.dataframe.BuildConfig
 import java.io.File
 
+@Suppress("UnstableApiUsage")
 abstract class KeywordsGeneratorPlugin : Plugin<Project> {
 
     override fun apply(target: Project): Unit = with(target) {
         // from https://kotlinlang.org/docs/whatsnew21.html#compiler-symbols-hidden-from-the-kotlin-gradle-plugin-api
-        val taskDependencyScope = configurations.create("keywordsGeneratorScope")
-        dependencies.add(taskDependencyScope.name, "$KOTLIN_COMPILER_EMBEDDABLE:$KOTLIN_COMPILER_VERSION")
-        val taskResolvableConfiguration = configurations.create("keywordGeneratorResolvable") {
-            extendsFrom(taskDependencyScope)
-        }
+        val dependencyScopeConfiguration: DependencyScopeConfiguration = configurations.dependencyScope("keywordsGeneratorDependencyScope").get()
+        dependencies.add(dependencyScopeConfiguration.name, "$KOTLIN_COMPILER_EMBEDDABLE:$KOTLIN_COMPILER_VERSION")
+
+        val resolvableConfiguration: ResolvableConfiguration = configurations.resolvable("keywordGeneratorResolvable") {
+            extendsFrom(dependencyScopeConfiguration)
+        }.get()
 
         val genSrcDir = layout.buildDirectory.asFile.get().resolve("generatedSrc")
 
@@ -26,7 +30,7 @@ abstract class KeywordsGeneratorPlugin : Plugin<Project> {
         mainSourceSet.addDir(genSrcDir)
 
         val genTask = tasks.register<KeywordsGeneratorTask>(KeywordsGeneratorTask.NAME) {
-            kotlinCompiler.from(taskResolvableConfiguration)
+            kotlinCompiler.from(resolvableConfiguration)
             srcDir = genSrcDir
         }
 
