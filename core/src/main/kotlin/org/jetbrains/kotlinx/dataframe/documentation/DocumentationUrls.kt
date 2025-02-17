ec0f9f24ab8b2fb46fe8a830783ce1e36bd17cd1
zaleslaw <zaleslaw.sin@gmail.com>
2025-02-17 14:16:53 +0100

    Add documentation for the `flatten` operation
    Introduced detailed documentation for the `flatten` operation, explaining its behavior, parameters, and examples.

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/documentation/DocumentationUrls.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/documentation/DocumentationUrls.kt
@@ -80,4 +80,7 @@ internal interface DocumentationUrls {
 
     /** <a href="{@include [Url]}/distinct.html">See `distinct` on the documentation website.</a> */
     interface Distinct
+
+    /** <a href="{@include [Url]}/flatten.html">See `flatten` on the documentation website.</a> */
+    interface Flatten
 }
