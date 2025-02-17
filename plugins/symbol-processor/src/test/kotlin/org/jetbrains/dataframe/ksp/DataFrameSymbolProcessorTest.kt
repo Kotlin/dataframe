b2a590f93a7d583bb1d2f023a0e59ff0eb5ad581
Jolan Rensen <jolan.rensen@jetbrains.com>
2025-02-17 12:43:14 +0100

    fixed ksp test

--- plugins/symbol-processor/src/test/kotlin/org/jetbrains/dataframe/ksp/DataFrameSymbolProcessorTest.kt
+++ plugins/symbol-processor/src/test/kotlin/org/jetbrains/dataframe/ksp/DataFrameSymbolProcessorTest.kt
@@ -1043,7 +1043,7 @@ class DataFrameSymbolProcessorTest {
         result.successfulCompilation shouldBe false
     }
 
-    private val jetbrainsCsv = File("../../data/jetbrains repositories.csv")
+    private val jetbrainsCsv = File("../../data/jetbrains_repositories.csv")
 
     @Test
     fun `imported schema resolved`() {
