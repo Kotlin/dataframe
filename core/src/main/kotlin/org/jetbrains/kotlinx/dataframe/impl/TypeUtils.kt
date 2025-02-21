a5991a4610335e595ff7e1f404adc5a076ed4314
Jolan Rensen <jolan.rensen@jetbrains.com>
2025-02-21 15:50:55 +0100

    merging fix

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/TypeUtils.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/TypeUtils.kt
@@ -473,7 +473,7 @@ internal fun guessValueType(
 
     if (unifyNumbers) {
         val usedNumberClasses = classes.filter { it.isSubclassOf(Number::class) }
-        val unifiedNumberClass = usedNumberClasses.commonNumberClass() as KClass<Number>
+        val unifiedNumberClass = usedNumberClasses.unifiedNumberClass() as KClass<Number>
         classes -= usedNumberClasses
         classes += unifiedNumberClass
     }
