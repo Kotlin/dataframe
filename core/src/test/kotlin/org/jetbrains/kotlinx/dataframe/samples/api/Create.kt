725395095dfcdb198f0d0839cef4befefe00f0b3
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2025-04-23 01:37:31 +0300

    Add a constructor to create a nested dataframe from columns inplace
    https://github.com/Kotlin/dataframe/issues/353

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Create.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Create.kt
@@ -263,6 +263,21 @@ class Create : TestBase() {
 
     @Test
     @TransformDataFrameExpressions
+    fun createNestedDataFrameInplace() {
+        // SampleStart
+        // DataFrame with 2 columns and 3 rows
+        val df = dataFrameOf(
+            "name" to columnOf(
+                "firstName" to columnOf("Alice", "Bob", "Charlie"),
+                "lastName" to columnOf("Cooper", "Dylan", "Daniels"),
+            ),
+            "age" to columnOf(15, 20, 100),
+        )
+        // SampleEnd
+    }
+
+    @Test
+    @TransformDataFrameExpressions
     fun createDataFrameWithFill() {
         // SampleStart
         // Multiplication table
