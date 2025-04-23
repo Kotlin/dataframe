9844b7d46bf72bda510255976f225277a19768f1
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2025-04-23 18:50:16 +0200

    Refactor type conversion and column handling logic

--- plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/impl/api/statistics.kt
+++ plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/impl/api/statistics.kt
@@ -1,6 +1,5 @@
 package org.jetbrains.kotlinx.dataframe.plugin.impl.api
 
-import org.jetbrains.kotlin.fir.symbols.ConeClassLikeLookupTag
 import org.jetbrains.kotlin.fir.types.ConeClassLikeErrorLookupTag
 import org.jetbrains.kotlin.fir.types.isSubtypeOf
 import org.jetbrains.kotlinx.dataframe.impl.aggregation.aggregators.Aggregators
@@ -21,62 +20,69 @@ import org.jetbrains.kotlinx.dataframe.impl.aggregation.aggregators.Aggregator
 import org.jetbrains.kotlinx.dataframe.plugin.impl.SimpleCol
 import kotlin.reflect.KClass
 
+private object PrimitiveClassIds {
+    const val INT = "kotlin/Int"
+    const val LONG = "kotlin/Long"
+    const val DOUBLE = "kotlin/Double"
+    const val FLOAT = "kotlin/Float"
+    const val SHORT = "kotlin/Short"
+    const val BYTE = "kotlin/Byte"
+}
+
+private fun KClass<*>.toClassId(): ClassId? = when (this) {
+    Int::class -> ClassId.fromString(PrimitiveClassIds.INT)
+    Long::class -> ClassId.fromString(PrimitiveClassIds.LONG)
+    Double::class -> ClassId.fromString(PrimitiveClassIds.DOUBLE)
+    Float::class -> ClassId.fromString(PrimitiveClassIds.FLOAT)
+    Short::class -> ClassId.fromString(PrimitiveClassIds.SHORT)
+    Byte::class -> ClassId.fromString(PrimitiveClassIds.BYTE)
+    else -> null
+}
+
+private val primitiveTypeMap = mapOf(
+    PrimitiveClassIds.INT to Int::class,
+    PrimitiveClassIds.LONG to Long::class,
+    PrimitiveClassIds.DOUBLE to Double::class,
+    PrimitiveClassIds.FLOAT to Float::class,
+    PrimitiveClassIds.SHORT to Short::class,
+    PrimitiveClassIds.BYTE to Byte::class
+)
+
 fun ConeKotlinType.toKType(): KType? {
-    if (this is ConeClassLikeType) {
-        val classId: ClassId = this.lookupTag.classId
-        val isNullable = this.nullability == ConeNullability.NULLABLE
-        return when (classId.asString()) {
-            "kotlin/Int" -> Int::class.createType(nullable = isNullable)
-            "kotlin/Long" -> Long::class.createType(nullable = isNullable)
-            "kotlin/Double" -> Double::class.createType(nullable = isNullable)
-            "kotlin/Float" -> Float::class.createType(nullable = isNullable)
-            "kotlin/Short" -> Short::class.createType(nullable = isNullable)
-            "kotlin/Byte" -> Byte::class.createType(nullable = isNullable)
-            else -> null
-        }
+    return (this as? ConeClassLikeType)?.let { coneType ->
+        val nullable = coneType.nullability == ConeNullability.NULLABLE
+        primitiveTypeMap[coneType.lookupTag.classId.asString()]
+            ?.createType(nullable = nullable)
     }
-    return null
 }
 
 fun KType.toConeKotlinType(): ConeKotlinType? {
     val kClass = this.classifier as? KClass<*> ?: return null
-
-    val classId = when (kClass) {
-        Int::class -> ClassId.fromString("kotlin/Int")
-        Long::class -> ClassId.fromString("kotlin/Long")
-        Double::class -> ClassId.fromString("kotlin/Double")
-        Float::class -> ClassId.fromString("kotlin/Float")
-        Short::class -> ClassId.fromString("kotlin/Short")
-        Byte::class -> ClassId.fromString("kotlin/Byte")
-        else -> return null
-    }
-
-    val lookupTag: ConeClassLikeLookupTag = ConeClassLikeErrorLookupTag(classId)
-
-    val nullability = if (this.isMarkedNullable) ConeNullability.NULLABLE else ConeNullability.NOT_NULL
+    val classId = kClass.toClassId() ?: return null
 
     return ConeClassLikeTypeImpl(
-        lookupTag = lookupTag,
+        lookupTag = ConeClassLikeErrorLookupTag(classId),
         typeArguments = emptyArray(),
-        isNullable = (nullability == ConeNullability.NULLABLE)
+        isNullable = this.isMarkedNullable
     )
 }
 
-private fun Arguments.calculateResultTypeForStatistic(
-    aggregator: Aggregator<Number, Number>,
-    resolvedColumns: List<SimpleDataColumn>
+private fun Arguments.generateStatisticResultColumns(
+    statisticAggregator: Aggregator<Number, Number>,
+    inputColumns: List<SimpleDataColumn>
 ): List<SimpleCol> {
-    val newColumns = resolvedColumns
-        .map { col ->
-            val columnType = col.type.type
-            val inputColumnKType = columnType.toKType()
-            val calculatedReturnKType = inputColumnKType?.let { aggregator.calculateReturnType(it, emptyInput = false) }
-            val updatedType =
-                calculatedReturnKType?.toConeKotlinType() ?: columnType // stay with input type by default
-            simpleColumnOf(col.name, updatedType)
-        }
-        .toList()
-    return newColumns
+    return inputColumns.map { col -> createUpdatedColumn(col, statisticAggregator) }
+}
+
+private fun Arguments.createUpdatedColumn(
+    column: SimpleDataColumn,
+    statisticAggregator: Aggregator<Number, Number>
+): SimpleCol {
+    val originalType = column.type.type
+    val inputKType = originalType.toKType()
+    val resultKType = inputKType?.let { statisticAggregator.calculateReturnType(it, emptyInput = false) }
+    val updatedType = resultKType?.toConeKotlinType() ?: originalType
+    return simpleColumnOf(column.name, updatedType)
 }
 
 val skipNaN = true
@@ -91,7 +97,7 @@ abstract class Aggregator0 : AbstractSchemaModificationInterpreter() {
             .filterIsInstance<SimpleDataColumn>()
             .filter { it.type.type.isSubtypeOf(session.builtinTypes.numberType.type, session) }
 
-        val newColumns = calculateResultTypeForStatistic(sum, resolvedColumns)
+        val newColumns = generateStatisticResultColumns(sum, resolvedColumns)
 
         return PluginDataFrameSchema(receiver.columns() + newColumns)
     }
@@ -108,7 +114,7 @@ abstract class Aggregator1 : AbstractSchemaModificationInterpreter() {
     override fun Arguments.interpret(): PluginDataFrameSchema {
         val resolvedColumns = columns.resolve(receiver).map { it.column }.filterIsInstance<SimpleDataColumn>().toList()
 
-        val newColumns = calculateResultTypeForStatistic(sum, resolvedColumns)
+        val newColumns = generateStatisticResultColumns(sum, resolvedColumns)
 
         return PluginDataFrameSchema(receiver.columns() + newColumns)
     }
