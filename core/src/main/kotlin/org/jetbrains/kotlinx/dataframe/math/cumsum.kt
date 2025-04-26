c817531689d4b1e1457754f7219a19fe47bb59b8
Jolan Rensen <Jolan.Rensen@jetbrains.com>
2025-04-26 16:53:42 +0200

    CumSum (#1152)
    * reimplementation of cumSum:
    - removed big numbers
    - following type conversion of sum
    - added correct overloads
    - fixed behavior of float/double like how it was described on the documentation site
    
    * fixed netflix example notebook for cumsum

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/math/cumsum.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/math/cumsum.kt
@@ -1,66 +1,172 @@
 package org.jetbrains.kotlinx.dataframe.math
 
 import org.jetbrains.kotlinx.dataframe.DataColumn
+import org.jetbrains.kotlinx.dataframe.api.asIterable
+import org.jetbrains.kotlinx.dataframe.api.cast
 import org.jetbrains.kotlinx.dataframe.api.isNA
 import org.jetbrains.kotlinx.dataframe.api.map
+import org.jetbrains.kotlinx.dataframe.api.toColumn
+import org.jetbrains.kotlinx.dataframe.impl.UnifiedNumberTypeOptions
+import org.jetbrains.kotlinx.dataframe.impl.aggregation.aggregators.CalculateReturnType
+import org.jetbrains.kotlinx.dataframe.impl.convertToUnifiedNumberType
+import org.jetbrains.kotlinx.dataframe.impl.nothingType
+import org.jetbrains.kotlinx.dataframe.impl.nullableNothingType
+import org.jetbrains.kotlinx.dataframe.impl.renderType
+import org.jetbrains.kotlinx.dataframe.impl.types
+import org.jetbrains.kotlinx.dataframe.impl.unifiedNumberTypeOrNull
 import java.math.BigDecimal
 import java.math.BigInteger
+import kotlin.reflect.KType
+import kotlin.reflect.full.withNullability
+import kotlin.reflect.typeOf
 
-internal val defaultCumSumSkipNA: Boolean = true
+/**
+ * @param skipNA when true, ignores NA values (null or NaN)
+ *   When false, all values after first NA will be NaN (for Double and Float columns)
+ *   or null (for integer columns).
+ */
+internal fun DataColumn<Number?>.cumSumImpl(type: KType, skipNA: Boolean): DataColumn<Number?> =
+    when (type) {
+        typeOf<Double>() -> cast<Double>().cumSumImpl(skipNA)
+
+        typeOf<Double?>() -> cast<Double?>().cumSumImpl(skipNA)
+
+        typeOf<Float>() -> cast<Float>().cumSumImpl(skipNA)
+
+        typeOf<Float?>() -> cast<Float?>().cumSumImpl(skipNA)
+
+        typeOf<Int>() -> cast<Int>().cumSumImpl()
+
+        // Note: returns Int
+        typeOf<Byte>() -> cast<Byte>().cumSumImpl()
+
+        // Note: returns Int
+        typeOf<Short>() -> cast<Short>().cumSumImpl()
+
+        typeOf<Int?>() -> cast<Int?>().cumSumImpl(skipNA)
+
+        // Note: returns Int
+        typeOf<Byte?>() -> cast<Byte?>().cumSumImpl(skipNA)
+
+        // Note: returns Int
+        typeOf<Short?>() -> cast<Short?>().cumSumImpl(skipNA)
+
+        typeOf<Long>() -> cast<Long>().cumSumImpl()
+
+        typeOf<Long?>() -> cast<Long?>().cumSumImpl(skipNA)
+
+        typeOf<BigInteger>(), typeOf<BigInteger?>(), typeOf<BigDecimal>(), typeOf<BigDecimal?>() ->
+            throw IllegalArgumentException(
+                "Cannot calculate the cumSum for big numbers in DataFrame. Only primitive numbers are supported.",
+            )
+
+        // accepts mixed primitive numbers by unifying them
+        typeOf<Number?>(), typeOf<Number>() -> {
+            val types = this.asIterable().types().toSet()
+            val unifiedType = types.unifiedNumberTypeOrNull(UnifiedNumberTypeOptions.PRIMITIVES_ONLY)
+                ?: error(
+                    "Couldn't unify the numbers of types ${
+                        types.joinToString { renderType(it) }
+                    } of column ${name()} in cumSum. Please manually convert the numbers in column ${name()} to the same primitive number type before using cumSum.",
+                )
+
+            this.asIterable()
+                .convertToUnifiedNumberType(UnifiedNumberTypeOptions.PRIMITIVES_ONLY, unifiedType)
+                .toColumn(this.name())
+                .cumSumImpl(unifiedType, skipNA)
+        }
+
+        // CumSum for empty column or column with just null is itself
+        nothingType, nullableNothingType -> this
+
+        else -> error("CumSum for type ${type()} is not supported")
+    }
 
 @JvmName("doubleCumsum")
-internal fun DataColumn<Double>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<Double> {
+internal fun DataColumn<Double>.cumSumImpl(skipNA: Boolean): DataColumn<Double> {
     var sum = .0
+    var fillNaN = false
     return map {
-        if (skipNA && it.isNaN()) {
-            it
-        } else {
-            sum += it
-            sum
+        when {
+            it.isNaN() -> {
+                if (!skipNA) fillNaN = true
+                Double.NaN
+            }
+
+            fillNaN -> Double.NaN
+
+            else -> {
+                sum += it
+                sum
+            }
         }
     }
 }
 
 @JvmName("cumsumDoubleNullable")
-internal fun DataColumn<Double?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<Double> {
+internal fun DataColumn<Double?>.cumSumImpl(skipNA: Boolean): DataColumn<Double> {
     var sum = .0
+    var fillNaN = false
     return map {
-        if (skipNA && it.isNA) {
-            Double.NaN
-        } else {
-            sum += it ?: Double.NaN
-            sum
+        when {
+            it.isNA() -> {
+                if (!skipNA) fillNaN = true
+                Double.NaN
+            }
+
+            fillNaN -> Double.NaN
+
+            else -> {
+                sum += it
+                sum
+            }
         }
     }
 }
 
 @JvmName("floatCumsum")
-internal fun DataColumn<Float>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<Float> {
+internal fun DataColumn<Float>.cumSumImpl(skipNA: Boolean): DataColumn<Float> {
     var sum = .0f
+    var fillNaN = false
     return map {
-        if (skipNA && it.isNaN()) {
-            it
-        } else {
-            sum += it
-            sum
+        when {
+            it.isNaN() -> {
+                if (!skipNA) fillNaN = true
+                Float.NaN
+            }
+
+            fillNaN -> Float.NaN
+
+            else -> {
+                sum += it
+                sum
+            }
         }
     }
 }
 
-internal fun DataColumn<Float?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<Float> {
+internal fun DataColumn<Float?>.cumSumImpl(skipNA: Boolean): DataColumn<Float> {
     var sum = .0f
+    var fillNaN = false
     return map {
-        if (skipNA && it.isNA) {
-            Float.NaN
-        } else {
-            sum += it ?: Float.NaN
-            sum
+        when {
+            it.isNA() -> {
+                if (!skipNA) fillNaN = true
+                Float.NaN
+            }
+
+            fillNaN -> Float.NaN
+
+            else -> {
+                sum += it
+                sum
+            }
         }
     }
 }
 
 @JvmName("intCumsum")
-internal fun DataColumn<Int>.cumSum(): DataColumn<Int> {
+internal fun DataColumn<Int>.cumSumImpl(): DataColumn<Int> {
     var sum = 0
     return map {
         sum += it
@@ -69,7 +175,7 @@ internal fun DataColumn<Int>.cumSum(): DataColumn<Int> {
 }
 
 @JvmName("intCumsum")
-internal fun DataColumn<Int?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<Int?> {
+internal fun DataColumn<Int?>.cumSumImpl(skipNA: Boolean): DataColumn<Int?> {
     var sum = 0
     var fillNull = false
     return map {
@@ -90,7 +196,7 @@ internal fun DataColumn<Int?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): Dat
 }
 
 @JvmName("byteCumsum")
-internal fun DataColumn<Byte>.cumSum(): DataColumn<Int> {
+internal fun DataColumn<Byte>.cumSumImpl(): DataColumn<Int> {
     var sum = 0
     return map {
         sum += it
@@ -99,7 +205,7 @@ internal fun DataColumn<Byte>.cumSum(): DataColumn<Int> {
 }
 
 @JvmName("cumsumByteNullable")
-internal fun DataColumn<Byte?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<Int?> {
+internal fun DataColumn<Byte?>.cumSumImpl(skipNA: Boolean): DataColumn<Int?> {
     var sum = 0
     var fillNull = false
     return map {
@@ -120,7 +226,7 @@ internal fun DataColumn<Byte?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): Da
 }
 
 @JvmName("shortCumsum")
-internal fun DataColumn<Short>.cumSum(): DataColumn<Int> {
+internal fun DataColumn<Short>.cumSumImpl(): DataColumn<Int> {
     var sum = 0
     return map {
         sum += it
@@ -129,7 +235,7 @@ internal fun DataColumn<Short>.cumSum(): DataColumn<Int> {
 }
 
 @JvmName("cumsumShortNullable")
-internal fun DataColumn<Short?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<Int?> {
+internal fun DataColumn<Short?>.cumSumImpl(skipNA: Boolean): DataColumn<Int?> {
     var sum = 0
     var fillNull = false
     return map {
@@ -150,7 +256,7 @@ internal fun DataColumn<Short?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): D
 }
 
 @JvmName("longCumsum")
-internal fun DataColumn<Long>.cumSum(): DataColumn<Long> {
+internal fun DataColumn<Long>.cumSumImpl(): DataColumn<Long> {
     var sum = 0L
     return map {
         sum += it
@@ -159,7 +265,7 @@ internal fun DataColumn<Long>.cumSum(): DataColumn<Long> {
 }
 
 @JvmName("cumsumLongNullable")
-internal fun DataColumn<Long?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<Long?> {
+internal fun DataColumn<Long?>.cumSumImpl(skipNA: Boolean): DataColumn<Long?> {
     var sum = 0L
     var fillNull = false
     return map {
@@ -179,62 +285,27 @@ internal fun DataColumn<Long?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): Da
     }
 }
 
-@JvmName("bigIntegerCumsum")
-internal fun DataColumn<BigInteger>.cumSum(): DataColumn<BigInteger> {
-    var sum = BigInteger.ZERO
-    return map {
-        sum += it
-        sum
-    }
-}
+/**
+ * Double(?) -> Double
+ * Float(?) -> Float
+ * Short(?), Byte(?) -> Int(?)
+ * T : Number(?) -> T(?)
+ */
+public val cumSumTypeConversion: CalculateReturnType = { type, _ ->
+    when (val type = type.withNullability(false)) {
+        // type changes to Int, carrying nullability
+        typeOf<Short>(), typeOf<Byte>() -> typeOf<Int>().withNullability(type.isMarkedNullable)
 
-@JvmName("cumsumBigIntegerNullable")
-internal fun DataColumn<BigInteger?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<BigInteger?> {
-    var sum = BigInteger.ZERO
-    var fillNull = false
-    return map {
-        when {
-            it == null -> {
-                if (!skipNA) fillNull = true
-                null
-            }
+        // type remains the same, carrying nullability
+        typeOf<Int>(), typeOf<Long>(), typeOf<Number>() -> type
 
-            fillNull -> null
+        // type remains the same, but nulls are turned into NaN, so no more nullability
+        typeOf<Double>(), typeOf<Float>() -> type.withNullability(false)
 
-            else -> {
-                sum += it
-                sum
-            }
-        }
-    }
-}
+        // CumSum for empty column or column with just nulls remains unchanged
+        nothingType -> type
 
-@JvmName("bigDecimalCumsum")
-internal fun DataColumn<BigDecimal>.cumSum(): DataColumn<BigDecimal> {
-    var sum = BigDecimal.ZERO
-    return map {
-        sum += it
-        sum
-    }
-}
-
-@JvmName("cumsumBigDecimalNullable")
-internal fun DataColumn<BigDecimal?>.cumSum(skipNA: Boolean = defaultCumSumSkipNA): DataColumn<BigDecimal?> {
-    var sum = BigDecimal.ZERO
-    var fillNull = false
-    return map {
-        when {
-            it == null -> {
-                if (!skipNA) fillNull = true
-                null
-            }
-
-            fillNull -> null
-
-            else -> {
-                sum += it
-                sum
-            }
-        }
+        else ->
+            error("Unable to compute the cumSum for ${renderType(type)}, Only primitive numbers are supported.")
     }
 }
