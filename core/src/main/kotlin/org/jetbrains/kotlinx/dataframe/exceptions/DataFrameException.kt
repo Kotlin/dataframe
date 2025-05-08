fb62d9c506d0b8b77c2106985e96b465aff95eff
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2025-05-09 02:37:31 +0300

    Rename to clarify that interface itself is not an exception

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/exceptions/DataFrameException.kt
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.kotlinx.dataframe.exceptions
-
-/**
- * If DataFrame function used by compiler plugin as implementation detail throws this exception, [message] will be reported as warning
- */
-public interface DataFrameException {
-    public val message: String
-}
