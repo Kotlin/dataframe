2327777899c52122231bc0ac66e6c57f63b785ac
Jolan Rensen <jolan.rensen@jetbrains.com>
2025-06-12 20:24:29 +0200

    Made mri-like example for Multik inside dataframe

--- examples/idea-examples/unsupported-data-sources/src/main/kotlin/org/jetbrains/kotlinx/dataframe/examples/multik/multikInsideDataFrame.kt
+++ examples/idea-examples/unsupported-data-sources/src/main/kotlin/org/jetbrains/kotlinx/dataframe/examples/multik/multikInsideDataFrame.kt
@@ -1,134 +1,115 @@
 package org.jetbrains.kotlinx.dataframe.examples.multik
 
+import kotlinx.datetime.LocalDate
 import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
+import org.jetbrains.kotlinx.dataframe.api.append
+import org.jetbrains.kotlinx.dataframe.api.cast
+import org.jetbrains.kotlinx.dataframe.api.mapToFrame
 import org.jetbrains.kotlinx.dataframe.api.print
+import org.jetbrains.kotlinx.dataframe.api.single
 import org.jetbrains.kotlinx.dataframe.api.toDataFrame
-import org.jetbrains.kotlinx.dataframe.io.toStandaloneHtml
-import org.jetbrains.kotlinx.multik.api.identity
 import org.jetbrains.kotlinx.multik.api.mk
-import org.jetbrains.kotlinx.multik.ndarray.data.D2Array
-import org.jetbrains.kotlinx.multik.ndarray.data.set
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.tan
-
-@DataSchema
-data class Transformation(
-    val type: TransformationType,
-    val parameters: Map<String, Double>,
-    val note: String,
-    val matrix: D2Array<Double>,
-)
-
-enum class TransformationType {
-    IDENTITY,
-    TRANSLATION,
-    SCALING,
-    ROTATION,
-    SHEARING,
-    REFLECTION_ABOUT_ORIGIN,
-    REFLECTION_ABOUT_X_AXIS,
-    REFLECTION_ABOUT_Y_AXIS,
-}
+import org.jetbrains.kotlinx.multik.api.rand
+import org.jetbrains.kotlinx.multik.ndarray.data.D3Array
+import org.jetbrains.kotlinx.multik.ndarray.data.D4Array
+import java.time.Month.JULY
 
 /**
- * IDK yet about this one... TODO
+ * DataFrames can store anything inside, including Multik ndarrays.
+ * This can be useful for storing matrices for easier access later or to simply organize data read from other files.
+ * For example, MRI data is often stored as 3D arrays and sometimes even 4D arrays.
  */
 fun main() {
-    // DataFrames can store anything inside, including Multik nd arrays.
-    // This can be useful for storing matrices for easier access later,
-    // such as affine transformations when making 2D graphics!
-    // (https://en.wikipedia.org/wiki/Affine_transformation)
-
-    // let's make a transformation sequence that rotates and scales an image in place.
-    // It's currently 100x50, positioned with its left bottom corner at (x=10, y=0)
-    val transformations = listOf(
-        Transformation(
-            type = TransformationType.TRANSLATION,
-            parameters = mapOf("x" to -10.0, "y" to 0.0),
-            note = "Translate so left-bottom touches origin",
-            matrix = translationMatrixOf(x = -10.0, y = 0.0),
-        ),
-        Transformation(
-            type = TransformationType.SCALING,
-            parameters = mapOf("w" to 2.0, "h" to 2.0),
-            note = "Scale by x2",
-            matrix = scaleMatrixOf(w = 2.0, h = 2.0),
-        ),
-        Transformation(
-            type = TransformationType.TRANSLATION,
-            parameters = mapOf("x" to -100.0, "y" to -50.0),
-            note = "Translate so the new image center is at the origin",
-            matrix = translationMatrixOf(x = -100.0, y = -50.0),
-        ),
-        Transformation(
-            type = TransformationType.ROTATION,
-            parameters = mapOf("angle" to 45.0),
-            note = "Rotate by 45 degrees",
-            matrix = rotationMatrixOf(angle = 45.0),
-        ),
-        Transformation(
-            type = TransformationType.TRANSLATION,
-            parameters = mapOf("x" to 10.0 + 50.0, "y" to 0.0 + 25.0),
-            note = "Translate back so the center is at the same original position",
-            matrix = translationMatrixOf(x = 10.0 + 50.0, y = 0.0 + 25.0),
-        ),
+    // imaginary list of patient data
+    @Suppress("ktlint:standard:argument-list-wrapping")
+    val metadata = listOf(
+        MriMetadata(10012L, 25, "Healthy", LocalDate(2023, 1, 1)),
+        MriMetadata(10013L, 45, "Tuberculosis", LocalDate(2023, 2, 15)),
+        MriMetadata(10014L, 32, "Healthy", LocalDate(2023, 3, 22)),
+        MriMetadata(10015L, 58, "Pneumonia", LocalDate(2023, 4, 8)),
+        MriMetadata(10016L, 29, "Tuberculosis", LocalDate(2023, 5, 30)),
+        MriMetadata(10017L, 42, "Healthy", LocalDate(2023, 6, 15)),
+        MriMetadata(10018L, 37, "Healthy", LocalDate(2023, 7, 1)),
+        MriMetadata(10019L, 55, "Healthy", LocalDate(2023, 8, 15)),
+        MriMetadata(10020L, 28, "Healthy", LocalDate(2023, 9, 1)),
+        MriMetadata(10021L, 44, "Healthy", LocalDate(2023, 10, 15)),
+        MriMetadata(10022L, 31, "Healthy", LocalDate(2023, 11, 1)),
     ).toDataFrame()
 
-    transformations.print(borders = true)
-    transformations.toStandaloneHtml().openInBrowser()
+    // "reading" the results from "files"
+    val results = metadata.mapToFrame {
+        +patientId
+        +age
+        +diagnosis
+        +scanDate
+        "t1WeightedMri" from { readT1WeightedMri(patientId) }
+        "fMriBoldSeries" from { readFMRiBoldSeries(patientId) }
+    }.cast<MriResults>(verify = true)
+        .append()
+
+    results.print(borders = true)
+
+    // now when we want to check and visualize the T1-weighted MRI scan
+    // for that one healthy patient in July, we can do:
+    val scan = results
+        .single { scanDate.month == JULY && diagnosis == "Healthy" }
+        .t1WeightedMri
+
+    // easy :)
+    visualize(scan)
 }
 
-fun identityMatrix(): D2Array<Double> = mk.identity(3)
+@DataSchema
+data class MriMetadata(
+    /** Unique patient ID. */
+    val patientId: Long,
+    /** Patient age. */
+    val age: Int,
+    /** Clinical diagnosis (e.g. "Healthy", "Tuberculosis") */
+    val diagnosis: String,
+    /** Date of the scan */
+    val scanDate: LocalDate,
+)
 
-/** Returns a 3x3 affine transformation matrix that translates by (x, y) */
-fun translationMatrixOf(x: Double = 0.0, y: Double = 0.0): D2Array<Double> =
-    identityMatrix().apply {
-        this[0, 2] = x
-        this[1, 2] = y
-    }
+@DataSchema
+data class MriResults(
+    /** Unique patient ID. */
+    val patientId: Long,
+    /** Patient age. */
+    val age: Int,
+    /** Clinical diagnosis (e.g. "Healthy", "Tuberculosis") */
+    val diagnosis: String,
+    /** Date of the scan */
+    val scanDate: LocalDate,
+    /**
+     * T1-weighted anatomical MRI scan.
+     *
+     * Dimensions: (256 x 256 x 180)
+     * - 256 width x 256 height
+     * - 180 slices
+     */
+    val t1WeightedMri: D3Array<Float>,
+    /**
+     * Blood oxygenation level-dependent (BOLD) time series from an fMRI scan.
+     *
+     * Dimensions: (64 x 64 x 30 x 200)
+     * - 64 width x 64 height
+     * - 30 slices
+     * - 200 timepoints
+     */
+    val fMriBoldSeries: D4Array<Float>,
+)
 
-/** Returns a 3x3 affine transformation matrix that scales by (w, h) about the origin */
-fun scaleMatrixOf(w: Double = 1.0, h: Double = 1.0): D2Array<Double> =
-    identityMatrix().apply {
-        this[0, 0] = w
-        this[1, 1] = h
-    }
-
-/** Returns a 3x3 affine transformation matrix that rotates by [angle] degrees about the origin */
-fun rotationMatrixOf(angle: Double): D2Array<Double> {
-    val cos = cos(angle)
-    val sin = sin(angle)
-    return identityMatrix().apply {
-        this[0, 0] = cos
-        this[0, 1] = -sin
-        this[1, 0] = sin
-        this[1, 1] = cos
-    }
+fun readT1WeightedMri(id: Long): D3Array<Float> {
+    // This should in practice, of course, read the actual data, but for this example we just return a dummy array
+    return mk.rand(256, 256, 180)
 }
 
-/** Returns a 3x3 affine transformation matrix that shears by [x] and [y] */
-fun shearingMatrixOf(x: Double = 0.0, y: Double = 0.0): D2Array<Double> =
-    identityMatrix().apply {
-        this[0, 1] = tan(x)
-        this[1, 0] = tan(y)
-    }
+fun readFMRiBoldSeries(id: Long): D4Array<Float> {
+    // This should in practice, of course, read the actual data, but for this example we just return a dummy array
+    return mk.rand(64, 64, 30, 200)
+}
 
-/** Returns a 3x3 affine transformation matrix that reflects about the origin */
-fun reflectionAboutOriginMatrix(): D2Array<Double> =
-    identityMatrix().apply {
-        this[0, 0] = -1.0
-        this[1, 1] = -1.0
-    }
-
-/** Returns a 3x3 affine transformation matrix that reflects about the x-axis */
-fun reflectionAboutXAxisMatrix(): D2Array<Double> =
-    identityMatrix().apply {
-        this[1, 1] = -1.0
-    }
-
-/** Returns a 3x3 affine transformation matrix that reflects about the y-axis */
-fun reflectionAboutYAxisMatrix(): D2Array<Double> =
-    identityMatrix().apply {
-        this[0, 0] = -1.0
-    }
+fun visualize(scan: D3Array<Float>) {
+    // This would then actually visualize the scan
+}
