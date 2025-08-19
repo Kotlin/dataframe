f5d2db12caf2150514befa7fc7958f00df1af99e
andrei.kislitsyn <kisandy@yandex.ru>
2025-08-19 17:03:58 +0400

    `map` for column group tests

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/map.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/map.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.kotlinx.dataframe.api
 
 import io.kotest.matchers.shouldBe
+import org.jetbrains.kotlinx.dataframe.DataColumn
 import org.junit.Test
 
 class MapTests {
@@ -17,5 +18,20 @@ class MapTests {
             expr { "a"<Int>() + 1 }.cumSum() into "b"
         }
         df["b"][1] shouldBe 5
+    }
+
+    @Test
+    fun `ColumnGroup map`() {
+        val group = dataFrameOf("x", "y")(1, 10, 2, 20, 3, 30).asColumnGroup("g")
+        val sums = group.asDataFrame().map { row -> row["x"] as Int + row["y"] as Int }
+        sums shouldBe listOf(11, 22, 33)
+    }
+
+    @Test
+    fun `ColumnGroup asDataColumn map`() {
+        val group = dataFrameOf("x", "y")(1, 10, 2, 20, 3, 30).asColumnGroup("g")
+        val col: DataColumn<Int> = group.asDataColumn().map { it["x"] as Int + it["y"] as Int }
+        col.name() shouldBe "g"
+        col.toList() shouldBe listOf(11, 22, 33)
     }
 }
