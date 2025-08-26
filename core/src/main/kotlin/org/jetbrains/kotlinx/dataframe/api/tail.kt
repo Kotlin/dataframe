5ec6fd21803ce143803fb578cb0b3639e3014a34
andrei.kislitsyn <kisandy@yandex.ru>
2025-08-26 14:37:56 +0400

    deprecate tail

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/tail.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/tail.kt
@@ -1,9 +1,12 @@
 package org.jetbrains.kotlinx.dataframe.api
 
 import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.util.TAIL
+import org.jetbrains.kotlinx.dataframe.util.TAIL_REPLACE
 
 // region DataFrame
 
+@Deprecated(TAIL, ReplaceWith(TAIL_REPLACE), DeprecationLevel.ERROR)
 public fun <T> DataFrame<T>.tail(numRows: Int = 5): DataFrame<T> = takeLast(numRows)
 
 // endregion
