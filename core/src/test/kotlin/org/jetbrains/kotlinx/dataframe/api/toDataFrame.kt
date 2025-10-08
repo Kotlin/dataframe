239b1a1a8285558dd9ddfaa4e61c553bf898a6dd
Jolan Rensen <jolan.rensen@jetbrains.com>
2025-10-08 12:16:39 +0200

    linting

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/toDataFrame.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/toDataFrame.kt
@@ -427,7 +427,7 @@ class CreateDataFrameTests {
 
     @Test
     fun `should convert iterables of maps representing rows to DataFrame with value columns`() {
-        val maps: Iterable<Map<String, *>> = listOf(mapOf("a" to 1, "b" to true), mapOf("c" to 2, "d" to false),)
+        val maps: Iterable<Map<String, *>> = listOf(mapOf("a" to 1, "b" to true), mapOf("c" to 2, "d" to false))
         val df = maps.toDataFrame()
         df["a"][0] shouldBe 1
         df["b"][0] shouldBe true
