e4ed3727a02fb4541f3adbc08f3645a4b3f226d9
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2025-12-03 16:29:20 +0200

    Add internal DataFrame.toCode utility to help convert examples

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/Utils.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/Utils.kt
@@ -1,7 +1,9 @@
 package org.jetbrains.kotlinx.dataframe
 
+import org.jetbrains.kotlinx.dataframe.api.getColumns
 import org.jetbrains.kotlinx.dataframe.api.print
 import org.jetbrains.kotlinx.dataframe.api.schema
+import org.jetbrains.kotlinx.dataframe.columns.ColumnGroup
 import org.jetbrains.kotlinx.dataframe.io.renderToString
 import org.jetbrains.kotlinx.dataframe.types.UtilTests
 import java.net.URL
@@ -28,3 +30,45 @@ fun <T : DataFrame<*>> T.alsoDebug(println: String? = null, rowsLimit: Int = 20)
         print(borders = true, title = true, columnTypes = true, valueLimit = -1, rowsLimit = rowsLimit)
         schema().print()
     }
+
+fun DataFrame<*>.toCode(variableName: String = "df"): String =
+    buildString {
+        append("val $variableName = dataFrameOf(\n")
+        appendColumns(this@toCode, indent = 1)
+        append(")")
+    }
+
+private fun StringBuilder.appendColumns(df: DataFrame<*>, indent: Int) {
+    val pad = "    ".repeat(indent)
+    df.getColumns { colsAtAnyDepth().simplify() }.forEach { col ->
+        if (col is ColumnGroup<*>) {
+            append("$pad\"${col.name()}\" to columnOf(\n")
+            appendColumns(col, indent + 1)
+            append("$pad),\n")
+        } else {
+            appendColumn(col, pad)
+        }
+    }
+}
+
+private fun StringBuilder.appendColumn(column: DataColumn<Any?>, pad: String) {
+    append("$pad\"${column.name()}\" to columnOf(")
+    append(column.values().joinToString(", ") { it.toLiteral() })
+    append("),\n")
+}
+
+private fun Any?.toLiteral(): String =
+    when (this) {
+        null -> "null"
+        is String -> "\"${escape()}\""
+        is Char -> "'$this'"
+        is Long -> "${this}L"
+        is Float -> "${this}f"
+        else -> toString()
+    }
+
+private fun String.escape() =
+    replace("\\", "\\\\")
+        .replace("\"", "\\\"")
+        .replace("\n", "\\n")
+        .replace("\t", "\\t")
