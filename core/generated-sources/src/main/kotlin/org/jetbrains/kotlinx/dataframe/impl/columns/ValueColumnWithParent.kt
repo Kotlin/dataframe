9913e8fd7d66b53e4ceac905860306c0a548fb98
Carlo Maria Proietti <carlo.maria.proietti@outlook.it>
2026-01-17 11:59:46 +0100

    update generated sources

--- core/generated-sources/src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/columns/ValueColumnWithParent.kt
+++ core/generated-sources/src/main/kotlin/org/jetbrains/kotlinx/dataframe/impl/columns/ValueColumnWithParent.kt
@@ -7,8 +7,7 @@ import kotlin.reflect.KType
 
 internal class ValueColumnWithParent<T>(override val parent: ColumnGroup<*>, override val source: ValueColumn<T>) :
     ColumnWithParent<T>,
-    ValueColumn<T> by source,
-    ValueColumnInternal<T>,
+    ValueColumnInternal<T> by source.internalValueColumn(),
     DataColumnInternal<T> {
 
     override fun equals(other: Any?) = source.checkEquals(other)
@@ -30,15 +29,4 @@ internal class ValueColumnWithParent<T>(override val parent: ColumnGroup<*>, ove
         ValueColumnWithParent(parent, source.internal().changeType(type).asValueColumn())
 
     override fun addParent(parent: ColumnGroup<*>) = source.addParent(parent)
-
-    private val statisticsCache = mutableMapOf<String, MutableMap<Map<String, Any>, StatisticResult>>()
-
-    override fun putStatisticCache(statName: String, arguments: Map<String, Any>, value: StatisticResult) {
-        statisticsCache.getOrPut(statName) {
-            mutableMapOf<Map<String, Any>, StatisticResult>()
-        }[arguments] = value
-    }
-
-    override fun getStatisticCacheOrNull(statName: String, arguments: Map<String, Any>): StatisticResult? =
-        statisticsCache[statName]?.get(arguments)
 }
