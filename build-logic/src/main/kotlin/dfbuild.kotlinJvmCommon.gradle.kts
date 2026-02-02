383654ab50585f7ec3b939479c2b6fcbef48a198
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-02-02 13:05:18 +0100

    moved instrumentedJars to build-logic as part of kotlinJvmCommon

--- build-logic/src/main/kotlin/dfbuild.kotlinJvmCommon.gradle.kts
+++ build-logic/src/main/kotlin/dfbuild.kotlinJvmCommon.gradle.kts
@@ -17,3 +17,16 @@ kotlin {
         optIn.add("kotlin.uuid.ExperimentalUuidApi")
     }
 }
+
+// Adds the instrumentedJars configuration/artifact to all Kotlin sub-projects.
+// This allows other modules to depend on the output of this task, aka the compiled jar of that module
+// Used in :plugins:dataframe-gradle-plugin integration tests and in :samples for compiler plugin support
+val instrumentedJars: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = false
+}
+artifacts {
+    add("instrumentedJars", tasks.jar.get().archiveFile) {
+        builtBy(tasks.jar)
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -119,28 +119,6 @@ tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
     }
 }
 
-allprojects {
-    // Attempts to configure ktlint for each sub-project that uses the plugin
-    afterEvaluate {
-        // Adds the instrumentedJars configuration/artifact to all sub-projects with a `jar` task
-        // This allows other modules to depend on the output of this task, aka the compiled jar of that module
-        // Used in :plugins:dataframe-gradle-plugin integration tests and in :samples for compiler plugin support
-        try {
-            val instrumentedJars: Configuration by configurations.creating {
-                isCanBeConsumed = true
-                isCanBeResolved = false
-            }
-            artifacts {
-                add("instrumentedJars", tasks.jar.get().archiveFile) {
-                    builtBy(tasks.jar)
-                }
-            }
-        } catch (_: Exception) {
-            logger.warn("Could not set instrumentedJars on :${this.name}")
-        }
-    }
-}
-
 group = "org.jetbrains.kotlinx"
 
 fun detectVersion(): String {
