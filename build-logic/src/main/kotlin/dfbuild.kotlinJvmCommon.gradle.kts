dc470586c45480c18b476b55611226f0032b298a
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-02-05 14:31:58 +0100

    removed optIn.add("kotlin.time.ExperimentalTime") from kotlinJvmCommon.gradle.kts, it's enabled by default from kotlin 2.3+

--- build-logic/src/main/kotlin/dfbuild.kotlinJvmCommon.gradle.kts
+++ build-logic/src/main/kotlin/dfbuild.kotlinJvmCommon.gradle.kts
@@ -10,9 +10,6 @@ kotlin {
     explicitApi()
     jvmToolchain(21)
     compilerOptions {
-        // enables support for kotlin.time.Instant as kotlinx.datetime.Instant was deprecated; Issue #1350
-        // Can be removed once kotlin.time.Instant is marked "stable".
-        optIn.add("kotlin.time.ExperimentalTime")
         // can be removed once kotlin.uuid.ExperimentalUuidApi is marked "stable".
         optIn.add("kotlin.uuid.ExperimentalUuidApi")
     }
