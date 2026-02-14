a119ecc2656c4c97448f51e4f704b76ca42961c4
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2026-02-14 18:19:59 +0200

    Create source set for java record declarations to avoid testing everything on JDK16+

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/toDataFrame.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/toDataFrame.kt
@@ -611,34 +611,6 @@ class CreateDataFrameTests {
         } shouldBe dataFrameOf("b")("bb")
     }
 
-    @Test
-    fun `convert Java Record to DF`() {
-        val record = JavaRecord(42, "test", 3.14)
-        val df = listOf(record).toDataFrame()
-
-        df shouldBe dataFrameOf(
-            DataColumn.createValueColumn("property1", listOf(42), typeOf<Int>()),
-            DataColumn.createValueColumn("property2", listOf("test"), typeOf<String>()),
-            DataColumn.createValueColumn("property3", listOf(3.14), typeOf<Double>()),
-        )
-    }
-
-    @Test
-    fun `convert nested Java Record to DF with maxDepth`() {
-        val record = JavaRecord(1, "nested", 2.5)
-        val wrapper = JavaRecordWrapper("wrapper", record)
-        val df = listOf(wrapper).toDataFrame(maxDepth = 2)
-
-        df.columnNames() shouldBe listOf("name", "record")
-        df["name"][0] shouldBe "wrapper"
-
-        val recordCol = df.getColumnGroup("record")
-        recordCol.columnNames() shouldBe listOf("property1", "property2", "property3")
-        recordCol["property1"][0] shouldBe 1
-        recordCol["property2"][0] shouldBe "nested"
-        recordCol["property3"][0] shouldBe 2.5
-    }
-
     data class Arrays(val a: IntArray, val b: Array<Int>, val c: Array<Int?>)
 
     @Test
