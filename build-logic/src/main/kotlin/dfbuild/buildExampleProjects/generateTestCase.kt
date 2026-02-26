c66c820caaf79076f7f51690cb2df350ebac0463
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-02-26 15:44:45 +0100

    improving generateTestCase to satisfy linter

--- build-logic/src/main/kotlin/dfbuild/buildExampleProjects/generateTestCase.kt
+++ build-logic/src/main/kotlin/dfbuild/buildExampleProjects/generateTestCase.kt
@@ -8,23 +8,24 @@ typealias Code = String
 @Language("kt")
 fun generateTestCase(testClassName: String, folder: File, isDev: Boolean, tags: List<String>): Code =
     """
-    package dfbuild.buildExampleProjects
-    
-    import org.junit.jupiter.api.Tag
-    import org.junit.jupiter.api.Test
-    import java.io.File
-
-    class $testClassName : TestBuildingExampleProjects() {
-        val folder: File = File("${folder.path}")
-        val isDev: Boolean = $isDev
-        
-        ${tags.joinToString(prefix = "@[", separator = " ", postfix = "]") { "Tag(\"$it\")" }}
-        @Test
-        fun `can build project successfully`(): Unit =
-            buildExampleProject(
-                folder = folder,
-                isDev = isDev,
-            )
-    }
-
-    """.trimIndent()
+    |package dfbuild.buildExampleProjects
+    |
+    |import org.junit.jupiter.api.Tag
+    |import org.junit.jupiter.api.Test
+    |import java.io.File
+    |
+    |class $testClassName : TestBuildingExampleProjects() {
+    |    val folder: File =
+    |        File("${folder.path}")
+    |    val isDev: Boolean = $isDev
+    |
+    |    ${tags.joinToString(prefix = "@[", separator = " ", postfix = "]") { "Tag(\"$it\")" }}
+    |    @Test
+    |    fun `can build project successfully`(): Unit =
+    |        buildExampleProject(
+    |            folder = folder,
+    |            isDev = isDev,
+    |        )
+    |}
+    |
+    """.trimMargin()
