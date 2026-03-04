b89b328a6d9472c9d0b0e600de511ae75c432c38
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-03-04 16:41:53 +0100

    WIP moving youtube example project to /examples/projects(/dev) with compiler plugin enabled

--- examples/projects/dev/youtube/src/main/kotlin/org/jetbrains/kotlinx/dataframe/examples/youtube/youtube.kt
+++ examples/projects/dev/youtube/src/main/kotlin/org/jetbrains/kotlinx/dataframe/examples/youtube/youtube.kt
@@ -1,47 +1,32 @@
-@file:ImportDataSchema(
-    "SearchResponse",
-    "src/main/resources/searchResponse.json",
-)
-@file:ImportDataSchema(
-    "StatisticsResponse",
-    "src/main/resources/statisticsResponse.json",
-)
-
 package org.jetbrains.kotlinx.dataframe.examples.youtube
 
-import kotlinx.datetime.Instant
 import org.jetbrains.kotlinx.dataframe.AnyFrame
-import org.jetbrains.kotlinx.dataframe.AnyRow
 import org.jetbrains.kotlinx.dataframe.DataRow
-import org.jetbrains.kotlinx.dataframe.annotations.ImportDataSchema
+import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
 import org.jetbrains.kotlinx.dataframe.api.*
-import org.jetbrains.kotlinx.dataframe.dataTypes.IFRAME
-import org.jetbrains.kotlinx.dataframe.dataTypes.IMG
+import org.jetbrains.kotlinx.dataframe.api.convertTo
+import org.jetbrains.kotlinx.dataframe.api.getValueOrNull
+import org.jetbrains.kotlinx.dataframe.dataTypes.*
 import org.jetbrains.kotlinx.dataframe.io.read
 import java.net.URL
 
 fun load(path: String) = DataRow.read("$basePath/$path&key=$apiKey")
 
-fun load(path: String, maxPages: Int): AnyFrame {
-    val rows = mutableListOf<AnyRow>()
-    var pagePath = path
-    do {
-        val row = load(pagePath)
-        rows.add(row)
-        val next = row.getValueOrNull<String>("nextPageToken")
-        pagePath = "$path&pageToken=$next"
-
-    } while (next != null && rows.size < maxPages)
-    return rows.concat()
-}
+fun load(path: String, maxPages: Int): AnyFrame =
+    buildList {
+        var pagePath = path
+        do {
+            val row = load(pagePath)
+            this += row
+            val next = row.getValueOrNull<String>("nextPageToken")
+            pagePath = "$path&pageToken=$next"
+        } while (next != null && this.size < maxPages)
+    }.concat()
 
 fun main() {
     val searchRequest = "cute%20cats"
     val resultsPerPage = 50
     val maxPages = 5
-
-    val videoId by column<String>("id")
-    val channel by columnGroup()
 
     val videos = load("search?q=$searchRequest&maxResults=$resultsPerPage&part=snippet", maxPages)
         .convertTo<SearchResponse> {
@@ -50,19 +35,20 @@ fun main() {
         }
         .items.concat()
         .dropNulls { id.videoId }
-        .select { id.videoId into videoId and snippet }
+        .select { id.videoId into "id" and snippet }
         .distinct()
         .parse()
         .convert { colsAtAnyDepth().colsOf<URL>() }.with {
             IMG(it, maxHeight = 150)
-        }.add("video") {
-            val id = videoId()
+        }
+        .add("video") {
             IFRAME("http://www.youtube.com/embed/$id")
-        }.move { snippet.title and snippet.publishTime }.toTop()
-        .move { snippet.channelId and snippet.channelTitle }.under(channel)
+        }
+        .move { snippet.title and snippet.publishTime }.toTop()
+        .move { snippet.channelId and snippet.channelTitle }.under("channel")
         .remove { snippet }
 
-    val stats = videos[videoId]
+    val stats = videos.id
         .chunked(50)
         .map {
             val ids = it.joinToString("%2C")
@@ -71,14 +57,13 @@ fun main() {
         .items.concat()
         .select { id and statistics.allCols() }
         .parse()
+        // TODO replace with requireColumn {} when available
+        .cast<ParsedStats>(verify = false)
 
-    val withStat = videos.join(stats) { videoId match right.id }
-
-    val viewCount by column<Int>()
-    val publishTime by column<Instant>()
+    val withStat = videos.join(stats) { id match right.id }
 
     val channels = withStat
-        .groupBy { channel }.sum { viewCount }
+        .groupBy { channel }.sumFor { viewCount }
         .sortByDesc { viewCount }
         .flatten()
 
@@ -91,4 +76,13 @@ fun main() {
         .cumSum { viewCount }
 
     growth.print(borders = true, columnTypes = true)
+}
+
+@DataSchema
+internal interface ParsedStats {
+    val id: String
+    val commentCount: Int
+    val favoriteCount: Int
+    val likeCount: Int
+    val viewCount: Int
 }
