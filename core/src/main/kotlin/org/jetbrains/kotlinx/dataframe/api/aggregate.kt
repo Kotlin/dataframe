8d0bc0996b4e77fb49507f708a3952dca4d495e6
andrei.kislitsyn <kisandy@yandex.ru>
2026-04-08 16:18:31 +0400

    Add KDocs for `aggregate`

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/aggregate.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/aggregate.kt
@@ -3,18 +3,44 @@ package org.jetbrains.kotlinx.dataframe.api
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.DataRow
 import org.jetbrains.kotlinx.dataframe.Selector
+import org.jetbrains.kotlinx.dataframe.aggregation.AGGREGATE_DSL_APPLY
 import org.jetbrains.kotlinx.dataframe.aggregation.AGGREGATE_DSL_OPERATING_COLUMNS
 import org.jetbrains.kotlinx.dataframe.aggregation.AGGREGATE_DSL_RECEIVER
 import org.jetbrains.kotlinx.dataframe.aggregation.AGGREGATE_DSL_RESULT
+import org.jetbrains.kotlinx.dataframe.aggregation.AGGREGATE_DSL_TYPE
 import org.jetbrains.kotlinx.dataframe.aggregation.AggregateDsl
+import org.jetbrains.kotlinx.dataframe.aggregation.AggregateDslDocsSnippet
 import org.jetbrains.kotlinx.dataframe.aggregation.AggregateGroupedBody
+import org.jetbrains.kotlinx.dataframe.aggregation.AggregateGroupedDsl
 import org.jetbrains.kotlinx.dataframe.annotations.Interpretable
 import org.jetbrains.kotlinx.dataframe.annotations.Refine
+import org.jetbrains.kotlinx.dataframe.documentation.DocumentationUrls
 import org.jetbrains.kotlinx.dataframe.impl.aggregateGroupBy
-import org.jetbrains.kotlinx.dataframe.aggregation.AggregateDslDocsSnippet
+import org.jetbrains.kotlinx.dataframe.aggregation.AggregateGroupedDslDocsSnippet
 
 // region Pivot
 
+/**
+ * Aggregates this [Pivot] using the provided statistics
+ * inside the [AggregateDsl].
+ *
+ * Returns a new [DataRow] with the original [pivot] keys as top-level columns on top level
+ * and the correspndonding aggregated values in new nested columns.
+ *
+ * {@include [AggregateDslDocsSnippet]}
+ * {@set [AGGREGATE_DSL_TYPE] [AggregateDsl]}
+ * {@set [AGGREGATE_DSL_RECEIVER] [Pivot]}
+ * {@set [AGGREGATE_DSL_APPLY] The given [expression][body] is applied to each group independently.}
+ * {@set [AGGREGATE_DSL_RESULT] [DataRow]}
+ * {@set [AGGREGATE_DSL_OPERATING_COLUMNS] columns within groups in [Pivot]}
+ *
+ * Check out [`pivot` Grammar][PivotDocs.Grammar] for more information.
+ *
+ * For more information: {@include [DocumentationUrls.Pivot]}
+ *
+ * @param body The aggregation logic defined using [AggregateDsl].
+ * @return A new [DataFrame] with the results of the aggregation applied to each group.
+ */
 public fun <T, R> Pivot<T>.aggregate(separate: Boolean = false, body: Selector<AggregateDsl<T>, R>): DataRow<T> =
     delegate {
         aggregate(separate, body)
@@ -24,19 +50,19 @@ public fun <T, R> Pivot<T>.aggregate(separate: Boolean = false, body: Selector<A
 
 /**
  * Aggregates this [GroupBy] using the provided statistics
- * inside the [AggregateDsl].
+ * inside the [AggregateGroupedDsl].
  *
- * {@include [AggregateDslDocsSnippet]}
- * {@set [AGGREGATE_DSL_RECEIVER] [GroupBy]}
- * {@set [AGGREGATE_DSL_RESULT] [DataFrame]}
- * {@set [AGGREGATE_DSL_OPERATING_COLUMNS] columns within groups of this [GroupBy]}
-
+ * Returns a new [DataFrame] with the original [groupBy] key columns
+ * and the correspodning aggregated values in new columns.
  *
- * @param T The schema type of the DataFrame.
- * @param R The return type of the aggregation logic.
- * @param body The aggregation logic defined as an [AggregateGroupedBody].
- *             This logic specifies how each group should be aggregated.
- * @return A new DataFrame with the results of the aggregation applied to each group.
+ * {@include [AggregateGroupedDslDocsSnippet]}
+ *
+ * Check out [`groupBy` Grammar][GroupByDocs.Grammar] for more information.
+ *
+ * For more information: {@include [DocumentationUrls.GroupBy]}
+ *
+ * @param body The aggregation logic defined using [AggregateGroupedDsl].
+ * @return A new [DataFrame] with the results of the aggregation applied to each group.
  */
 @Refine
 @Interpretable("Aggregate")
