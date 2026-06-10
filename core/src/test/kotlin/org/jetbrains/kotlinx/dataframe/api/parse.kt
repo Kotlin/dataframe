c67f5fedf48b4500971fe265e15b520f13a95bb9
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-06-10 13:33:16 +0200

    linting

--- core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/parse.kt
+++ core/src/test/kotlin/org/jetbrains/kotlinx/dataframe/api/parse.kt
@@ -38,7 +38,6 @@ import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaInstant
 import kotlin.time.toKotlinInstant
 import kotlin.uuid.Uuid
-import kotlinx.datetime.Instant as DeprecatedInstant
 import java.time.Duration as JavaDuration
 import java.time.Instant as JavaInstant
 import java.time.LocalDate as JavaLocalDate
@@ -46,6 +45,7 @@ import java.time.LocalDateTime as JavaLocalDateTime
 import java.time.LocalTime as JavaLocalTime
 import java.time.Month as JavaMonth
 import kotlin.time.Instant as StdlibInstant
+import kotlinx.datetime.Instant as DeprecatedInstant
 
 class ParseTests {
 
