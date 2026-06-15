586ec4ea8c1bb39390d7544f9fd5835c4897389f
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-06-15 14:50:12 +0200

    added some helpful exceptions to Any.getVersionName() function for dfbuild.buildExampleProjects

--- build-logic/src/main/kotlin/dfbuild/utils.kt
+++ build-logic/src/main/kotlin/dfbuild/utils.kt
@@ -20,24 +20,51 @@ fun File.findRootDir(): File {
 }
 
 // tiny reflection-based solution to retrieve the original version name from the accessor,
-// so we can keep the list of version type-safe
+// so we can keep the list of version type-safe in dfbuild.buildExampleProjects
 fun Any.getVersionName(): String {
     val klass = this::class.java
     if (this is Provider<*>) {
-        val valueField = klass.declaredFields
-            .single { it.name == "value" }
-            .also { it.isAccessible = true }
-        val result = valueField.get(this)
+        val result = try {
+            val valueField = klass.declaredFields
+                .single { it.name == "value" }
+                .also { it.isAccessible = true }
+            valueField.get(this)
+        } catch (e: Throwable) {
+            throw IllegalStateException(
+                "`dfbuild.buildExampleProjects` failed to get a version name from libs.versions.toml because the " +
+                    "`Provider<String>` did not have an (accessible) `value: Callable<String>` field. " +
+                    "`Any.getVersionName()` might need to be fixed. Class: $this",
+                e,
+            )
+        }
         val lambdaClass = result::class.java
-        val nameField = lambdaClass.declaredFields
-            .single { it.type == String::class.java }
-            .also { it.isAccessible = true }
-        val name = nameField.get(result) as String
+        val name = try {
+            val nameField = lambdaClass.declaredFields
+                .single { it.type == String::class.java }
+                .also { it.isAccessible = true }
+            nameField.get(result) as String
+        } catch (e: Throwable) {
+            throw IllegalStateException(
+                "`dfbuild.buildExampleProjects` failed to get a version name from libs.versions.toml because the " +
+                    "`value: Callable<String>` did not have an (accessible) `String` typed field containing the " +
+                    "name of the version. `Any.getVersionName()` might need to be fixed.\nClass: $this",
+                e,
+            )
+        }
         return name.replace('.', '-')
     } else {
-        val asProviderFunction = klass.declaredMethods
-            .single { it.name == "asProvider" }
-        val provider = asProviderFunction.invoke(this) as Provider<*>
+        val provider = try {
+            val asProviderFunction = klass.declaredMethods
+                .single { it.name == "asProvider" }
+            asProviderFunction.invoke(this) as Provider<*>
+        } catch (e: Throwable) {
+            throw IllegalStateException(
+                "`dfbuild.buildExampleProjects` failed to get a version name from libs.versions.toml because a " +
+                    "version group was expected, but the encountered value did not have an (accessible) " +
+                    "`asProvider()` function. `Any.getVersionName()` might need to be fixed.\nClass: $this",
+                e,
+            )
+        }
         return provider.getVersionName()
     }
 }
