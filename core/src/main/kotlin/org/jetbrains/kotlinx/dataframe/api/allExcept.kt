3f7fcb6f776062c47cfac64051c490a8a4e72621
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-06-16 15:50:40 +0200

    renamed incorrect vararg function argument other -> others

--- core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/allExcept.kt
+++ core/src/main/kotlin/org/jetbrains/kotlinx/dataframe/api/allExcept.kt
@@ -552,8 +552,8 @@ public interface AllExceptColumnsSelectionDsl {
      * @include [ColumnGroupDocs.ColumnPathArgs]
      */
     @Interpretable("ColumnGroupAllColsExceptColumnPaths")
-    public fun SingleColumn<DataRow<*>>.allColsExcept(vararg other: ColumnPath): ColumnSet<*> =
-        allColsExceptInternal(other.toColumnSet())
+    public fun SingleColumn<DataRow<*>>.allColsExcept(vararg others: ColumnPath): ColumnSet<*> =
+        allColsExceptInternal(others.toColumnSet())
 
     // endregion
 
