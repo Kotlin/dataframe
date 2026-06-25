cf496fc902af458e2f2134aaac2f0f6c21225b94
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-06-25 11:57:43 +0200

    updated KoDEx generated-sources due to the removal of tests from its context

--- core/generated-sources/src/generated-dataschema-accessors/test/kotlin/org/jetbrains/kotlinx/dataframe/samples/api/Modify.ParseDf$Generated.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.kotlinx.dataframe.samples.api
-
-import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
-
-@DataSchema
-interface ParseDf {
-    val date: String
-    val value: String
-}
