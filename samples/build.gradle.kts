84e8c03049ed2d7eef8325821ff5a1c4bf7b7017
Aleksandr Nikolaev <aleksandr.nikolaev@jetbrains.com>
2026-08-14 14:59:21 +0200

    Update `valueCounts` webpage and samples

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -117,6 +117,7 @@ korro {
                 include("groupBy.md")
                 include("pivot.md")
                 include("countDistinct.md")
+                include("valueCounts.md")
             },
         )
         baseDir = rootProject.file("docs/StardustDocs/topics")
