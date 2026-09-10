e58ab1c4d64e636ec917a77b2d1035f626285378
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2026-09-10 16:45:32 +0300

    Rework plugin-core shadow jar publishing to completely avoid dependencies in pom
    Before, the problem was that even though dependency is excluded and actually no files are bundled, Gradle dependency verification still picks up that core dependency brings slf4j-api.

--- dataframe-compiler-plugin-core/build.gradle.kts
+++ dataframe-compiler-plugin-core/build.gradle.kts
@@ -13,8 +13,13 @@ plugins {
 
 group = "org.jetbrains.kotlinx.dataframe"
 
+val shadowCore = configurations.create("shadowCore") {
+    isCanBeConsumed = false
+    isCanBeResolved = true
+}
+
 dependencies {
-    implementation(projects.core) {
+    shadowCore(projects.core) {
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-reflect")
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
         exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-datetime-jvm")
@@ -22,26 +27,28 @@ dependencies {
         exclude(group = "commons-io", module = "commons-csv")
         exclude(group = "org.apache.commons", module = "commons-csv")
         exclude(group = "org.slf4j", module = "slf4j-api")
-        exclude(group = "io.github.microutils", module = "kotlin-logging-jvm")
+        exclude(group = "io.github.oshai", module = "kotlin-logging")
         exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-serialization-core-jvm")
         exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-serialization-json-jvm")
         exclude(group = "commons-codec", module = "commons-codec")
-        exclude(group = "com.squareup", module = "kotlinpoet-jvm")
+        exclude(group = "com.squareup", module = "kotlinpoet")
         exclude(group = "ch.randelshofer", module = "fastdoubleparser")
-        exclude(group = "io.github.oshai", module = "kotlin-logging-jvm")
         exclude(group = "org.jetbrains", module = "annotations")
     }
 
     // we assume Kotlin plugin has reflect dependency - we're not bringing our own version
     testImplementation(libs.kotlin.reflect)
     testImplementation(libs.kotlin.test.junit5)
+    testImplementation(files(tasks.named<ShadowJar>("shadowJar")))
 }
 
 tasks.test {
+    dependsOn(tasks.named<ShadowJar>("shadowJar"))
     useJUnitPlatform()
 }
 
 tasks.withType<ShadowJar> {
+    configurations = listOf(shadowCore)
     dependencies {
         exclude(dependency("org.jetbrains.kotlin:kotlin-reflect:.*"))
         exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib:.*"))
@@ -50,13 +57,12 @@ tasks.withType<ShadowJar> {
         exclude(dependency("commons-io:commons-csv:.*"))
         exclude(dependency("org.apache.commons:commons-csv:.*"))
         exclude(dependency("org.slf4j:slf4j-api:.*"))
-        exclude(dependency("io.github.microutils:kotlin-logging-jvm:.*"))
+        exclude(dependency("io.github.oshai:kotlin-logging:.*"))
         exclude(dependency("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:.*"))
         exclude(dependency("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:.*"))
         exclude(dependency("commons-codec:commons-codec:.*"))
-        exclude(dependency("com.squareup:kotlinpoet-jvm:.*"))
+        exclude(dependency("com.squareup:kotlinpoet:.*"))
         exclude(dependency("ch.randelshofer:fastdoubleparser:.*"))
-        exclude(dependency("io.github.oshai:kotlinlogging:.*"))
         exclude(dependency("org.jetbrains:annotations:.*"))
     }
     exclude("org/jetbrains/kotlinx/dataframe/jupyter/**")
@@ -88,5 +94,12 @@ kotlinPublications {
         publicationName = "shadowed"
         artifactId = "dataframe-compiler-plugin-core"
         packageName = artifactId
+        composeOf {
+            artifact(tasks.named<ShadowJar>("shadowJar")) {
+                classifier = null
+            }
+            artifact(tasks.named("sourcesJar"))
+            artifact(tasks.named("javadocJar"))
+        }
     }
 }
